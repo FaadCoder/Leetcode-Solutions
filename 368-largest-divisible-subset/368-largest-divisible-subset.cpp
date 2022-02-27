7bf486e89e6fdf14aeca3e5071fd2b04a421a8bc
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-02-27 21:33:22 +0530

    Time: 62 ms (65.50%), Space: 8.9 MB (48.10%) - LeetHub

--- 368-largest-divisible-subset/368-largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/368-largest-divisible-subset.cpp
@@ -8,20 +8,25 @@ public:
       vector<int> dp(n, 1);
       vector<int> previous_index(n, -1);
       int max_ind = 0;  
-       for(int i=1; i<n; i++) {
-           for(int j=0; j<i; j++) {
-               if(A[i]%A[j]==0 and dp[i] < dp[j] + 1) {
+       for(int i=1; i<n; i++)
+       {
+           for(int j=0; j<i; j++)
+           {
+               if(A[i]%A[j]==0 and dp[i] < dp[j] + 1)
+               {
                    dp[i] = dp[j]+1;
                    previous_index[i] = j;
                }
            }
-           if(dp[i] > dp[max_ind]) {
+           if(dp[i] > dp[max_ind])
+           {
                max_ind = i;
            }
        }
       vector<int> answer;
       int t = max_ind;  
-      while(t >= 0) {
+      while(t >= 0)
+      {
           answer.push_back(A[t]);
           t = previous_index[t];
       }
