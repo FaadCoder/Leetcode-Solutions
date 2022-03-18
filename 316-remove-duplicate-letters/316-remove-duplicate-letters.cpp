d086253721e21e3332580a53e37b3943d641632e
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-03-18 07:44:22 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (97.70%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -5,6 +5,7 @@ public:
         string result;
         int freq[26] = {0};
         int vis[26] = {0};
+        
         // 1. Count occurrences of each character in the string.
         for(char ch:s)
             freq[ch-'a']++;
@@ -13,14 +14,26 @@ public:
         for(char ch:s)
         {
             freq[ch-'a']--;
+            
+            // Don't use the same character again (As mentioned in the constraints)
             if(vis[ch-'a'])
                 continue;
+            
+            // Mark the used character visited.
             vis[ch-'a'] = 1;
-            while(!result.empty() and ch < result.back() and freq[result.back()-'a']>0)
-            {
+            
+            // Try to make monotonic increasing string.
+            while(!result.empty() and ch < result.back() and freq[result.back()-'a'] > 0)
+            {   
+                // Mark the removed character UnVisited.
                 vis[result.back()-'a'] = 0;
+                
+                // Remove the character.
                 result.pop_back();
             }
+            
+            // At this point we are sure that the string is monotonic,
+            // so add the character in the resultant string.
             result.push_back(ch);
         }
         return result;
