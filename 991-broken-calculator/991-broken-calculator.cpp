62af66e27a04c18e234ef62923a39b13edd1d3f3
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-03-23 18:57:59 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (30.21%) - LeetHub

---
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int brokenCalc(int startValue, int target) 
+    {
+        if(startValue >= target)
+            return startValue-target;
+        if(target%2)
+            return 1+brokenCalc(startValue,target+1);
+        return 1+brokenCalc(startValue,target/2);
+    }
+};
