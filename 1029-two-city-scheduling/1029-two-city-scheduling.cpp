24d1af78a4f287ea814dc2c02b300cebee785f30
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-03-25 09:32:23 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (94.48%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.cpp
+++ 1029-two-city-scheduling/1029-two-city-scheduling.cpp
@@ -2,19 +2,13 @@ class Solution {
 public:
     int twoCitySchedCost(vector<vector<int>>& costs)
     {
-        vector<pair<int,int>> savings;
-        for(int idx=0; idx<costs.size(); idx++)
-            savings.push_back({costs[idx][0]-costs[idx][1],idx});
-        int sumA = 0;
-        int sumB = 0;
-        sort(savings.begin(),savings.end());
+        sort(costs.begin(),costs.end(),[&](vector<int> &a,vector<int> &b){
+            return (a[1]-a[0] > b[1] - b[0]);
+        });
+        int cost = 0;
         int n = costs.size();
-        for(int i=0;i<n/2;i++)
-            sumA += costs[savings[i].second][0];
-        
-        for(int i=n/2;i<n;i++)
-            sumB += costs[savings[i].second][1];
-              
-        return sumA + sumB;
+        for(int idx=0; idx<n; idx++)
+            cost += (idx>=(n/2))?costs[idx][1]:costs[idx][0];
+        return cost;
     }
 };
