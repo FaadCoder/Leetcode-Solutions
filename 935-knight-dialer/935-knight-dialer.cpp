4b2d672e30f1cd6fb8c1bf50c203d63c765b7692
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-05-11 09:28:02 +0530

    Time: 384 ms (23.50%), Space: 6.6 MB (79.00%) - LeetHub

---
+++ 935-knight-dialer/935-knight-dialer.cpp
@@ -0,0 +1,64 @@
+class Solution {
+    static const int numberOfRows = 4;
+    static const int numberOfCols = 3;
+    const int mod = 1e9+7;
+    int dir[8][2] = {{-2, 1}, {-1, 2}, {1, 2}, {2, 1}, {2, -1}, {1, -2}, {-1, -2}, {-2, -1}};
+    
+    int dp[numberOfRows][numberOfCols][5002];
+    
+    bool isValidMove(int row,int col)
+    {
+        return (row>=0 and col>=0 and row<numberOfRows and col<numberOfCols);
+    }
+
+    bool isCurrentPositionANonNumericCell(int row,int col)
+    {
+        return ((row==numberOfRows-1) and col!=1);
+    }
+
+    bool isCurrentPositionANumericCell(int row,int col)
+    {
+        return !isCurrentPositionANonNumericCell(row,col);
+    }
+
+    int kngihtDialerHelper(int row,int col,int n)
+    {   
+        if(!isValidMove(row,col) or !isCurrentPositionANumericCell(row,col))
+            return 0;
+
+        if(n==1)
+           return 1;
+
+        if(dp[row][col][n]!=-1)
+            return dp[row][col][n];
+
+        int res=0;
+        for(int idx = 0; idx<8; idx++)
+        {
+            int newRow = row + dir[idx][0];
+            int newCol = col + dir[idx][1];
+            res = addUnderModulo(res,kngihtDialerHelper(newRow,newCol,n-1));
+        }
+        return dp[row][col][n] = res;
+    }
+
+    int addUnderModulo(int num1,int num2)
+    {
+        return (num1%mod + num2%mod)%mod;
+    }
+    
+public:
+    int knightDialer(int n) 
+    {
+        memset(dp,-1,sizeof(dp));
+        int res = 0;
+        for(int row = 0; row<numberOfRows; row++)
+        {
+            for(int col = 0; col<numberOfCols; col++)
+            {
+                res = addUnderModulo(res,kngihtDialerHelper(row,col,n));
+            }
+        }
+        return res;
+    }
+};
