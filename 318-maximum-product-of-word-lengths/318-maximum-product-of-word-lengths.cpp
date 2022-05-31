b4791792441914a856290e6c82144a032a5a2956
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-05-31 09:06:06 +0530

    Time: 50 ms (90.33%), Space: 16.9 MB (37.15%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.cpp
@@ -1,54 +1,55 @@
-class Solution {
-    
-    int getWordMask(string &word)
+class Solution
+{
+
+    int getWordMask(string & word)
     {
         int wordMask = 0;
-        
-        for(char ch:word)
+
+        for (char ch: word)
         {
-            wordMask |= (1<<(ch-'a'));
+            wordMask |= (1 << (ch - 'a'));
         }
-        
+
         return wordMask;
     }
-    
+
     vector<int> maskWords(vector<string> &words)
     {
         int numberOfWords = words.size();
-        vector<int> maskArray(numberOfWords,0);
+        vector<int> maskArray(numberOfWords, 0);
         int idx = 0;
-        for(string word:words)
+        for (string word: words)
         {
             maskArray[idx] = getWordMask(word);
-            idx+=1;
+            idx += 1;
         }
-        
+
         return maskArray;
     }
-    
-    int calculateMaxProduct(vector<int> charMaskArray,vector<string>& words)
+
+    int calculateMaxProduct(vector<int> charMaskArray, vector<string> &words)
     {
         int numberOfWords = words.size();
         int maxProduct = 0;
-        for(int outerIdx = 0; outerIdx<numberOfWords;outerIdx++)
+        for (int outerIdx = 0; outerIdx < numberOfWords; outerIdx++)
         {
-            for(int innerIdx = outerIdx+1; innerIdx<numberOfWords;innerIdx++)
+            for (int innerIdx = outerIdx + 1; innerIdx < numberOfWords; innerIdx++)
             {
-                if(!(charMaskArray[outerIdx] & charMaskArray[innerIdx]))
+                if (!(charMaskArray[outerIdx] &charMaskArray[innerIdx]))
                 {
-                    maxProduct = max(maxProduct,(int)words[outerIdx].length() * (int)words[innerIdx].length());
+                    maxProduct = max(maxProduct, (int) words[outerIdx].length() *(int) words[innerIdx].length());
                 }
-            }   
+            }
         }
         return maxProduct;
     }
 
-public:
-    int maxProduct(vector<string>& words) 
-    {
-        int numberOfWords = words.size();
-        vector<int> charMaskArray = maskWords(words);
-        int maxProductAns = calculateMaxProduct(charMaskArray,words);
-        return maxProductAns;
-    }
+    public:
+        int maxProduct(vector<string> &words)
+        {
+            int numberOfWords = words.size();
+            vector<int> charMaskArray = maskWords(words);
+            int maxProductAns = calculateMaxProduct(charMaskArray, words);
+            return maxProductAns;
+        }
 };
