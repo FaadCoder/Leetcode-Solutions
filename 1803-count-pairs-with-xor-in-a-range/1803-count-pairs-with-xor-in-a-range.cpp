ee12f2b5fcd0822ab27c348aef9ba2322c91fa08
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-05-31 10:25:33 +0530

    Time: 402 ms (32.39%), Space: 71.3 MB (76.06%) - LeetHub

--- 1803-count-pairs-with-xor-in-a-range/1803-count-pairs-with-xor-in-a-range.cpp
+++ 1803-count-pairs-with-xor-in-a-range/1803-count-pairs-with-xor-in-a-range.cpp
@@ -1,5 +1,6 @@
-struct Node{
-    Node *left,*right;
+struct Node
+{
+    Node *left, *right;
     int count;
     Node()
     {
@@ -8,103 +9,96 @@ struct Node{
     }
 };
 
+class Trie
+{
+    private:
+        Node * root;
 
-class Trie{
-private:
-    Node *root;
-    
     int getCount(Node *node)
     {
-        return (!node)?0:node->count;
+        return (!node) ? 0 : node->count;
     }
-    
-    int queryHelper(int val,int limit,int bitIdx,Node *node)
+
+    int queryHelper(int val, int limit, int bitIdx, Node *node)
     {
-        if(!node)
+        if (!node)
             return 0;
-        if(bitIdx == -1)
+        if (bitIdx == -1)
             return node->count;
-        int valIdx = (val & (1<<bitIdx))>0?1:0;
-        int limitIdx = (limit & (1<<bitIdx))>0?1:0;
-        if(valIdx == 0)
+        int valIdx = (val &(1 << bitIdx)) > 0 ? 1 : 0;
+        int limitIdx = (limit &(1 << bitIdx)) > 0 ? 1 : 0;
+        if (valIdx == 0)
         {
-            if(limitIdx==0)
+            if (limitIdx == 0)
             {
-                return queryHelper(val,limit,bitIdx-1,node->left);
+                return queryHelper(val, limit, bitIdx - 1, node->left);
             }
             else
             {
-                return getCount(node->left) + queryHelper(val,limit,bitIdx-1,node->right);
+                return getCount(node->left) + queryHelper(val, limit, bitIdx - 1, node->right);
             }
         }
         else
         {
-            if(limitIdx == 0)
+            if (limitIdx == 0)
             {
-                return queryHelper(val,limit,bitIdx-1,node->right);
+                return queryHelper(val, limit, bitIdx - 1, node->right);
             }
             else
             {
-               return getCount(node->right) + queryHelper(val,limit,bitIdx-1,node->left); 
+                return getCount(node->right) + queryHelper(val, limit, bitIdx - 1, node->left);
             }
         }
     }
-    
-public:  
-    Trie()
-    {
-        root = new Node();
-    }
 
-    void insert(int val,int maxBits)
+    public:
+        Trie()
+        {
+            root = new Node();
+        }
+
+    void insert(int val, int maxBits)
     {
         Node *temp = root;
-        for(int bit = maxBits; bit>=0; bit--)
+        for (int bit = maxBits; bit >= 0; bit--)
         {
-            int valIdx = (val & (1<<bit))>0?1:0;
-            if(valIdx==0)
+            int valIdx = (val &(1 << bit)) > 0 ? 1 : 0;
+            if (valIdx == 0)
             {
-                if(!temp->left)
+                if (!temp->left)
                     temp->left = new Node();
                 temp = temp->left;
             }
             else
             {
-                if(!temp->right)
+                if (!temp->right)
                     temp->right = new Node();
                 temp = temp->right;
             }
             temp->count++;
         }
-        
     }
-    
-    int query(int val,int limit,int bitIdx)
+
+    int query(int val, int limit, int bitIdx)
     {
-        return queryHelper(val,limit,bitIdx,root);
+        return queryHelper(val, limit, bitIdx, root);
     }
-  
 };
 
-class Solution {
-public:
-    int countPairs(vector<int>& nums, int low, int high) 
-    {
-        int maxBits = 14;
-        int pairsCount = 0;
-        Trie trie;
-        for(int num:nums)
+class Solution
+{
+    public:
+        int countPairs(vector<int> &nums, int low, int high)
         {
-            pairsCount += trie.query(num,high,maxBits);
-            pairsCount -= trie.query(num,low-1,maxBits);
-            trie.insert(num,maxBits);
+            int maxBits = 14;
+            int pairsCount = 0;
+            Trie trie;
+            for (int num: nums)
+            {
+                pairsCount += trie.query(num, high, maxBits);
+                pairsCount -= trie.query(num, low - 1, maxBits);
+                trie.insert(num, maxBits);
+            }
+            return pairsCount;
         }
-        return pairsCount;
-    }
 };
-
-
-
-
-
-
