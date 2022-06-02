f336ea6bb789b10f285d81be09adf4d890b597c9
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-06-02 16:29:47 +0530

    Time: 437 ms (5.10%), Space: 74.6 MB (36.33%) - LeetHub

---
+++ 1818-minimum-absolute-sum-difference/1818-minimum-absolute-sum-difference.cpp
@@ -0,0 +1,23 @@
+class Solution
+{
+    const int mod = 1e9+7;
+    public:
+        int minAbsoluteSumDiff(vector<int> &nums1, vector<int> &nums2)
+        {
+            set<int> treeSet(begin(nums1), end(nums1));
+            int numberOfElements = nums1.size();
+            int origDiff = 0;
+            int maxDecrement = 0;
+            for(int idx = 0; idx<numberOfElements; idx++)
+            {
+                int currDiff = abs(nums1[idx]-nums2[idx]);
+                origDiff = (origDiff + currDiff)%mod;
+                auto it = treeSet.lower_bound(nums2[idx]);
+                if(it!=treeSet.begin())
+                    maxDecrement = max(maxDecrement, currDiff - abs(*prev(it) - nums2[idx]));
+                if(it!=treeSet.end())
+                    maxDecrement = max(maxDecrement, currDiff - abs(*(it) - nums2[idx]));
+            }
+            return (origDiff - maxDecrement + mod)%mod;
+        }
+};
