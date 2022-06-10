d7aee24c526b2dc038fd1b19a806c37d3dda21c6
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-06-10 09:07:15 +0530

    Time: 26 ms (51.93%), Space: 8.9 MB (40.08%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,23 +1,24 @@
-class Solution {
-public:
-    int lengthOfLongestSubstring(string s) 
-    {
-        unordered_map<char,int> letterFrequencyMap;
-        int windowStart = 0;
-        int stringSize = (int)s.length();
-        int longestSubstringLength = 0;
-        for(int windowEnd = 0; windowEnd < stringSize; windowEnd++)
+class Solution
+{
+    public:
+        int lengthOfLongestSubstring(string s)
         {
-            letterFrequencyMap[s[windowEnd]]++;
-            while(letterFrequencyMap[s[windowEnd]]>1 and windowStart<=windowEnd)
+            unordered_map<char, int> letterFrequencyMap;
+            int windowStart = 0;
+            int stringSize = (int) s.length();
+            int longestSubstringLength = 0;
+            for (int windowEnd = 0; windowEnd < stringSize; windowEnd++)
             {
-                letterFrequencyMap[s[windowStart]]-=1;
-                if(letterFrequencyMap[s[windowStart]]<=0)
-                    letterFrequencyMap.erase(s[windowStart]);
-                windowStart+=1;
+                letterFrequencyMap[s[windowEnd]]++;
+                while (letterFrequencyMap[s[windowEnd]] > 1 and windowStart <= windowEnd)
+                {
+                    letterFrequencyMap[s[windowStart]] -= 1;
+                    if (letterFrequencyMap[s[windowStart]] <= 0)
+                        letterFrequencyMap.erase(s[windowStart]);
+                    windowStart += 1;
+                }
+                longestSubstringLength = max(longestSubstringLength, windowEnd - windowStart + 1);
             }
-            longestSubstringLength = max(longestSubstringLength,windowEnd - windowStart + 1);
+            return longestSubstringLength;
         }
-        return longestSubstringLength;
-    }
 };
