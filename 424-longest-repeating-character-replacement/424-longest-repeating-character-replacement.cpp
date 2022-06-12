24273cf555e732badd9961258dd36412f77e473b
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-06-12 10:48:18 +0530

    Time: 33 ms (22.24%), Space: 7 MB (54.84%) - LeetHub

---
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) 
+    {
+        unordered_map<char,int> frequencyMap;
+        int longestSubstringWithSameLetter = 0;
+        int windowStart = 0;
+        int maxCharacterCount = 0;
+        for(int windowEnd = 0; windowEnd<s.length(); windowEnd++)
+        {
+            char charAtWindowEnd = s[windowEnd];
+            frequencyMap[charAtWindowEnd]++;
+            maxCharacterCount = max(maxCharacterCount,frequencyMap[charAtWindowEnd]);
+            int windowSize = windowEnd - windowStart + 1;
+            
+            while(windowSize - maxCharacterCount > k)
+            {
+                char charAtWindowStart = s[windowStart];
+                frequencyMap[charAtWindowStart]--;
+                maxCharacterCount = max(maxCharacterCount,frequencyMap[charAtWindowEnd]);
+                windowStart++;
+                windowSize = windowEnd - windowStart + 1;
+            }
+            
+            windowSize = windowEnd - windowStart + 1;
+            longestSubstringWithSameLetter = max(longestSubstringWithSameLetter,windowSize);
+        }
+        
+        return longestSubstringWithSameLetter;
+    }
+};
