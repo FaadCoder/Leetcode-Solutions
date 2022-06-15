823837f86d3d9a0621ce18e5492db29e9176f2cf
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-06-15 10:24:58 +0530

    Time: 79 ms (87.76%), Space: 17.4 MB (48.86%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,32 +1,35 @@
-class Solution {
-public:
-    int longestStrChain(vector<string>& words) {
-    
-        // Lambda function for Custom Sorting Comparator.
-        auto sortingComparator = [&](string &word1,string &word2){
-            return word1.length() < word2.length();
-        };
-        
-        // Apply sorting to words vector on the basis of Lengths.
-        sort(words.begin(),words.end(),sortingComparator);
-        
-        // HashMap to keep track of visited words and values.
-        unordered_map<string,int> stringCache;
-        
-        int longestStringChain = 0;
-        
-        // Go through each word in words, and try to form longest chain.
-        for(string word:words)
+class Solution
+{
+    public:
+        int longestStrChain(vector<string> &words)
         {
-            int wordLen = word.length();
-            for(int idx = 0; idx<wordLen; idx++)
+
+           	// Lambda function for Custom Sorting Comparator.
+            auto sortingComparator =[& ](string &word1, string &word2)
             {
-                string stringExcludingCurrentIdx = word.substr(0,idx) + word.substr(idx+1);
-                stringCache[word] = max(stringCache[word],
-                                        stringCache.count(stringExcludingCurrentIdx) ? stringCache[stringExcludingCurrentIdx] + 1 : 1);
+                return word1.length() < word2.length();
+            };
+
+           	// Apply sorting to words vector on the basis of Lengths.
+            sort(words.begin(), words.end(), sortingComparator);
+
+           	// HashMap to keep track of visited words and values.
+            unordered_map<string, int> stringCache;
+
+            int longestStringChain = 0;
+
+           	// Go through each word in words, and try to form longest chain.
+            for (string word: words)
+            {
+                int wordLen = word.length();
+                for (int idx = 0; idx < wordLen; idx++)
+                {
+                    string stringExcludingCurrentIdx = word.substr(0, idx) + word.substr(idx + 1);
+                    stringCache[word] = max(stringCache[word],
+                        stringCache.count(stringExcludingCurrentIdx) ? stringCache[stringExcludingCurrentIdx] + 1 : 1);
+                }
+                longestStringChain = max(longestStringChain, stringCache[word]);
             }
-            longestStringChain = max(longestStringChain, stringCache[word]);
+            return longestStringChain;
         }
-        return longestStringChain;
-    }
 };
