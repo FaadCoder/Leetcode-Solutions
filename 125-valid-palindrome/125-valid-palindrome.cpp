b8c5a27f11a99f35228a508b10561c8b5803a98c
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-06-22 09:05:43 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (75.33%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,39 +1,38 @@
-class Solution {
-    
+class Solution
+{
+
     bool isAlphanumeric(char ch)
     {
-        return ((ch>='A' and ch<='Z') or (ch>='a' and ch<='z') or (ch>='0' and ch<='9'));
+        return ((ch >= 'A' and ch <= 'Z') 
+                or (ch >= 'a' and ch <= 'z') 
+                or (ch >= '0' and ch <= '9'));
     }
-    
-public:
-    bool isPalindrome(string s) 
-    {
-        int startPtr = 0;
-        int endPtr = s.length()-1;
-        while(startPtr < endPtr)
-        {
-            char charAtStartPtr = s[startPtr];
-            char charAtEndPtr = s[endPtr];
 
-            bool isCharAtStartPtrAlphanumeric = isAlphanumeric(charAtStartPtr);
-            bool isCharAtEndtPtrAlphanumeric = isAlphanumeric(charAtEndPtr);
-            
-            if(isCharAtStartPtrAlphanumeric and isCharAtEndtPtrAlphanumeric)
+    public:
+        bool isPalindrome(string s)
+        {
+            int startPtr = 0;
+            int endPtr = s.length() - 1;
+            while (startPtr < endPtr)
             {
-                if(tolower(charAtStartPtr) != tolower(charAtEndPtr))
-                    return false;
-                startPtr+=1;
-                endPtr-=1;
+                char charAtStartPtr = s[startPtr];
+                char charAtEndPtr = s[endPtr];
+
+                bool isCharAtStartPtrAlphanumeric = isAlphanumeric(charAtStartPtr);
+                bool isCharAtEndtPtrAlphanumeric = isAlphanumeric(charAtEndPtr);
+
+                if (isCharAtStartPtrAlphanumeric and isCharAtEndtPtrAlphanumeric)
+                {
+                    if (tolower(charAtStartPtr) != tolower(charAtEndPtr))
+                        return false;
+                    startPtr += 1;
+                    endPtr -= 1;
+                }
+                else if (!isCharAtStartPtrAlphanumeric)
+                    startPtr += 1;
+                else
+                    endPtr -= 1;
             }
-            else if(!isCharAtStartPtrAlphanumeric)
-            {
-                startPtr += 1;
-            }
-            else
-            {
-                endPtr -= 1;
-            }
+            return true;
         }
-        return true;
-    }
 };
