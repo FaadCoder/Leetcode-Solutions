2ea0e0eb87ca2592cee20ca3cad33e7f3c6b80c2
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-06-22 09:04:29 +0530

    Time: 4 ms (85.20%), Space: 7.3 MB (75.33%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -0,0 +1,39 @@
+class Solution {
+    
+    bool isAlphanumeric(char ch)
+    {
+        return ((ch>='A' and ch<='Z') or (ch>='a' and ch<='z') or (ch>='0' and ch<='9'));
+    }
+    
+public:
+    bool isPalindrome(string s) 
+    {
+        int startPtr = 0;
+        int endPtr = s.length()-1;
+        while(startPtr < endPtr)
+        {
+            char charAtStartPtr = s[startPtr];
+            char charAtEndPtr = s[endPtr];
+
+            bool isCharAtStartPtrAlphanumeric = isAlphanumeric(charAtStartPtr);
+            bool isCharAtEndtPtrAlphanumeric = isAlphanumeric(charAtEndPtr);
+            
+            if(isCharAtStartPtrAlphanumeric and isCharAtEndtPtrAlphanumeric)
+            {
+                if(tolower(charAtStartPtr) != tolower(charAtEndPtr))
+                    return false;
+                startPtr+=1;
+                endPtr-=1;
+            }
+            else if(!isCharAtStartPtrAlphanumeric)
+            {
+                startPtr += 1;
+            }
+            else
+            {
+                endPtr -= 1;
+            }
+        }
+        return true;
+    }
+};
