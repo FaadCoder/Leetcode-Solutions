8c5b21bd8a51eb9fa06ce481f3bb34201ba7e2ba
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-06-22 09:23:36 +0530

    Time: 110 ms (70.43%), Space: 20 MB (68.38%) - LeetHub

---
+++ 15-3sum/15-3sum.cpp
@@ -0,0 +1,50 @@
+class Solution
+{
+    public:
+        vector<vector < int>> threeSum(vector<int> &nums)
+        {
+            sort(nums.begin(), nums.end());
+            vector<vector < int>> triplets;
+
+            if (nums.empty() or nums.size() <= 2)
+                return triplets;
+
+            int idx = 0;
+            while (idx < nums.size() - 1)
+            {
+                int startPtr = idx + 1;
+                int endPtr = nums.size() - 1;
+                int target = -nums[idx];
+                while (startPtr < endPtr)
+                {
+                    int currentSum = nums[startPtr] + nums[endPtr];
+                    if (currentSum == target)
+                    {
+                        triplets.push_back({ nums[idx],
+                            nums[startPtr],
+                            nums[endPtr] });
+                        while(startPtr < endPtr and nums[startPtr+1]==nums[startPtr])
+                            startPtr += 1;
+                        startPtr += 1;
+                        while(startPtr < endPtr and nums[endPtr-1]==nums[endPtr])
+                            endPtr -= 1;
+                        endPtr -= 1;
+                    }
+                    else if (currentSum < target)
+                        startPtr += 1;
+                    else
+                        endPtr -= 1;
+                }
+                while ((idx < nums.size()-1) and (nums[idx + 1] == nums[idx]))
+                {
+                    idx++;
+                }
+
+                idx++;
+            }
+
+            return triplets;
+        }
+};
+
+//  -2 0 0 2 2
