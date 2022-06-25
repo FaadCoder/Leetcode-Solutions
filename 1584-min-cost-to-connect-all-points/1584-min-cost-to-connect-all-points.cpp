ce6076810eb83a4dfb7fbddd4ff3435b2c3bc95e
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-06-25 22:26:35 +0530

    Time: 1792 ms (5.20%), Space: 58.3 MB (39.11%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,12 +1,12 @@
 class DSU
 {
-private:
+    private:
         vector<int> par, size;
-public:
+    public:
         DSU(int n)
         {
-            par.resize(n+1);
-            size.resize(n+1);
+            par.resize(n + 1);
+            size.resize(n + 1);
             for (int i = 0; i <= n; i++)
                 par[i] = i, size[i] = 1;
         }
@@ -33,44 +33,47 @@ public:
     }
 };
 
-class Pair{
- public:
-    
-    int distance;
+class Pair
+{
+    public:
+
+        int distance;
     int from;
     int to;
-    
-    Pair(int _dist,int _from,int _to)
+
+    Pair(int _dist, int _from, int _to)
     {
         distance = _dist;
         from = _from;
         to = _to;
     }
-    
 };
 
-struct compare{
-  bool operator()(Pair &pair1, Pair &pair2){
-      return pair1.distance > pair2.distance;
-  };
+struct compare
+{
+    bool operator()(Pair &pair1, Pair &pair2)
+    {
+        return pair1.distance > pair2.distance;
+    };
 };
 
 class Solution
 {
     int numberOfPoints;
-    priority_queue<Pair,vector<Pair>,compare> pq;
-    
-    int manhattanDistance(vector<int> &point1,vector<int> &point2)
+    priority_queue<Pair, vector < Pair>, compare> pq;
+
+    int manhattanDistance(vector<int> &point1, vector<int> &point2)
     {
-        return abs(point1[0]-point2[0]) + abs(point1[1]-point2[1]);
+        return abs(point1[0] - point2[0]) + abs(point1[1] - point2[1]);
     }
-    
-    void formGraph(vector<vector <int>> &points)
+
+    void formGraph(vector<vector < int>> &points)
     {
         for (int idx1 = 0; idx1 < points.size(); idx1++)
         {
-            for (int idx2 = idx1+1; idx2 < points.size(); idx2++) {
-                pq.push(Pair(manhattanDistance(points[idx1],points[idx2]),idx1,idx2));
+            for (int idx2 = idx1 + 1; idx2 < points.size(); idx2++)
+            {
+                pq.push(Pair(manhattanDistance(points[idx1], points[idx2]), idx1, idx2));
             }
         }
     }
@@ -78,24 +81,23 @@ class Solution
     int getMinCost()
     {
         DSU dsu(numberOfPoints);
-        
+
         int minCost = 0;
-        
-        while(!pq.empty())
+
+        while (!pq.empty())
         {
             Pair top = pq.top();
             pq.pop();
             int cost = top.distance;
             int from = top.from;
             int to = top.to;
-            if(dsu.unionSets(from,to))
-                minCost+=cost;
-            
+            if (dsu.unionSets(from, to))
+                minCost += cost;
         }
-            
+
         return minCost;
     }
-    
+
     public:
         int minCostConnectPoints(vector<vector < int>> &points)
         {
