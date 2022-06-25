4e3c45c63851a1647b2ff0ac33e2791221e4ac77
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-06-25 15:47:47 +0530

    Time: 130 ms (51.00%), Space: 41.4 MB (60.38%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -1,20 +1,22 @@
-class Solution {
-public:
-    int subarraySum(vector<int>& nums, int k) {
-        
-        unordered_map<int,int> hashMap;
-        int currentSum = 0;
-        int subarrayCount = 0;
-        hashMap[currentSum] = 1;
-        
-        for(int num: nums)
+class Solution
+{
+    public:
+        int subarraySum(vector<int> &nums, int k)
         {
-            currentSum += num;
-            if(hashMap.count(currentSum - k))
-                subarrayCount += hashMap[currentSum-k];
-            hashMap[currentSum] += 1;
+
+            unordered_map<int, int> hashMap;
+            int currentSum = 0;
+            int subarrayCount = 0;
+            hashMap[currentSum] = 1;
+
+            for (int num: nums)
+            {
+                currentSum += num;
+                if (hashMap.count(currentSum - k))
+                    subarrayCount += hashMap[currentSum - k];
+                hashMap[currentSum] += 1;
+            }
+
+            return subarrayCount;
         }
-        
-        return subarrayCount;
-    }
 };
