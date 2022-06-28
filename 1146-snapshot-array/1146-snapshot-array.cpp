736f98b8c3e2b4c18d517538eea4b66a7e520ee8
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-06-28 12:47:13 +0530

    Time: 627 ms (7.34%), Space: 124.1 MB (8.88%) - LeetHub

--- 1146-snapshot-array/1146-snapshot-array.cpp
+++ 1146-snapshot-array/1146-snapshot-array.cpp
@@ -1,6 +1,6 @@
 class SnapshotArray {
     int snapCount = 0;
-    map<int,map<int,int>> snapToIdxToElementMap;
+    unordered_map<int,map<int,int>> snapToIdxToElementMap;
 public:
     SnapshotArray(int length) {
         for (int i = 0; i < length; i++) {
@@ -18,7 +18,10 @@ public:
     }
     
     int get(int index, int snap_id) {
-        auto it = snapToIdxToElementMap[index].upper_bound(snap_id); it--;
+        auto it = snapToIdxToElementMap[index].upper_bound(snap_id); 
+        if(it==snapToIdxToElementMap[index].begin())
+            return 0;
+        it--;
         return it->second;
     }
 };
