342f330b49db3e8237e0f99080a69ef51ef5e66e
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-06-28 13:12:23 +0530

    Time: 11 ms (82.70%), Space: 11.9 MB (88.61%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
@@ -1,29 +1,57 @@
-class Solution {
-   unordered_map<string,function<int (int,int)> > hashMap = {
-       {"+",[](int a,int b){ return a+b;}},
-       {"-",[](int a,int b){ return a-b;}},
-       {"*",[](int a,int b){ return a*b;}},
-       {"/",[](int a,int b){ return a/b;}},
-       
-   };
-    
-public:
-    int evalRPN(vector<string>& tokens) {
-        stack<int> q;
-        for(string &token:tokens)
+class Solution
+{
+    unordered_map< string,
+        function < int(int, int) > > hashMap = {
+		{
+                "+",
+               [](int a, int b)
+                {
+                    return a + b;
+                }
+            },
+            {
+                "-",
+               [](int a, int b)
+                {
+                    return a - b;
+                }
+            },
+            {
+                "*",
+               [](int a, int b)
+                {
+                    return a * b;
+                }
+            },
+            {
+                "/",
+               [](int a, int b)
+                {
+                    return a / b;
+                }
+            },
+        };
+
+    public:
+        int evalRPN(vector<string> &tokens)
         {
-            const auto &it = hashMap.find(token);
-            if(it!=hashMap.end())
+            stack<int> q;
+            for (string &token: tokens)
             {
-                int rhs = q.top();q.pop();
-                int lhs = q.top();q.pop();
-                q.push(it->second(lhs,rhs));
+                const auto &it = hashMap.find(token);
+                if (it != hashMap.end())
+                {
+                    int rhs = q.top();
+                    q.pop();
+                    int lhs = q.top();
+                    q.pop();
+                    q.push(it->second(lhs, rhs));
+                }
+                else
+                {
+                    q.push(stoi(token));
+                }
             }
-            else
-            {
-                q.push(stoi(token));
-            }
+            return q.top();
         }
-        return q.top();
-    }
 };
