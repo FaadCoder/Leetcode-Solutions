8bc4607898852449c9ececfcfe27ed0db0c3349c
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-06-28 14:42:56 +0530

    Time: 769 ms (49.03%), Space: 167.1 MB (61.64%) - LeetHub

--- 2034-stock-price-fluctuation/2034-stock-price-fluctuation.cpp
+++ 2034-stock-price-fluctuation/2034-stock-price-fluctuation.cpp
@@ -1,40 +1,47 @@
-class StockPrice {
-    unordered_map<int,int> hashMap;
+class StockPrice
+{
+    unordered_map<int, int> timeStampToPriceHashMap;
     multiset<int> priceSet;
     int latestTimestamp = 0;
-public:
-    StockPrice() {
-        hashMap.clear();
-        priceSet.clear();
-        latestTimestamp = 0;
-    }
-    
-    void update(int timestamp, int price) {
-        if(hashMap.count(timestamp))
-            priceSet.erase(priceSet.find(hashMap[timestamp]));
-        hashMap[timestamp] = price;
-        latestTimestamp = max(latestTimestamp,timestamp);
+    public:
+        StockPrice()
+        {
+            timeStampToPriceHashMap.clear();
+            priceSet.clear();
+            latestTimestamp = 0;
+        }
+
+    void update(int timestamp, int price)
+    {
+        if (timeStampToPriceHashMap.count(timestamp))
+            priceSet.erase(priceSet.find(timeStampToPriceHashMap[timestamp]));
+        
+        timeStampToPriceHashMap[timestamp] = price;
+        latestTimestamp = max(latestTimestamp, timestamp);
         priceSet.insert(price);
     }
-    
-    int current() {
-        return hashMap[latestTimestamp];
+
+    int current()
+    {
+        return timeStampToPriceHashMap[latestTimestamp];
     }
-    
-    int maximum() {
+
+    int maximum()
+    {
         return *priceSet.rbegin();
     }
-    
-    int minimum() {
+
+    int minimum()
+    {
         return *priceSet.begin();
     }
 };
 
 /**
- * Your StockPrice object will be instantiated and called as such:
- * StockPrice* obj = new StockPrice();
- * obj->update(timestamp,price);
- * int param_2 = obj->current();
- * int param_3 = obj->maximum();
- * int param_4 = obj->minimum();
+ *Your StockPrice object will be instantiated and called as such:
+ *StockPrice* obj = new StockPrice();
+ *obj->update(timestamp,price);
+ *int param_2 = obj->current();
+ *int param_3 = obj->maximum();
+ *int param_4 = obj->minimum();
  */
