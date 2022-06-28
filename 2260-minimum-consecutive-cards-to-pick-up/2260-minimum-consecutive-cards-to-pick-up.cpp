6fb6f6c59a76f3fd96b43b4feded14b4d24832d7
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-06-28 09:24:30 +0530

    Time: 446 ms (68.04%), Space: 115.1 MB (89.06%) - LeetHub

--- 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.cpp
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.cpp
@@ -1,25 +1,22 @@
-class Solution {
-public:
-    int minimumCardPickup(vector<int>& cards) 
+class Solution
+{
+    int getDistance(int idx1,int idx2)
     {
-        unordered_map<int,vector<int>> posMap;
-        
-        for(int idx=0; idx<cards.size(); idx++)
-        {
-            posMap[cards[idx]].push_back(idx);
-        }
-        
-        int minDis = INT_MAX;
-        
-        for(auto keyValue : posMap)
-        {
-            vector<int> value = keyValue.second;
-            for(int idx = 0; idx<value.size()-1; idx++)
-            {
-                minDis = min(minDis,value[idx+1] - value[idx] + 1);
-            }
-        }
-        
-        return minDis==INT_MAX?-1:minDis;
+        return abs(idx2-idx1) + 1;
     }
+    
+    public:
+        int minimumCardPickup(vector<int> &cards)
+        {
+            unordered_map<int, int> posMap;
+            int minDist = INT_MAX;
+            for (int idx = 0; idx < cards.size(); idx++)
+            {
+                if(posMap.count(cards[idx]))
+                    minDist = min(minDist,getDistance(idx,posMap[cards[idx]]));
+                posMap[cards[idx]] = idx;
+            }
+
+            return minDist == INT_MAX ? -1 : minDist;
+        }
 };
