70362cc87d3b85ac599a1ca4b6e1a23a99d8c0b3
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-06-29 15:09:17 +0530

    Time: 236 ms (78.71%), Space: 59.9 MB (41.35%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,26 +1,25 @@
 struct Node
 {
     public: int idx;
-    const string& word;
-    Node(const string &_word, int _idx) : word(_word),idx(_idx)
-    {
-    }
+    const string &word;
+    Node(const string &_word, int _idx): word(_word),
+    idx(_idx) {}
 };
 class Solution
 {
     public:
         int numMatchingSubseq(string s, vector<string> &words)
         {
-            unordered_map<char, vector < Node>> mp;
+            vector<Node> mp[26];
             int ans = 0;
 
             for (string &word: words)
-                mp[word[0]].push_back(Node(word, 0));
+                mp[word[0] - 'a'].push_back(Node(word, 0));
 
             for (char &ch: s)
             {
-                vector<Node> values = mp[ch];
-                mp[ch].clear();
+                vector<Node> values = mp[ch - 'a'];
+                mp[ch - 'a'].clear();
 
                 for (Node &value: values)
                 {
@@ -28,7 +27,7 @@ class Solution
                     if (value.idx == value.word.size())
                         ans += 1;
                     else
-                        mp[value.word[value.idx]].push_back(value);
+                        mp[value.word[value.idx] - 'a'].push_back(value);
                 }
             }
 
