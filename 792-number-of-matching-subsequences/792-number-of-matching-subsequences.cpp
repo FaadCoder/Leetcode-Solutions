3e2370653e0980350657a06542c8ab106e53379f
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-06-29 15:07:03 +0530

    Time: 1093 ms (11.34%), Space: 257.4 MB (25.11%) - LeetHub

---
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -0,0 +1,39 @@
+struct Node
+{
+    public: int idx;
+    string word;
+    Node(string _word, int _idx)
+    {
+        word = _word;
+        idx = _idx;
+    }
+};
+class Solution
+{
+    public:
+        int numMatchingSubseq(string s, vector<string> &words)
+        {
+            unordered_map<char, vector < Node>> mp;
+            int ans = 0;
+
+            for (string &word: words)
+                mp[word[0]].push_back(Node(word, 0));
+
+            for (char &ch: s)
+            {
+                vector<Node> values = mp[ch];
+                mp[ch].clear();
+
+                for (Node &value: values)
+                {
+                    value.idx += 1;
+                    if (value.idx == value.word.size())
+                        ans += 1;
+                    else
+                        mp[value.word[value.idx]].push_back(value);
+                }
+            }
+
+            return ans;
+        }
+};
