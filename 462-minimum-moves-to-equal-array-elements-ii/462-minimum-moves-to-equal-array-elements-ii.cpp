417708a22765e4a53aca65e40ae7f75a6a28191a
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-06-30 10:04:09 +0530

    Time: 23 ms (20.71%), Space: 10.7 MB (92.09%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,13 +1,13 @@
-class Solution {
-public:
-    int minMoves2(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int mid = nums[nums.size()/2];
-        int totalMinMoves = 0;
-        for(int num:nums)
+class Solution
+{
+    public:
+        int minMoves2(vector<int> &nums)
         {
-            totalMinMoves += abs(mid - num);
+            sort(nums.begin(), nums.end());
+            int mid = nums[nums.size() / 2];
+            int totalMinMoves = 0;
+            for (int num: nums)
+                totalMinMoves += abs(mid - num);
+            return totalMinMoves;
         }
-        return totalMinMoves;
-    }
 };
