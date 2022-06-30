4fb31fc944a3eb1e7fd0f69066935d2d8c88fb46
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-06-30 13:23:18 +0530

    Time: 306 ms (88.28%), Space: 117.6 MB (72.77%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,34 +1,38 @@
-class Solution {
-public:
-    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-        vector<int> adj[n];
-        int headId = 0;
-        for(int employeeId = 0; employeeId<manager.size(); employeeId++)
+class Solution
+{
+    public:
+        int numOfMinutes(int n, int headID, vector<int> &manager, vector<int> &informTime)
         {
-            if(manager[employeeId]==-1)
-                headId = employeeId;
-            else
-                adj[manager[employeeId]].push_back(employeeId);
-        }
-        
-        int maxTimeToReachLeaf = 0;
-        
-        queue<pair<int,int>> bfsQueue;
-        bfsQueue.push({headId,0});
-        while(!bfsQueue.empty())
-        {
-            pair<int,int> top = bfsQueue.front();
-            bfsQueue.pop();
-            int currentManagerId = top.first;
-            int timePassed = top.second;
-            maxTimeToReachLeaf = max(maxTimeToReachLeaf,timePassed);
-            
-            for(int employeeId:adj[currentManagerId])
+            vector<int> adj[n];
+            int headId = 0;
+            for (int employeeId = 0; employeeId < manager.size(); employeeId++)
             {
-                bfsQueue.push({employeeId,timePassed + informTime[currentManagerId]});
+                if (manager[employeeId] == -1)
+                    headId = employeeId;
+                else
+                    adj[manager[employeeId]].push_back(employeeId);
             }
+
+            int maxTimeToReachLeaf = 0;
+
+            queue<pair<int, int>> bfsQueue;
+            bfsQueue.push({ headId,
+                0 });
+            while (!bfsQueue.empty())
+            {
+                pair<int, int> top = bfsQueue.front();
+                bfsQueue.pop();
+                int currentManagerId = top.first;
+                int timePassed = top.second;
+                maxTimeToReachLeaf = max(maxTimeToReachLeaf, timePassed);
+
+                for (int employeeId: adj[currentManagerId])
+                {
+                    bfsQueue.push({ employeeId,
+                        timePassed + informTime[currentManagerId] });
+                }
+            }
+
+            return maxTimeToReachLeaf;
         }
-        
-        return maxTimeToReachLeaf;
-    }
 };
