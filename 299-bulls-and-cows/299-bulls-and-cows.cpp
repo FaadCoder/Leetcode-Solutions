b9d9700360ddb57f041147a2ceeaf0c54458c5ed
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-06-30 11:08:11 +0530

    Time: 6 ms (61.15%), Space: 6.6 MB (75.83%) - LeetHub

--- 299-bulls-and-cows/299-bulls-and-cows.cpp
+++ 299-bulls-and-cows/299-bulls-and-cows.cpp
@@ -1,43 +1,44 @@
-class Solution {
-public:
-    string getHint(string secret, string guess) {
-        int hash[10] = {0};
-        
-        for(char &ch:secret)
-            hash[ch-'0']++;
-        
-        int bulls = 0;
-        int cows = 0;
-        
-        // step1 : calculate bulls
-        for(int idx = 0; idx<guess.length(); idx++)
+class Solution
+{
+    public:
+        string getHint(string secret, string guess)
         {
-            char digitInGuess = guess[idx];
-            char digitInSecret = secret[idx];
-            
-            if(digitInGuess == digitInSecret)
+            int hash[10] = { 0 };
+
+            for (char &ch: secret)
+                hash[ch - '0']++;
+
+            int bulls = 0;
+            int cows = 0;
+
+           	// step1 : calculate bulls
+            for (int idx = 0; idx < guess.length(); idx++)
             {
-                bulls++;
-                hash[digitInGuess - '0']--;
+                char digitInGuess = guess[idx];
+                char digitInSecret = secret[idx];
+
+                if (digitInGuess == digitInSecret)
+                {
+                    bulls++;
+                    hash[digitInGuess - '0']--;
+                }
             }
-        }
-        
-        // step2: calculate cows
-        for(int idx = 0; idx<guess.length(); idx++)
-        {
-            char digitInGuess = guess[idx];
-            char digitInSecret = secret[idx];
-            
-            if(digitInGuess != digitInSecret and hash[digitInGuess-'0']>0)
+
+           	// step2: calculate cows
+            for (int idx = 0; idx < guess.length(); idx++)
             {
-                cows++;
-                hash[digitInGuess - '0']--;
+                char digitInGuess = guess[idx];
+                char digitInSecret = secret[idx];
+
+                if (digitInGuess != digitInSecret and hash[digitInGuess - '0'] > 0)
+                {
+                    cows++;
+                    hash[digitInGuess - '0']--;
+                }
             }
+
+            return to_string(bulls) + "A" + to_string(cows) + "B";
         }
-        
-        
-        return to_string(bulls)+"A"+to_string(cows)+"B";
-    }
 };
 // Bulls : digits are in correct positions.
 // Cows : digits are in wrong positions.
