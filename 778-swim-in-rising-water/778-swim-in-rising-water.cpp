f15d785b72c40bc779dddf86f3983ffa81a6a209
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-01 21:36:24 +0530

    Time: 77 ms (22.08%), Space: 10.9 MB (42.29%) - LeetHub

--- 778-swim-in-rising-water/778-swim-in-rising-water.cpp
+++ 778-swim-in-rising-water/778-swim-in-rising-water.cpp
@@ -1,118 +1,73 @@
-class Pair {
-  int time;
-  int row;
-  int col;
-  public:
-    Pair(int _time, int _row, int _col) {
-      time = _time;
-      row = _row;
-      col = _col;
-    }
-
-  int getTime() {
-    return time;
-  }
-
-  int getRow() {
-    return row;
-  }
-
-  int getCol() {
-    return col;
-  }
-
-  void setTime(int _time) {
-    time = _time;
-  }
-
-  void setRow(int _row) {
-    row = _row;
-  }
-
-  void setCol(int _col) {
-    col = _col;
-  }
-
-};
-
-struct compare {
-  bool operator()(Pair & a, Pair & b) {
-    return a.getTime() > b.getTime();
-  }
-};
-
-class Solution {
-  int rows, cols;
-
-  int directions[4][2] = {
-    {
-      1,
-      0
-    },
-    {
-      0,
-      1
-    },
-    {
-      -1,
-      0
-    },
-    {
-      0,
-      -1
-    }
-  };
-
-  int swimInWaterHelper(vector < vector < int >> & grid) {
-    priority_queue < Pair, vector < Pair > , compare > pq;
-    set < pair < int, int >> vis;
-
-    auto isValid = [ & ](int row, int col) {
-      return (row >= 0 and row < rows and col >= 0 and col < cols and!vis.count({
-        row,
-        col
-      }));
+class Solution
+{
+    int rows, cols;
+    int dirs[4][2] = {
+		{ 1,
+            0 },
+        { 0,
+            1 },
+        { -1,
+            0 },
+        { 0,
+            -1 }
     };
 
-    vis.insert({
-      0,
-      0
-    });
-    pq.push(Pair(grid[0][0], 0, 0));
+    int getMinimumTimeToReachBottom(vector<vector < int>> &grid)
+    {
+        if (rows == 0 or cols == 0)
+            return -1;
 
-    while (!pq.empty()) {
-      Pair queueTop = pq.top();
-      pq.pop();
+        bool visited[rows][cols];
+        memset(visited, false, sizeof(visited));
 
-      int currentTime = queueTop.getTime();
-      int currentRow = queueTop.getRow();
-      int currentCol = queueTop.getCol();
+        priority_queue<vector < int>, vector< vector< int >>, greater<vector< int>>> minHeap;
+        visited[0][0] = true;
 
-      if (currentRow == rows - 1 and currentCol == cols - 1)
-        return currentTime;
+        auto isValid =[& ](int row, int col)
+        {
+            return row >= 0 and row < rows and col >= 0 and col < cols and!visited[row][col];
+        };
 
-      for (int dir = 0; dir < 4; dir++) {
-        int newRow = currentRow + directions[dir][0];
-        int newCol = currentCol + directions[dir][1];
+        minHeap.push({ grid[0][0],
+            0,
+            0 });
 
-        if (isValid(newRow, newCol)) {
-          vis.insert({
-            newRow,
-            newCol
-          });
-          pq.push(Pair(max(grid[newRow][newCol], currentTime), newRow, newCol));
+        int minTime = grid[0][0];
+
+        while (!minHeap.empty())
+        {
+            auto top = minHeap.top();
+            minHeap.pop();
+            int currentTime = top[0];
+            int row = top[1];
+            int col = top[2];
+            minTime = max(minTime, currentTime);
+
+            if (row == rows - 1 and col == cols - 1)
+                return minTime;
+
+            for (int dir = 0; dir < 4; dir++)
+            {
+                int newRow = row + dirs[dir][0];
+                int newCol = col + dirs[dir][1];
+                if (isValid(newRow, newCol))
+                {
+                    visited[newRow][newCol] = true;
+                    minHeap.push({ grid[newRow][newCol],
+                        newRow,
+                        newCol });
+                }
+            }
         }
-      }
+
+        return -1;
     }
 
-    return -1;
-  }
-
-  public:
-    int swimInWater(vector < vector < int >> & grid) {
-      rows = grid.size();
-      cols = grid[0].size();
-
-      return swimInWaterHelper(grid);
-    }
+    public:
+        int swimInWater(vector<vector < int>> &grid)
+        {
+            rows = grid.size();
+            cols = grid[0].size();
+            return getMinimumTimeToReachBottom(grid);
+        }
 };
