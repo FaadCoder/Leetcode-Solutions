152569a830c75d1e8160972943f0e8beb30905b2
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-01 16:40:03 +0530

    Time: 66 ms (5.85%), Space: 22 MB (5.57%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -1,72 +1,73 @@
-struct TrieNode{
-    unordered_map<char,TrieNode *> children;
+struct TrieNode
+{
+    unordered_map<char, TrieNode*> children;
     string word;
     TrieNode()
     {
         children.clear();
         word = "";
     }
-    
 };
 
-class Trie{
-public:
-    void addWord(TrieNode *root,const string &word)
-    {
-        for(const char &ch:word)
+class Trie
+{
+    public:
+        void addWord(TrieNode *root, const string &word)
         {
-            if(!root->children.count(ch))
-                root->children[ch] = new TrieNode();
-            root = root->children[ch];
+            for (const char &ch: word)
+            {
+                if (!root->children.count(ch))
+                    root->children[ch] = new TrieNode();
+                root = root->children[ch];
+            }
+            root->word = word;
         }
-        root->word = word;
-    }
-    
-    void populateWordsInLexicographicalOrder(TrieNode *root,vector<string> &res,int k)
+
+    void populateWordsInLexicographicalOrder(TrieNode *root, vector<string> &res, int k)
     {
-        if(!root)
+        if (!root)
             return;
-        
-        if(!root->word.empty())
+
+        if (!root->word.empty())
             res.push_back(root->word);
-        
-        if(res.size() == k)
+
+        if (res.size() == k)
             return;
-        
-        for(char ch = 'a'; ch<='z'; ch++)
+
+        for (char ch = 'a'; ch <= 'z'; ch++)
         {
-            if(root->children.count(ch))
-                populateWordsInLexicographicalOrder(root->children[ch],res,k);
+            if (root->children.count(ch))
+                populateWordsInLexicographicalOrder(root->children[ch], res, k);
         }
     }
 };
 
-class Solution {
-public:
-    vector<string> topKFrequent(vector<string>& words, int k)
-    {
-        Trie trie;
-        vector<string> topKFrequentWords;
-        unordered_map<string,int> frequencyMap;
-        vector<TrieNode *> frequencyBuckets;
-        
-        for(string &word:words)
-            frequencyMap[word]++;
-        
-        for(int i=0;i<=words.size();i++)
-            frequencyBuckets.push_back(new TrieNode());
-        
-        for(auto &it:frequencyMap)
-            trie.addWord(frequencyBuckets[it.second],it.first);
-
-        
-        for(int i = words.size(); i>=1; i--)
+class Solution
+{
+    public:
+        vector<string> topKFrequent(vector<string> &words, int k)
         {
-            trie.populateWordsInLexicographicalOrder(frequencyBuckets[i],topKFrequentWords,k);
-            if(topKFrequentWords.size() == k)
-                break;
+            Trie trie;
+            vector<string> topKFrequentWords;
+            unordered_map<string, int> frequencyMap;
+            vector<TrieNode*> frequencyBuckets;
+
+            for (string &word: words)
+                frequencyMap[word]++;
+
+            for (int i = 0; i <= words.size(); i++)
+                frequencyBuckets.push_back(new TrieNode());
+
+            for (auto &it: frequencyMap)
+                trie.addWord(frequencyBuckets[it.second], it.first);
+
+            for (int i = words.size(); i >= 1; i--)
+            {
+                trie.populateWordsInLexicographicalOrder(frequencyBuckets[i], topKFrequentWords, k);
+                if (topKFrequentWords.size() == k)
+                    break;
+            }
+
+            return topKFrequentWords;
         }
-        
-        return topKFrequentWords;
-    }
 };
