5d7c54cd34a21f95c1e0742f9204356c9df29984
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-01 10:02:41 +0530

    Time: 55 ms (73.26%), Space: 15.9 MB (77.60%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.cpp
@@ -1,27 +1,29 @@
-class Solution {
-public:
-    int maximumUnits(vector<vector<int>>& boxTypes, int truckSize) 
-    {
-        auto comparatorFunction = [&](vector<int> &boxType1, vector<int> &boxType2){
-            int numberOfUnitsPerBoxOfType1 = boxType1[1];
-            int numberOfUnitsPerBoxOfType2 = boxType2[1];
-            
-            return  (numberOfUnitsPerBoxOfType1 > numberOfUnitsPerBoxOfType2);
-        };
-        
-        sort(boxTypes.begin(),boxTypes.end(),comparatorFunction);
-        
-        int maxUnits = 0;
-        
-        for(vector<int> &boxType:boxTypes)
+class Solution
+{
+    public:
+        int maximumUnits(vector<vector < int>> &boxTypes, int truckSize)
         {
-            int boxesToTake = min(truckSize,boxType[0]);
-            if(boxesToTake <= 0)
-                break;
-            maxUnits += boxesToTake * boxType[1];
-            truckSize -= boxesToTake;
+            auto comparatorFunction =[& ](vector<int> &boxType1, vector<int> &boxType2)
+            {
+                int numberOfUnitsPerBoxOfType1 = boxType1[1];
+                int numberOfUnitsPerBoxOfType2 = boxType2[1];
+
+                return (numberOfUnitsPerBoxOfType1 > numberOfUnitsPerBoxOfType2);
+            };
+
+            sort(boxTypes.begin(), boxTypes.end(), comparatorFunction);
+
+            int maxUnits = 0;
+
+            for (vector<int> &boxType: boxTypes)
+            {
+                int boxesToTake = min(truckSize, boxType[0]);
+                if (boxesToTake <= 0)
+                    break;
+                maxUnits += boxesToTake *boxType[1];
+                truckSize -= boxesToTake;
+            }
+
+            return maxUnits;
         }
-        
-        return maxUnits;
-    }
 };
