8242bc2383b3c784b22bb7dd8b5d2d152aa710cb
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-01 16:53:20 +0530

    Time: 7 ms (24.68%), Space: 8.4 MB (50.16%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.cpp
+++ 399-evaluate-division/399-evaluate-division.cpp
@@ -1,54 +1,60 @@
-class Solution {
-    unordered_map<string,vector<pair<string,double>>> graph;
-    
-    double evaluateDivision(string &source,string &destination)
+class Solution
+{
+    unordered_map<string, vector<pair<string, double>>> graph;
+
+    double evaluateDivision(string &source, string &destination)
     {
-        if(!graph.count(source) or !graph.count(destination))
+        if (!graph.count(source) or!graph.count(destination))
             return -1.0;
-        
+
         unordered_set<string> vis;
-        queue<pair<string,double>> q;
-        q.push({source,1.0});
+        queue<pair<string, double>> q;
+        q.push({ source,
+            1.0 });
         vis.insert(source);
-        
-        while(!q.empty())
+
+        while (!q.empty())
         {
             auto front = q.front();
             q.pop();
             string currentNode = front.first;
             double currentValue = front.second;
-            if(currentNode == destination)
+            if (currentNode == destination)
                 return currentValue;
-            
-            for(auto child:graph[currentNode])
+
+            for (auto child: graph[currentNode])
             {
-                if(!vis.count(child.first))
+                if (!vis.count(child.first))
                 {
                     vis.insert(child.first);
-                    q.push({child.first,currentValue * child.second});
+                    q.push({ child.first,
+                        currentValue *child.second });
                 }
             }
         }
         return -1.0;
     }
-    
-public:
-    vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
-        graph.clear();
-        
-        for(int idx = 0; idx<equations.size(); idx++)
+
+    public:
+        vector<double> calcEquation(vector<vector < string>> &equations, vector< double > &values, vector< vector< string>> &queries)
         {
-            graph[equations[idx][0]].push_back({equations[idx][1],values[idx]});
-            graph[equations[idx][1]].push_back({equations[idx][0],1.0/values[idx]});
+            graph.clear();
+
+            for (int idx = 0; idx < equations.size(); idx++)
+            {
+                graph[equations[idx][0]].push_back({ equations[idx][1],
+                    values[idx] });
+                graph[equations[idx][1]].push_back({ equations[idx][0],
+                    1.0 / values[idx] });
+            }
+
+            vector<double> ans;
+
+            for (auto query: queries)
+            {
+                ans.push_back(evaluateDivision(query[0], query[1]));
+            }
+
+            return ans;
         }
-        
-        vector<double> ans;
-        
-        for(auto query:queries)
-        {
-            ans.push_back(evaluateDivision(query[0],query[1]));    
-        }
-        
-        return ans;
-    }
 };
