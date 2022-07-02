bfe7a7b5e10d44a535e157d2d452dac57a5858b7
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-02 20:11:39 +0530

    Time: 49 ms (59.38%), Space: 11.7 MB (41.59%) - LeetHub

--- 759-employee-free-time/759-employee-free-time.cpp
+++ 759-employee-free-time/759-employee-free-time.cpp
@@ -14,60 +14,58 @@ public:
 };
 */
 
-class Solution {
-public:
-    vector<Interval> employeeFreeTime(vector<vector<Interval>> schedule) {
-        map<int,int> lineSweep;
-        for(vector<Interval> &intervals:schedule)
+class Solution
+{
+    public:
+        vector<Interval> employeeFreeTime(vector<vector < Interval>> schedule)
         {
-            for(Interval &interval:intervals)
+            map<int, int> lineSweep;
+            for (vector<Interval> &intervals: schedule)
             {
-                lineSweep[interval.start]++;
-                lineSweep[interval.end]--;
-            }
-        }
-        vector<Interval> finiteIntervals;
-        int currentOverlappingMeetings = 0;
-        int st = 0, en =0;
-        auto it = lineSweep.begin();
-        while(it!=lineSweep.end())
-        {
-            currentOverlappingMeetings += it->second;
-            if(currentOverlappingMeetings == 0)
-            {
-                st = it->first;
-                it++;
-                while(it!=lineSweep.end())
+                for (Interval &interval: intervals)
                 {
-                    currentOverlappingMeetings += it->second;
-                    if(currentOverlappingMeetings>0)
-                        break;
+                    lineSweep[interval.start]++;
+                    lineSweep[interval.end]--;
+                }
+            }
+            vector<Interval> finiteIntervals;
+            int currentOverlappingMeetings = 0;
+            int st = 0, en = 0;
+            auto it = lineSweep.begin();
+            while (it != lineSweep.end())
+            {
+                currentOverlappingMeetings += it->second;
+                if (currentOverlappingMeetings == 0)
+                {
+                    st = it->first;
                     it++;
+                    while (it != lineSweep.end())
+                    {
+                        currentOverlappingMeetings += it->second;
+                        if (currentOverlappingMeetings > 0)
+                            break;
+                        it++;
+                    }
+                    if (currentOverlappingMeetings != 0)
+                    {
+                        en = it->first;
+                        if (en >= st)
+                            finiteIntervals.push_back(Interval(st, en));
+                    }
                 }
-                if(currentOverlappingMeetings != 0)
+                else
                 {
-                    en = it->first;
-                    if(en>=st)
-                    finiteIntervals.push_back(Interval(st,en));
+                    st = 0;
+                    en = 0;
                 }
-                
+                it++;
             }
-            else
-            {
-                st = 0;
-                en = 0;
-            }
-            it++;
+
+            return finiteIntervals;
         }
-        
-        return finiteIntervals;
-    }
 };
 
 // 1    2 3  4  5   6  7 8 9 10
 // 1----2       5---6
 // 1------3 
 //            4----------10
-    
-    
-    
