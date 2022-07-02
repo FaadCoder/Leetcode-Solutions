55d1b88a3517b8c440233675472d57c606150d5a
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-02 22:36:07 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (93.69%) - LeetHub

--- 818-race-car/818-race-car.cpp
+++ 818-race-car/818-race-car.cpp
@@ -1,25 +1,25 @@
-class Solution {
+class Solution
+{
     int dp[10002];
-public:
-    int racecar(int target) 
-    {
-        if(dp[target] > 0)
-            return dp[target];
-        
-        int steps = ceil(log2(target+1));
-        if(target+1 == (1<<steps))
-            dp[target] = steps;
-        else
+    public:
+        int racecar(int target)
         {
-            dp[target] = steps + 1 + racecar((1<<steps) - 1 - target);
-            for(int backwardSteps = 0;backwardSteps < steps - 1;backwardSteps++)
+            if (dp[target] > 0)
+                return dp[target];
+
+            int steps = ceil(log2(target + 1));
+            if (target + 1 == (1 << steps))
+                dp[target] = steps;
+            else
             {
-                
-                dp[target] = min(dp[target],
-                                 steps+backwardSteps+1+racecar(target-(1<<steps-1)+(1<<backwardSteps)));
+                dp[target] = steps + 1 + racecar((1 << steps) - 1 - target);
+                for (int backwardSteps = 0; backwardSteps < steps - 1; backwardSteps++)
+                {
+
+                    dp[target] = min(dp[target],
+                        steps + backwardSteps + 1 + racecar(target - (1 << steps - 1) + (1 << backwardSteps)));
+                }
             }
-            
+            return dp[target];
         }
-        return dp[target];
-    }
 };
