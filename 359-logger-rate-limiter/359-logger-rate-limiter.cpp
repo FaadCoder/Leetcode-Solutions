70a40fb12304d1273c4cd4b3875c8de9bc57468b
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-02 20:31:05 +0530

    Time: 80 ms (88.36%), Space: 32.6 MB (26.54%) - LeetHub

--- 359-logger-rate-limiter/359-logger-rate-limiter.cpp
+++ 359-logger-rate-limiter/359-logger-rate-limiter.cpp
@@ -1,14 +1,16 @@
-class Logger {
-    unordered_map<string,int> messageToLastTimeHashMap;
-public:
-    Logger() {
-        messageToLastTimeHashMap.clear();
-    }
-    
-    bool shouldPrintMessage(int timestamp, string message) {
+class Logger
+{
+    unordered_map<string, int> messageToLastTimeHashMap;
+    public:
+        Logger()
+        {
+            messageToLastTimeHashMap.clear();
+        }
+
+    bool shouldPrintMessage(int timestamp, string message)
+    {
         bool didPrintMessage = false;
-        if(!messageToLastTimeHashMap.count(message) or 
-           (timestamp - messageToLastTimeHashMap[message] >= 10))
+        if (!messageToLastTimeHashMap.count(message) or(timestamp - messageToLastTimeHashMap[message] >= 10))
         {
             messageToLastTimeHashMap[message] = timestamp;
             return true;
@@ -18,7 +20,7 @@ public:
 };
 
 /**
- * Your Logger object will be instantiated and called as such:
- * Logger* obj = new Logger();
- * bool param_1 = obj->shouldPrintMessage(timestamp,message);
+ *Your Logger object will be instantiated and called as such:
+ *Logger* obj = new Logger();
+ *bool param_1 = obj->shouldPrintMessage(timestamp,message);
  */
