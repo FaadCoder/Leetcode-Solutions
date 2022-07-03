f1464d52ef53b95d00a4253420b195235ecfffaa
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-03 16:12:48 +0530

    Time: 842 ms, Memory: 112.6 MB - LeetHub

---
+++ find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -0,0 +1,51 @@
+class UnionFind {
+public:
+    UnionFind(int sz) : root(sz), rank(sz), count(sz) {
+        for (int i = 0; i < sz; i++) {
+            root[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    int find(int x) {
+        if (x == root[x]) {
+            return x;
+        }
+        return root[x] = find(root[x]);
+    }
+
+    void unionSet(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                root[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                root[rootX] = rootY;
+            } else {
+                root[rootY] = rootX;
+                rank[rootX] += 1;
+            }
+            count--;
+        }
+    }
+
+    int getCount() {
+        return count;
+    }
+
+private:
+    vector<int> root;
+    vector<int> rank;
+    int count;
+};
+class Solution {
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) 
+    {
+        UnionFind dsu(n);
+        for(auto &edge:edges)
+            dsu.unionSet(edge[0],edge[1]);
+        return dsu.find(source)==dsu.find(destination);
+    }
+};
