b75197bda64ae33a3b79ebe73168f6aa71066284
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-05 13:09:59 +0530

    Time: 34 ms (59.16%), Space: 16.6 MB (16.15%) - LeetHub

---
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<string> findItinerary(vector<vector<string>> tickets) {
+    for (auto ticket : tickets)
+        targets[ticket[0]].insert(ticket[1]);
+    visit("JFK");
+    return vector<string>(route.rbegin(), route.rend());
+}
+
+map<string, multiset<string>> targets;
+vector<string> route;
+
+void visit(string airport) {
+    while (targets[airport].size()) {
+        string next = *targets[airport].begin();
+        targets[airport].erase(targets[airport].begin());
+        visit(next);
+    }
+    route.push_back(airport);
+}
+};
