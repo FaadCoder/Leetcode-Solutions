54d08c0775454fd90506ea08de7cf2a332e6c78a
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-05 08:36:24 +0530

    Time: 833 ms (23.66%), Space: 45.7 MB (66.36%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,32 +1,32 @@
-class Solution {
-public:
-    int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> hashSet(nums.begin(),nums.end());
-        
-        int longestConsecutiveSequenceLength = 0;
-        for(int element:nums)
+class Solution
+{
+    public:
+        int longestConsecutive(vector<int> &nums)
         {
-            if(hashSet.count(element-1))
-                continue;
-            
-            int currentConsecutiveSequenceLength = 1;
-            
-            while(hashSet.count(element+1))
+            unordered_set<int> hashSet(nums.begin(), nums.end());
+
+            int longestConsecutiveSequenceLength = 0;
+            for (int element: nums)
             {
-                currentConsecutiveSequenceLength+=1;
-                element += 1;
+                if (hashSet.count(element - 1))
+                    continue;
+
+                int currentConsecutiveSequenceLength = 1;
+
+                while (hashSet.count(element + 1))
+                {
+                    currentConsecutiveSequenceLength += 1;
+                    element += 1;
+                }
+                longestConsecutiveSequenceLength = max(longestConsecutiveSequenceLength, currentConsecutiveSequenceLength);
             }
-            longestConsecutiveSequenceLength = max(longestConsecutiveSequenceLength,currentConsecutiveSequenceLength);
+
+            return longestConsecutiveSequenceLength;
         }
-        
-        return longestConsecutiveSequenceLength;
-    }
 };
 
 /*
 
 1,2,3,4,100,200
 
-
 */
-    
