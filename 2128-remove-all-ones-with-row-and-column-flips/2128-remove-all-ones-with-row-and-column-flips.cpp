2f88ceb7fc1ede3e5b37efbd8bd195ad22b52bf0
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-05 21:14:44 +0530

    Time: 94 ms (22.13%), Space: 24.6 MB (43.46%) - LeetHub

--- 2128-remove-all-ones-with-row-and-column-flips/2128-remove-all-ones-with-row-and-column-flips.cpp
+++ 2128-remove-all-ones-with-row-and-column-flips/2128-remove-all-ones-with-row-and-column-flips.cpp
@@ -1,20 +1,22 @@
-class Solution {
-public:
-    bool removeOnes(vector<vector<int>>& grid) {
-        int rows = grid.size();
-        int cols = grid[0].size();
-        for(int row = 0; row<rows; row++)
+class Solution
+{
+    public:
+        bool removeOnes(vector<vector < int>> &grid)
         {
-            bool isSame = true;
-            bool isOpposite = true;
-            for(int col=0; col<cols; col++)
+            int rows = grid.size();
+            int cols = grid[0].size();
+            for (int row = 0; row < rows; row++)
             {
-                isSame = isSame and (grid[0][col]==grid[row][col]);
-                isOpposite = isOpposite and (grid[0][col]!=grid[row][col]);
+                bool isSame = true;
+                bool isOpposite = true;
+                for (int col = 0; col < cols; col++)
+                {
+                    isSame = isSame and(grid[0][col] == grid[row][col]);
+                    isOpposite = isOpposite and(grid[0][col] != grid[row][col]);
+                }
+                if (!isSame and!isOpposite)
+                    return false;
             }
-            if(!isSame and !isOpposite)
-                return false;
+            return true;
         }
-        return true;
-    }
 };
