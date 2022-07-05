8938ba1ce5d98fe4131ae41b9ae78e7c644b13da
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-05 21:14:35 +0530

    Time: 103 ms (15.43%), Space: 24.4 MB (81.10%) - LeetHub

---
+++ 2128-remove-all-ones-with-row-and-column-flips/2128-remove-all-ones-with-row-and-column-flips.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool removeOnes(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        for(int row = 0; row<rows; row++)
+        {
+            bool isSame = true;
+            bool isOpposite = true;
+            for(int col=0; col<cols; col++)
+            {
+                isSame = isSame and (grid[0][col]==grid[row][col]);
+                isOpposite = isOpposite and (grid[0][col]!=grid[row][col]);
+            }
+            if(!isSame and !isOpposite)
+                return false;
+        }
+        return true;
+    }
+};
