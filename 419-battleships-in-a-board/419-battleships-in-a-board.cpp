29845c0b22a58ecab3846d9dde63c47aeadd904a
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-05 19:53:17 +0530

    Time: 22 ms (8.15%), Space: 8 MB (98.82%) - LeetHub

--- 419-battleships-in-a-board/419-battleships-in-a-board.cpp
+++ 419-battleships-in-a-board/419-battleships-in-a-board.cpp
@@ -1,36 +1,40 @@
-class Solution {
-public:
-    int countBattleships(vector<vector<char>>& board) {
-        int rows = board.size();
-        int cols = board[0].size();
-        int battleships = 0;
-        
-        for(int row = 0; row<rows; row++)
+class Solution
+{
+    public:
+        int countBattleships(vector<vector < char>> &board)
         {
-            for(int col = 0; col<cols; col++)
+            int rows = board.size();
+            int cols = board[0].size();
+            int battleships = 0;
+
+            for (int row = 0; row < rows; row++)
             {
-                if(row == 0 and col == 0 and board[row][col] == 'X')
+                for (int col = 0; col < cols; col++)
                 {
-                    battleships++;
-                }
-                else if(row == 0 and board[row][col] == 'X')
-                {
-                    if(board[row][col-1]=='.')
+                    if (row == 0 and col == 0 and board[row][col] == 'X')
+                    {
                         battleships++;
-                }
-                else if(col == 0 and board[row][col] == 'X')
-                {
-                    if(board[row-1][col]=='.')
-                        battleships++;
-                }
-                else
-                {
-                    if(board[row][col] == 'X' and board[row-1][col]=='.' and board[row][col-1]=='.')
-                        battleships++;
+                    }
+                    else if (row == 0 and board[row][col] == 'X')
+                    {
+                        if (board[row][col - 1] == '.')
+                            battleships++;
+                    }
+                    else if (col == 0 and board[row][col] == 'X')
+                    {
+                        if (board[row - 1][col] == '.')
+                            battleships++;
+                    }
+                    else
+                    {
+                        if (board[row][col] == 'X'
+                            and board[row - 1][col] == '.'
+                            and board[row][col - 1] == '.')
+                            battleships++;
+                    }
                 }
             }
+
+            return battleships;
         }
-        
-        return battleships;
-    }
 };
