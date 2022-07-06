4381110383e62452bbc2b3ac6a553daeb72f6e34
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-06 20:35:40 +0530

    Time: 7 ms (67.14%), Space: 13.8 MB (10.68%) - LeetHub

--- 833-find-and-replace-in-string/833-find-and-replace-in-string.cpp
+++ 833-find-and-replace-in-string/833-find-and-replace-in-string.cpp
@@ -1,24 +1,27 @@
-class Solution {
-public:
-    string findReplaceString(string s, vector<int>& indices, vector<string>& sources, vector<string>& targets) {
-        vector<pair<int,int>> index;
-        
-        for(int idx=0;idx<indices.size();idx++)
-            index.push_back({indices[idx],idx});
-        
-        sort(index.rbegin(),index.rend());
-        
-        for(auto idx:index)
+class Solution
+{
+    public:
+        string findReplaceString(string s, vector<int> &indices, vector<string> &sources, vector< string > &targets)
         {
-            int currentIndex = idx.second;
-            string source = sources[currentIndex];
-            string target = targets[currentIndex];
-            if(s.substr(idx.first,source.length())==source)
+            vector<pair<int, int>> index;
+
+            for (int idx = 0; idx < indices.size(); idx++)
+                index.push_back({ indices[idx],
+                    idx });
+
+            sort(index.rbegin(), index.rend());
+
+            for (auto idx: index)
             {
-                s = s.substr(0,idx.first)+target+s.substr(idx.first+source.length());
+                int currentIndex = idx.second;
+                string source = sources[currentIndex];
+                string target = targets[currentIndex];
+                if (s.substr(idx.first, source.length()) == source)
+                {
+                    s = s.substr(0, idx.first) + target + s.substr(idx.first + source.length());
+                }
             }
+
+            return s;
         }
-        
-        return s;
-    }
 };
