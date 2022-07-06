7c61237a90690b379952cc4363a59888f8afc6e5
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-06 12:53:23 +0530

    Time: 1334 ms (10.14%), Space: 156.8 MB (41.57%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.cpp
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.cpp
@@ -1,47 +1,47 @@
-class Solution {
-public:
-    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
-        unordered_map<string,int> indegree;
-        
-        unordered_map<string,unordered_set<string>> graph;
-        vector<string> ans;
-        
-        for(int recipe = 0; recipe<size(recipes); recipe++)
+class Solution
+{
+    public:
+        vector<string> findAllRecipes(vector<string> &recipes, vector<vector< string >> &ingredients, vector< string > &supplies)
         {
-            string currentRecipe = recipes[recipe];
-            for(string ingredient:ingredients[recipe])
+            unordered_map<string, int> indegree;
+
+            unordered_map<string, unordered_set < string>> graph;
+            vector<string> ans;
+
+            for (int recipe = 0; recipe < size(recipes); recipe++)
             {
-                graph[ingredient].insert(currentRecipe);
-                indegree[currentRecipe]++;
+                string currentRecipe = recipes[recipe];
+                for (string ingredient: ingredients[recipe])
+                {
+                    graph[ingredient].insert(currentRecipe);
+                    indegree[currentRecipe]++;
+                }
             }
-        }
-        
-        
-        queue<string> bfsQueue;
-        for(auto &it:supplies)
-        {
-            if(indegree[it]==0)
-                bfsQueue.push(it);
-        }
-        
-        while(!bfsQueue.empty())
-        {
-            string recipe = bfsQueue.front();
-            bfsQueue.pop();
-            for(auto child:graph[recipe])
+
+            queue<string> bfsQueue;
+            for (auto &it: supplies)
             {
-                indegree[child]--;
-                if(indegree[child]==0)
-                    bfsQueue.push(child);
+                if (indegree[it] == 0)
+                    bfsQueue.push(it);
             }
-            
+
+            while (!bfsQueue.empty())
+            {
+                string recipe = bfsQueue.front();
+                bfsQueue.pop();
+                for (auto child: graph[recipe])
+                {
+                    indegree[child]--;
+                    if (indegree[child] == 0)
+                        bfsQueue.push(child);
+                }
+            }
+
+            for (string recipe: recipes)
+            {
+                if (indegree[recipe] == 0)
+                    ans.push_back(recipe);
+            }
+            return ans;
         }
-        
-        for(string recipe:recipes)
-        {
-            if(indegree[recipe]==0)
-                ans.push_back(recipe);
-        }
-        return ans;
-    }
 };
