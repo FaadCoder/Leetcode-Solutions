d374228dd7fa7bb25784062063d9220e1fafceef
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-06 20:37:28 +0530

    Time: 0 ms (100.00%), Space: 10.7 MB (67.78%) - LeetHub

--- 833-find-and-replace-in-string/833-find-and-replace-in-string.cpp
+++ 833-find-and-replace-in-string/833-find-and-replace-in-string.cpp
@@ -1,19 +1,23 @@
-class Solution {
-public:
-    string findReplaceString(string S, vector<int>& indexes, vector<string>& sources, vector<string>& targets) {
-        unordered_map<int, pair<int, string> > m;
-        for(int i=0; i<indexes.size(); i++)
-            if(S.substr(indexes[i], sources[i].size())==sources[i])
-                m[indexes[i]]=make_pair(sources[i].size(), targets[i]);
-        string ans="";
-        for(int i=0; i<S.size(); i++){
-            if(m.find(i)!=m.end()){
-                ans+=m[i].second;
-                i=i+m[i].first-1;
+class Solution
+{
+    public:
+        string findReplaceString(string S, vector<int> &indexes, vector<string> &sources, vector< string > &targets)
+        {
+            unordered_map<int, pair<int, string>> m;
+            for (int i = 0; i < indexes.size(); i++)
+                if (S.substr(indexes[i], sources[i].size()) == sources[i])
+                    m[indexes[i]] = make_pair(sources[i].size(), targets[i]);
+            string ans = "";
+            for (int i = 0; i < S.size(); i++)
+            {
+                if (m.find(i) != m.end())
+                {
+                    ans += m[i].second;
+                    i = i + m[i].first - 1;
+                }
+                else
+                    ans += S[i];
             }
-            else
-                ans+=S[i];
+            return ans;
         }
-        return ans;
-    }
 };
