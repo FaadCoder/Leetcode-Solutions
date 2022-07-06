59913fdc7433fe47b189484f082f53d04f2085e6
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-06 08:53:46 +0530

    Time: 4 ms (52.45%), Space: 6 MB (41.61%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int fib(int n) {
-        int a = 0,b = 1,c=0;
-        for(int i=1;i<=n;i++)
+        if(n<=1)
+            return n;
+        
+        int prev2 = 0,prev1 = 1,current=0;
+        for(int i=2;i<=n;i++)
         {
-            c = a+b;
-            b=a;
-            a=c;
-            
+            current = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = current;
         }
-        return c;
+        return current;
     }
 };
