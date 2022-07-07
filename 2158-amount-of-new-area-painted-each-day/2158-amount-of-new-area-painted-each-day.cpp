3f21190ccb12f4f3d531b5446f485c2f09051173
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-07 21:29:41 +0530

    Time: 603 ms (41.66%), Space: 117.3 MB (58.66%) - LeetHub

--- 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.cpp
+++ 2158-amount-of-new-area-painted-each-day/2158-amount-of-new-area-painted-each-day.cpp
@@ -1,23 +1,25 @@
-class Solution {
-public:
-    vector<int> amountPainted(vector<vector<int>>& paint) {
-        vector<int> paintedArea(paint.size(),0);
-        vector<int> paintTill(50001,0);
-        int idx = 0;
-        for(auto &paintRange:paint)
+class Solution
+{
+    public:
+        vector<int> amountPainted(vector<vector < int>> &paint)
         {
-            int start = paintRange[0];
-            int end = paintRange[1];
-            while(start<end)
+            vector<int> paintedArea(paint.size(), 0);
+            vector<int> paintTill(50001, 0);
+            int idx = 0;
+            for (auto &paintRange: paint)
             {
-                int nextStart = max(start+1,paintTill[start]);
-                if(paintTill[start]==0)
-                    paintedArea[idx]++;
-                paintTill[start] = max(paintTill[start],end);
-                start = nextStart;
+                int start = paintRange[0];
+                int end = paintRange[1];
+                while (start < end)
+                {
+                    int nextStart = max(start + 1, paintTill[start]);
+                    if (paintTill[start] == 0)
+                        paintedArea[idx]++;
+                    paintTill[start] = max(paintTill[start], end);
+                    start = nextStart;
+                }
+                idx++;
             }
-            idx++;
+            return paintedArea;
         }
-        return paintedArea;
-    }
 };
