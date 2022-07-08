0a45c95672edc5a1da132a1b94938b133d89d14c
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-08 17:11:34 +0530

    Time: 6 ms (67.07%), Space: 8.6 MB (80.45%) - LeetHub

--- 900-rle-iterator/900-rle-iterator.cpp
+++ 900-rle-iterator/900-rle-iterator.cpp
@@ -1,24 +1,27 @@
-class RLEIterator {
+class RLEIterator
+{
     vector<int> encoding;
     int currentIdx = 0;
-public:
-    RLEIterator(vector<int>& encoding) {
-        this->encoding = encoding;
-        currentIdx = 0;
-    }
-    
-    int next(int n) {
-        while(currentIdx < encoding.size())
+    public:
+        RLEIterator(vector<int> &encoding)
         {
-            if(encoding[currentIdx]>=n)
+            this->encoding = encoding;
+            currentIdx = 0;
+        }
+
+    int next(int n)
+    {
+        while (currentIdx < encoding.size())
+        {
+            if (encoding[currentIdx] >= n)
             {
-                encoding[currentIdx]-=n;
-                return encoding[currentIdx+1];
+                encoding[currentIdx] -= n;
+                return encoding[currentIdx + 1];
             }
             else
             {
-                n-=encoding[currentIdx];
-                currentIdx+=2;
+                n -= encoding[currentIdx];
+                currentIdx += 2;
             }
         }
         return -1;
@@ -26,7 +29,7 @@ public:
 };
 
 /**
- * Your RLEIterator object will be instantiated and called as such:
- * RLEIterator* obj = new RLEIterator(encoding);
- * int param_1 = obj->next(n);
+ *Your RLEIterator object will be instantiated and called as such:
+ *RLEIterator* obj = new RLEIterator(encoding);
+ *int param_1 = obj->next(n);
  */
