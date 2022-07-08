5eb257ee80c668d6e4e817a6a388d09ef95d6ec5
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-08 17:11:23 +0530

    Time: 11 ms (25.26%), Space: 8.6 MB (80.45%) - LeetHub

---
+++ 900-rle-iterator/900-rle-iterator.cpp
@@ -0,0 +1,32 @@
+class RLEIterator {
+    vector<int> encoding;
+    int currentIdx = 0;
+public:
+    RLEIterator(vector<int>& encoding) {
+        this->encoding = encoding;
+        currentIdx = 0;
+    }
+    
+    int next(int n) {
+        while(currentIdx < encoding.size())
+        {
+            if(encoding[currentIdx]>=n)
+            {
+                encoding[currentIdx]-=n;
+                return encoding[currentIdx+1];
+            }
+            else
+            {
+                n-=encoding[currentIdx];
+                currentIdx+=2;
+            }
+        }
+        return -1;
+    }
+};
+
+/**
+ * Your RLEIterator object will be instantiated and called as such:
+ * RLEIterator* obj = new RLEIterator(encoding);
+ * int param_1 = obj->next(n);
+ */
