ef54e800ac8980965a690c846a9ed76a039285ee
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-08 21:13:30 +0530

    Time: 5 ms (38.08%), Space: 6.5 MB (97.53%) - LeetHub

--- 726-number-of-atoms/726-number-of-atoms.cpp
+++ 726-number-of-atoms/726-number-of-atoms.cpp
@@ -1,66 +1,69 @@
-class Solution {
-public:
-    string countOfAtoms(string formula) {
-        if(formula.empty())
-            return "";
-        reverse(begin(formula),end(formula));
-        
-        map<string,int> elementFrequencyMap;
-        stack<int> multiplyFactorStack;
-        multiplyFactorStack.push(1);
-        
-        string currentElement = "";
-        int multiplyFactor = 1;
-        
-        int idx = 0;
-        while(idx<formula.length())
+class Solution
+{
+    public:
+        string countOfAtoms(string formula)
         {
-            char currentChar = formula[idx];
-            if(isdigit(currentChar))
+            if (formula.empty())
+                return "";
+            reverse(begin(formula), end(formula));
+
+            map<string, int> elementFrequencyMap;
+            stack<int> multiplyFactorStack;
+            multiplyFactorStack.push(1);
+
+            string currentElement = "";
+            int multiplyFactor = 1;
+
+            int idx = 0;
+            while (idx < formula.length())
             {
-                string val;
-                while(isdigit(formula[idx]))
+                char currentChar = formula[idx];
+                if (isdigit(currentChar))
                 {
-                    val.push_back(formula[idx]);
-                    idx++;
+                    string val;
+                    while (isdigit(formula[idx]))
+                    {
+                        val.push_back(formula[idx]);
+                        idx++;
+                    }
+                    reverse(val.begin(), val.end());
+                    multiplyFactor = stoi(val);
+                    idx -= 1;
                 }
-                reverse(val.begin(),val.end());
-                multiplyFactor = stoi(val);
-                idx-=1;
+                else if (currentChar == ')')
+                {
+                    multiplyFactorStack.push(multiplyFactor *multiplyFactorStack.top());
+                    multiplyFactor = 1;
+                }
+                else if (currentChar >= 'a'
+                    and currentChar <= 'z')
+                {
+                    currentElement.push_back(currentChar);
+                }
+                else if (currentChar >= 'A'
+                    and currentChar <= 'Z')
+                {
+                    currentElement.push_back(currentChar);
+                    reverse(begin(currentElement), end(currentElement));
+                    elementFrequencyMap[currentElement] += multiplyFactor *multiplyFactorStack.top();
+                    multiplyFactor = 1;
+                    currentElement = "";
+                }
+                else
+                {
+                    multiplyFactorStack.pop();
+                }
+                idx++;
             }
-            else if(currentChar == ')')
+
+            string resultingString;
+
+            for (auto &it: elementFrequencyMap)
             {
-                multiplyFactorStack.push(multiplyFactor * multiplyFactorStack.top());
-                multiplyFactor = 1;
+                resultingString += it.first;
+                if (it.second > 1)
+                    resultingString += to_string(it.second);
             }
-            else if(currentChar>='a' and currentChar<='z')
-            {
-                currentElement.push_back(currentChar);
-            }
-            else if(currentChar>='A' and currentChar<='Z')
-            {
-                currentElement.push_back(currentChar);
-                reverse(begin(currentElement),end(currentElement));
-                elementFrequencyMap[currentElement] += multiplyFactor * multiplyFactorStack.top();
-                multiplyFactor = 1;
-                currentElement = "";
-            }
-            else
-            {
-                multiplyFactorStack.pop();
-            }
-            idx++;
+            return resultingString;
         }
-        
-        
-        string resultingString;
-        
-        for(auto &it:elementFrequencyMap)
-        {
-            resultingString+=it.first;
-            if(it.second>1)
-            resultingString+=to_string(it.second);
-        }
-        return resultingString;
-    }
 };
