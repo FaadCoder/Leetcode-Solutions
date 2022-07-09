0e3ccf616cbdce4dfbc6a0ed763b49365ef8cdd9
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-09 15:13:53 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (69.35%) - LeetHub

---
+++ 1099-two-sum-less-than-k/1099-two-sum-less-than-k.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int twoSumLessThanK(vector<int>& nums, int k) {
+        sort(begin(nums),end(nums));
+        int sum = INT_MIN;
+        int start = 0,end=nums.size()-1;
+        while(start<end)
+        {
+            int currentSum = nums[start]+nums[end];
+            if(currentSum < k)
+            {
+                sum = max(sum,currentSum);
+                start+=1;
+            }
+            else
+            {
+                end-=1;
+            }
+        }
+        return sum==INT_MIN?-1:sum;
+    }
+};
