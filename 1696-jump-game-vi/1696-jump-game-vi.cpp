6016614e84fb43dbc24bfb1abd545aa072f29f71
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-09 11:54:28 +0530

    Time: 367 ms (38.13%), Space: 81.9 MB (61.68%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -1,23 +1,25 @@
-class Solution {
-public:
-    int maxResult(vector<int>& nums, int k) {
-        int numberOfElements = nums.size();
-        deque<int> monotonicQueue;
-        vector<int> costToReachEnd(numberOfElements);
-        
-        costToReachEnd[numberOfElements-1] = nums.back();
-        monotonicQueue.push_back(numberOfElements-1);
-        
-        for(int elementIdx = numberOfElements-2; elementIdx>=0; elementIdx--)
+class Solution
+{
+    public:
+        int maxResult(vector<int> &nums, int k)
         {
-            if(!monotonicQueue.empty() and monotonicQueue.front()-elementIdx>k)
-                monotonicQueue.pop_front();
-            
-            costToReachEnd[elementIdx] = nums[elementIdx] + costToReachEnd[monotonicQueue.front()];
-            while(!monotonicQueue.empty() and costToReachEnd[monotonicQueue.back()]<costToReachEnd[elementIdx])
-                monotonicQueue.pop_back();
-            monotonicQueue.push_back(elementIdx);
+            int numberOfElements = nums.size();
+            deque<int> monotonicQueue;
+            vector<int> costToReachEnd(numberOfElements);
+
+            costToReachEnd[numberOfElements - 1] = nums.back();
+            monotonicQueue.push_back(numberOfElements - 1);
+
+            for (int elementIdx = numberOfElements - 2; elementIdx >= 0; elementIdx--)
+            {
+                if (!monotonicQueue.empty() and monotonicQueue.front() - elementIdx > k)
+                    monotonicQueue.pop_front();
+
+                costToReachEnd[elementIdx] = nums[elementIdx] + costToReachEnd[monotonicQueue.front()];
+                while (!monotonicQueue.empty() and costToReachEnd[monotonicQueue.back()] < costToReachEnd[elementIdx])
+                    monotonicQueue.pop_back();
+                monotonicQueue.push_back(elementIdx);
+            }
+            return costToReachEnd[0];
         }
-        return costToReachEnd[0];
-    }
 };
