b8c5d988f66a7381e9c71afce21aedbdd728dd4b
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-09 13:19:46 +0530

    Time: 636 ms (5.03%), Space: 7.5 MB (63.15%) - LeetHub

---
+++ 418-sentence-screen-fitting/418-sentence-screen-fitting.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+    public:
+        int wordsTyping(vector<string> &sentence, int rows, int cols)
+        {
+            string sentenceStringWithSpaces = "";
+            for (string &word: sentence)
+                sentenceStringWithSpaces += (word + " ");
+
+            int combinedSentenceStringLength = sentenceStringWithSpaces.length();
+            int cursor = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                cursor += cols;
+                cout << row << " " << sentenceStringWithSpaces[cursor % combinedSentenceStringLength] << endl;
+                if (sentenceStringWithSpaces[cursor % combinedSentenceStringLength] == ' ')
+                    cursor++;
+                else
+                {
+                    while (cursor > 0 and sentenceStringWithSpaces[(cursor - 1) % combinedSentenceStringLength] != ' ')
+                        cursor -= 1;
+                }
+            }
+
+            return cursor / combinedSentenceStringLength;
+        }
+};
