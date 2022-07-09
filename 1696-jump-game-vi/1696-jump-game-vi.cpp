207da325b17fa583d46d0dd0cb155e0549eb1b84
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-09 11:54:16 +0530

    Time: 386 ms (34.13%), Space: 81.8 MB (61.68%) - LeetHub

---
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+        int numberOfElements = nums.size();
+        deque<int> monotonicQueue;
+        vector<int> costToReachEnd(numberOfElements);
+        
+        costToReachEnd[numberOfElements-1] = nums.back();
+        monotonicQueue.push_back(numberOfElements-1);
+        
+        for(int elementIdx = numberOfElements-2; elementIdx>=0; elementIdx--)
+        {
+            if(!monotonicQueue.empty() and monotonicQueue.front()-elementIdx>k)
+                monotonicQueue.pop_front();
+            
+            costToReachEnd[elementIdx] = nums[elementIdx] + costToReachEnd[monotonicQueue.front()];
+            while(!monotonicQueue.empty() and costToReachEnd[monotonicQueue.back()]<costToReachEnd[elementIdx])
+                monotonicQueue.pop_back();
+            monotonicQueue.push_back(elementIdx);
+        }
+        return costToReachEnd[0];
+    }
+};
