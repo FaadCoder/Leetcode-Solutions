874cc622c0e4f38e6500c44a54ed193bf3628efc
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-10 13:28:39 +0530

    Time: 21 ms (8.07%), Space: 8.3 MB (16.70%) - LeetHub

---
+++ 1428-leftmost-column-with-at-least-a-one/1428-leftmost-column-with-at-least-a-one.cpp
@@ -0,0 +1,53 @@
+/**
+ * // This is the BinaryMatrix's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class BinaryMatrix {
+ *   public:
+ *     int get(int row, int col);
+ *     vector<int> dimensions();
+ * };
+ */
+
+class Solution {
+    
+    int getPositionOfOne(BinaryMatrix &binaryMatrix,int row,int low,int high)
+    {
+        int ans = INT_MAX;
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+            int valueAtMid = binaryMatrix.get(row,mid);
+            if(valueAtMid == 1)
+            {
+                ans = mid;
+                high = mid - 1;
+            }
+            else if(valueAtMid == 0)
+            {
+                low = mid+1;
+            }
+            else
+            {
+                high = mid-1;
+            }
+        }
+        return ans;
+    }
+    
+public:
+    int leftMostColumnWithOne(BinaryMatrix &binaryMatrix) {
+        int leftMostColumn = INT_MAX;
+        
+        auto dimensions = binaryMatrix.dimensions();
+        int rows = dimensions[0];
+        int cols = dimensions[1];
+        
+        for(int row = 0; row<rows;row++)
+        {
+            int positionOfOne = getPositionOfOne(binaryMatrix,row,0,cols-1);
+            leftMostColumn = min(leftMostColumn,positionOfOne);
+        }
+        
+        return leftMostColumn == INT_MAX ? -1 : leftMostColumn;
+    }
+};
