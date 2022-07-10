30c4d15fd8f6d4e0747b46f78de986e508fafbd8
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-10 13:29:08 +0530

    Time: 18 ms (15.95%), Space: 8.1 MB (81.61%) - LeetHub

--- 1428-leftmost-column-with-at-least-a-one/1428-leftmost-column-with-at-least-a-one.cpp
+++ 1428-leftmost-column-with-at-least-a-one/1428-leftmost-column-with-at-least-a-one.cpp
@@ -8,46 +8,48 @@
  * };
  */
 
-class Solution {
-    
-    int getPositionOfOne(BinaryMatrix &binaryMatrix,int row,int low,int high)
+class Solution
+{
+
+    int getPositionOfOne(BinaryMatrix &binaryMatrix, int row, int low, int high)
     {
         int ans = INT_MAX;
-        while(low<=high)
+        while (low <= high)
         {
-            int mid = low + (high-low)/2;
-            int valueAtMid = binaryMatrix.get(row,mid);
-            if(valueAtMid == 1)
+            int mid = low + (high - low) / 2;
+            int valueAtMid = binaryMatrix.get(row, mid);
+            if (valueAtMid == 1)
             {
                 ans = mid;
                 high = mid - 1;
             }
-            else if(valueAtMid == 0)
+            else if (valueAtMid == 0)
             {
-                low = mid+1;
+                low = mid + 1;
             }
             else
             {
-                high = mid-1;
+                high = mid - 1;
             }
         }
         return ans;
     }
-    
-public:
-    int leftMostColumnWithOne(BinaryMatrix &binaryMatrix) {
-        int leftMostColumn = INT_MAX;
-        
-        auto dimensions = binaryMatrix.dimensions();
-        int rows = dimensions[0];
-        int cols = dimensions[1];
-        
-        for(int row = 0; row<rows;row++)
+
+    public:
+        int leftMostColumnWithOne(BinaryMatrix & binaryMatrix)
         {
-            int positionOfOne = getPositionOfOne(binaryMatrix,row,0,cols-1);
-            leftMostColumn = min(leftMostColumn,positionOfOne);
+            int leftMostColumn = INT_MAX;
+
+            auto dimensions = binaryMatrix.dimensions();
+            int rows = dimensions[0];
+            int cols = dimensions[1];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int positionOfOne = getPositionOfOne(binaryMatrix, row, 0, cols - 1);
+                leftMostColumn = min(leftMostColumn, positionOfOne);
+            }
+
+            return leftMostColumn == INT_MAX ? -1 : leftMostColumn;
         }
-        
-        return leftMostColumn == INT_MAX ? -1 : leftMostColumn;
-    }
 };
