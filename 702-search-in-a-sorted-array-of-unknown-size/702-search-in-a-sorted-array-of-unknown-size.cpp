707a2c478ba37dbdef298d31d6dc079f004f1327
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-11 21:35:27 +0530

    Time: 58 ms (14.83%), Space: 10 MB (47.46%) - LeetHub

--- 702-search-in-a-sorted-array-of-unknown-size/702-search-in-a-sorted-array-of-unknown-size.cpp
+++ 702-search-in-a-sorted-array-of-unknown-size/702-search-in-a-sorted-array-of-unknown-size.cpp
@@ -7,33 +7,35 @@
  * };
  */
 
-class Solution {
-    
-    int getIndex(int low,int high,int target,const ArrayReader& reader)
+class Solution
+{
+
+    int getIndex(int low, int high, int target, const ArrayReader &reader)
     {
-        while(low<=high)
+        while (low <= high)
         {
-            int mid = low + (high-low)/2;
+            int mid = low + (high - low) / 2;
             int currentSecret = reader.get(mid);
-            if(currentSecret == target)
+            if (currentSecret == target)
                 return mid;
-            else if(currentSecret<target)
+            else if (currentSecret < target)
                 low++;
             else
                 high--;
         }
         return -1;
     }
-    
-public:
-    int search(const ArrayReader& reader, int target) {
-        int low = 0,high = 1;
-        int index = getIndex(low,high,target,reader);
-        while(index==-1 and reader.get(high)!=INT_MAX)
+
+    public:
+        int search(const ArrayReader &reader, int target)
         {
-            high*=2;
-            index = getIndex(low,high,target,reader);
+            int low = 0, high = 1;
+            int index = getIndex(low, high, target, reader);
+            while (index == -1 and reader.get(high) != INT_MAX)
+            {
+                high *= 2;
+                index = getIndex(low, high, target, reader);
+            }
+            return index;
         }
-        return index;
-    }
 };
