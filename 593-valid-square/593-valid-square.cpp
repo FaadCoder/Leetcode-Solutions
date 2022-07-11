f2bd5e133408938a55ece90e3531bf72b6d4c1d6
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-11 20:05:28 +0530

    Time: 19 ms (17.36%), Space: 26.5 MB (86.35%) - LeetHub

--- 593-valid-square/593-valid-square.cpp
+++ 593-valid-square/593-valid-square.cpp
@@ -1,29 +1,31 @@
-class Solution {
-    int getDistanceBetweenPoints(vector<int> &point1,vector<int> &point2)
+class Solution
+{
+    int getDistanceBetweenPoints(vector<int> &point1, vector<int> &point2)
     {
-        return (point1[0]-point2[0])*(point1[0]-point2[0]) + 
-            (point1[1]-point2[1])*(point1[1]-point2[1]);
+        return (point1[0] - point2[0]) *(point1[0] - point2[0]) +
+            (point1[1] - point2[1]) *(point1[1] - point2[1]);
     }
-    
-    bool isSame(vector<int> &point1,vector<int> &point2)
+
+    bool isSame(vector<int> &point1, vector<int> &point2)
     {
-       return (point1[0]==point2[0]) and (point1[1]==point2[1]); 
+        return (point1[0] == point2[0]) and(point1[1] == point2[1]);
     }
-    
-public:
-    bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
-        
-        if(isSame(p1,p2) or isSame(p1,p3) or isSame(p1,p4) or isSame(p2,p3) or isSame(p2,p4) or isSame(p3,p4))
-            return false;
-        
-        unordered_set<int> hashSet;
-        hashSet.insert(getDistanceBetweenPoints(p1,p2));
-        hashSet.insert(getDistanceBetweenPoints(p1,p3));
-        hashSet.insert(getDistanceBetweenPoints(p1,p4));
-        hashSet.insert(getDistanceBetweenPoints(p2,p3));
-        hashSet.insert(getDistanceBetweenPoints(p2,p4));
-        hashSet.insert(getDistanceBetweenPoints(p3,p4));
-        
-        return hashSet.size()==2;
-    }
+
+    public:
+        bool validSquare(vector<int> &p1, vector<int> &p2, vector< int > &p3, vector< int > &p4)
+        {
+
+            if (isSame(p1, p2) or isSame(p1, p3) or isSame(p1, p4) or isSame(p2, p3) or isSame(p2, p4) or isSame(p3, p4))
+                return false;
+
+            unordered_set<int> hashSet;
+            hashSet.insert(getDistanceBetweenPoints(p1, p2));
+            hashSet.insert(getDistanceBetweenPoints(p1, p3));
+            hashSet.insert(getDistanceBetweenPoints(p1, p4));
+            hashSet.insert(getDistanceBetweenPoints(p2, p3));
+            hashSet.insert(getDistanceBetweenPoints(p2, p4));
+            hashSet.insert(getDistanceBetweenPoints(p3, p4));
+
+            return hashSet.size() == 2;
+        }
 };
