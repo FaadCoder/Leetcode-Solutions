b5075c105bc5fe0ccb174254c7a1fe4e78069305
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-11 12:15:10 +0530

    Time: 328 ms (50.94%), Space: 9.1 MB (89.62%) - LeetHub

--- 2184-number-of-ways-to-build-sturdy-brick-wall/2184-number-of-ways-to-build-sturdy-brick-wall.cpp
+++ 2184-number-of-ways-to-build-sturdy-brick-wall/2184-number-of-ways-to-build-sturdy-brick-wall.cpp
@@ -1,58 +1,59 @@
-class Solution {
-    
-    int height,width;
+class Solution
+{
+
+    int height, width;
     vector<int> bricks;
     vector<int> allMasks;
-    const int MOD = 1e9+7;
-    int dp[101][(1<<10)+1];
-    int addUnderModulo(int a,int b)
+    const int MOD = 1e9 + 7;
+    int dp[101][(1 << 10) + 1];
+    int addUnderModulo(int a, int b)
     {
-        return (a%MOD + b%MOD)%MOD;
+        return (a % MOD + b % MOD) % MOD;
     }
-    
-    void getAllPossibleMasks(int currentWidth,int currentMask)
+
+    void getAllPossibleMasks(int currentWidth, int currentMask)
     {
-        if(currentWidth > width)
+        if (currentWidth > width)
             return;
-        if(currentWidth == width)
+        if (currentWidth == width)
             allMasks.push_back(currentMask);
         else
         {
-            if(currentWidth)
+            if (currentWidth)
             {
-                currentMask |= 1<<(currentWidth-1);
+                currentMask |= 1 << (currentWidth - 1);
             }
-            for(int brick:bricks)
+            for (int brick: bricks)
             {
                 int pos = currentWidth + brick;
-                getAllPossibleMasks(currentWidth + brick,currentMask);
+                getAllPossibleMasks(currentWidth + brick, currentMask);
             }
         }
     }
-    
-    int getWays(int currentHeight = 0, int prev=0)
+
+    int getWays(int currentHeight = 0, int prev = 0)
     {
-        if(currentHeight==height)
+        if (currentHeight == height)
             return 1;
-        if(dp[currentHeight][prev]!=-1)
+        if (dp[currentHeight][prev] != -1)
             return dp[currentHeight][prev];
         int ways = 0;
-        for(int mask:allMasks)
+        for (int mask: allMasks)
         {
-            if((prev & mask)==0)
-                ways = addUnderModulo(ways,getWays(currentHeight+1,mask));
+            if ((prev & mask) == 0)
+                ways = addUnderModulo(ways, getWays(currentHeight + 1, mask));
         }
         return dp[currentHeight][prev] = ways;
     }
-    
-    
-public:
-    int buildWall(int height, int width, vector<int>& bricks) {
-        this->height = height;
-        this->width = width;
-        this->bricks = bricks;
-        getAllPossibleMasks(0,0);
-        memset(dp,-1,sizeof(dp));
-        return getWays();
-    }
+
+    public:
+        int buildWall(int height, int width, vector<int> &bricks)
+        {
+            this->height = height;
+            this->width = width;
+            this->bricks = bricks;
+            getAllPossibleMasks(0, 0);
+            memset(dp, -1, sizeof(dp));
+            return getWays();
+        }
 };
