1f581fa3144a66da146c45717016ce3914f2efdf
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-11 09:58:40 +0530

    Time: 109 ms (91.33%), Space: 26.2 MB (90.67%) - LeetHub

--- 732-my-calendar-iii/732-my-calendar-iii.cpp
+++ 732-my-calendar-iii/732-my-calendar-iii.cpp
@@ -1,27 +1,30 @@
-class MyCalendarThree {
-    map<int,int> lineSweepMap;
-public:
-    MyCalendarThree() {
-        lineSweepMap.clear();
-    }
-    
-    int book(int start, int end) {
+class MyCalendarThree
+{
+    map<int, int> lineSweepMap;
+    public:
+        MyCalendarThree()
+        {
+            lineSweepMap.clear();
+        }
+
+    int book(int start, int end)
+    {
         lineSweepMap[start]++;
         lineSweepMap[end]--;
-        
+
         int booked = 0;
         int maxBooked = 0;
-        for(auto it:lineSweepMap)
+        for (auto it: lineSweepMap)
         {
             booked += it.second;
-            maxBooked = max(maxBooked,booked);
+            maxBooked = max(maxBooked, booked);
         }
         return maxBooked;
     }
 };
 
 /**
- * Your MyCalendarThree object will be instantiated and called as such:
- * MyCalendarThree* obj = new MyCalendarThree();
- * int param_1 = obj->book(start,end);
+ *Your MyCalendarThree object will be instantiated and called as such:
+ *MyCalendarThree* obj = new MyCalendarThree();
+ *int param_1 = obj->book(start,end);
  */
