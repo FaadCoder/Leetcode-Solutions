393b2dde31c69af19f6c5588f4d836abc0ccb85d
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-12 10:21:06 +0530

    Time: 67 ms (32.74%), Space: 21.8 MB (5.43%) - LeetHub

--- 244-shortest-word-distance-ii/244-shortest-word-distance-ii.cpp
+++ 244-shortest-word-distance-ii/244-shortest-word-distance-ii.cpp
@@ -1,28 +1,35 @@
-class WordDistance {
-    unordered_map<string,vector<int>> hashMap;
-public:
-    WordDistance(vector<string>& wordsDict) {
-        for(int idx=0;idx<size(wordsDict);idx++)
-            hashMap[wordsDict[idx]].push_back(idx);
-    }
+class WordDistance
+{
+    unordered_map<string,unordered_map<string,int>> dp;
     
-    int shortest(string word1, string word2) {
-        int shortestDistance = INT_MAX;
-        int i=0,j=0;
-        while(i<hashMap[word1].size() and j<hashMap[word2].size())
+    unordered_map<string, vector < int>> hashMap;
+    public:
+        WordDistance(vector<string> &wordsDict)
         {
-            shortestDistance = min(abs(hashMap[word1][i]-hashMap[word2][j]),shortestDistance);
-            if(hashMap[word1][i]<hashMap[word2][j])
+            for (int idx = 0; idx < size(wordsDict); idx++)
+                hashMap[wordsDict[idx]].push_back(idx);
+        }
+
+    int shortest(string word1, string word2)
+    {
+        if(dp.count(word1) and dp[word1].count(word2))
+            return dp[word1][word2];
+        int shortestDistance = INT_MAX;
+        int i = 0, j = 0;
+        while (i < hashMap[word1].size() and j < hashMap[word2].size())
+        {
+            shortestDistance = min(abs(hashMap[word1][i] - hashMap[word2][j]), shortestDistance);
+            if (hashMap[word1][i] < hashMap[word2][j])
                 i++;
             else
                 j++;
         }
-        return shortestDistance;
+        return dp[word1][word2] = shortestDistance;
     }
 };
 
 /**
- * Your WordDistance object will be instantiated and called as such:
- * WordDistance* obj = new WordDistance(wordsDict);
- * int param_1 = obj->shortest(word1,word2);
+ *Your WordDistance object will be instantiated and called as such:
+ *WordDistance* obj = new WordDistance(wordsDict);
+ *int param_1 = obj->shortest(word1,word2);
  */
