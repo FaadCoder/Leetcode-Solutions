38e50acb235ba0a1b9fb8e133a9bc749336be24a
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-12 19:58:53 +0530

    Time: 424 ms (43.06%), Space: 88.8 MB (46.11%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,28 +1,29 @@
-class Solution {
+class Solution
+{
     int dp[100002][2][3];
-    int getMaxProfit(vector<int>& prices,int idx=0,int currentAsk = 0,int transactions=0)
+    int getMaxProfit(vector<int> &prices, int idx = 0, int currentAsk = 0, int transactions = 0)
     {
-        if(transactions>2)
-            return INT_MIN/2;
-        
-        if(dp[idx][currentAsk][transactions]!=-1)
+        if (transactions > 2)
+            return INT_MIN / 2;
+
+        if (dp[idx][currentAsk][transactions] != -1)
             return dp[idx][currentAsk][transactions];
-        
-        if(idx>=prices.size())
+
+        if (idx >= prices.size())
             return 0;
-        
-        if(currentAsk == 0)
+
+        if (currentAsk == 0)
         {
-            return dp[idx][currentAsk][transactions] = max(getMaxProfit(prices,idx+1,0,transactions),getMaxProfit(prices,idx+1,1,transactions)-prices[idx]);
+            return dp[idx][currentAsk][transactions] = max(getMaxProfit(prices, idx + 1, 0, transactions), getMaxProfit(prices, idx + 1, 1, transactions) - prices[idx]);
         }
-    
-        return dp[idx][currentAsk][transactions] = max(getMaxProfit(prices,idx+1,1,transactions),getMaxProfit(prices,idx+1,0,transactions+1)+prices[idx]);
+
+        return dp[idx][currentAsk][transactions] = max(getMaxProfit(prices, idx + 1, 1, transactions), getMaxProfit(prices, idx + 1, 0, transactions + 1) + prices[idx]);
     }
-    
-public:
-    int maxProfit(vector<int>& prices)
-    {
-        memset(dp,-1,sizeof(dp));
-        return getMaxProfit(prices);
-    }
+
+    public:
+        int maxProfit(vector<int> &prices)
+        {
+            memset(dp, -1, sizeof(dp));
+            return getMaxProfit(prices);
+        }
 };
