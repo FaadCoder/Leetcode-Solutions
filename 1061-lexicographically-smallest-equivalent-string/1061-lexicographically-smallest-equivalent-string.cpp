7a15806ccce1d51bd7a140b697b4fa46bbd354eb
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-12 16:49:07 +0530

    Time: 13 ms (5.48%), Space: 6.8 MB (20.09%) - LeetHub

--- 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
+++ 1061-lexicographically-smallest-equivalent-string/1061-lexicographically-smallest-equivalent-string.cpp
@@ -1,47 +1,48 @@
 class UnionFind
 {
-private:
-    unordered_map<char,char> parent;
-public:
-    UnionFind()
-    {
-        parent.clear();
-        for(char ch='a';ch<='z';ch++)
-            parent[ch] = ch;
-    }
-    
+    private:
+        unordered_map<char, char> parent;
+    public:
+        UnionFind()
+        {
+            parent.clear();
+            for (char ch = 'a'; ch <= 'z'; ch++)
+                parent[ch] = ch;
+        }
+
     char findParent(char child)
     {
-        if(child==parent[child])
+        if (child == parent[child])
             return child;
         return parent[child] = findParent(parent[child]);
     }
-    
-    void unionSets(char set1,char set2)
+
+    void unionSets(char set1, char set2)
     {
         set1 = findParent(set1);
         set2 = findParent(set2);
-        if(set1==set2)
+        if (set1 == set2)
             return;
-        if(set1>set2)
-            swap(set1,set2);
+        if (set1 > set2)
+            swap(set1, set2);
         parent[set2] = set1;
     }
 };
 
-class Solution {
-public:
-    string smallestEquivalentString(string s1, string s2, string baseStr) {
-        UnionFind unionFind;
-        for(int idx=0;idx<s1.length();idx++)
-            unionFind.unionSets(s1[idx],s2[idx]);
-        string smallestString;
-        
-        for(char &ch:baseStr)
+class Solution
+{
+    public:
+        string smallestEquivalentString(string s1, string s2, string baseStr)
         {
-            ch = unionFind.findParent(ch);
+            UnionFind unionFind;
+            for (int idx = 0; idx < s1.length(); idx++)
+                unionFind.unionSets(s1[idx], s2[idx]);
+
+            for (char &ch: baseStr)
+            {
+                ch = unionFind.findParent(ch);
+            }
+
+            return baseStr;
         }
-        
-        return baseStr;
-    }
 };
