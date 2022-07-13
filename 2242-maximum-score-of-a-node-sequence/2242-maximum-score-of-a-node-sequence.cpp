575df59c31c68bf51321c1f1e7b1b8e66e647e8a
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-13 20:47:39 +0530

    Time: 1337 ms (8.46%), Space: 188.5 MB (11.41%) - LeetHub

---
+++ 2242-maximum-score-of-a-node-sequence/2242-maximum-score-of-a-node-sequence.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+    public:
+        int maximumScore(vector<int> &scores, vector<vector< int>> &edges)
+        {
+            set<pair<int, int>> graph[scores.size()];
+            for (auto edge: edges)
+            {
+                graph[edge[0]].insert({ scores[edge[1]],
+                    edge[1] });
+                graph[edge[1]].insert({ scores[edge[0]],
+                    edge[0] });
+                if (graph[edge[0]].size() > 3) graph[edge[0]].erase(graph[edge[0]].begin());
+                if (graph[edge[1]].size() > 3) graph[edge[1]].erase(graph[edge[1]].begin());
+            }
+            int res = -1;
+            for (auto edge: edges)
+            {
+                int ans = scores[edge[0]] + scores[edge[1]];
+                for (auto node1: graph[edge[0]])
+                {
+                    for (auto node2: graph[edge[1]])
+                    {
+                        if (node1.second != edge[0] && node1.second != edge[1] && node2.second != edge[0] && node2.second != edge[1] && node2.second != node1.second)
+                            res = max(res, ans + node1.first + node2.first);
+                    }
+                }
+            }
+            return res;
+        }
+};
