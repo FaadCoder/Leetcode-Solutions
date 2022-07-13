1ec27864122a1eb47ea1c7db7648ab18bd7e7152
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-13 10:03:41 +0530

    Time: 206 ms (46.93%), Space: 78.1 MB (27.93%) - LeetHub

--- 1660-correct-a-binary-tree/1660-correct-a-binary-tree.cpp
+++ 1660-correct-a-binary-tree/1660-correct-a-binary-tree.cpp
@@ -10,39 +10,21 @@
  * };
  */
 class Solution {
-    unordered_set<int> hashSet;
-    TreeNode *nodeToRemove;
-    void getNodeToRemove(TreeNode *root,TreeNode *prev)
-    {
-        if(!root)
-            return;
-        getNodeToRemove(root->right,root);
-        if(hashSet.count(root->val))
-        {
-            nodeToRemove = prev;
-            return;
-        }
-        hashSet.insert(root->val);
-        getNodeToRemove(root->left,root);
-    }
-    
-    TreeNode *removeNode(TreeNode* root,TreeNode* node)
-    {
-        if(!root)
-            return NULL;
-        root->left = removeNode(root->left,node);
-        root->right = removeNode(root->right,node);
-        if(root->val==node->val)
-        {
-            root->right = NULL;
-            root = NULL;
-        }
-        return root;
-    }
-    
 public:
-    TreeNode* correctBinaryTree(TreeNode* root) {
-        getNodeToRemove(root,NULL);
-        return removeNode(root,nodeToRemove);
+unordered_set<TreeNode*> s;
+TreeNode* correctBinaryTree(TreeNode* root) {
+        if (root == NULL){
+            return NULL;
+        }
+        if (root->right != NULL && s.find(root->right) != s.end()){
+            return NULL;
+        }
+        
+        root->right = correctBinaryTree(root->right);
+        root->left = correctBinaryTree(root->left);
+        
+        s.insert(root);
+        
+        return root;
     }
 };
