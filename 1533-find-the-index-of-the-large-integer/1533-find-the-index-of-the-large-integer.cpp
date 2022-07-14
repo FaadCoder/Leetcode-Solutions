b9999e417a03af02b82f80b585b6c1b136fe0694
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-14 13:13:05 +0530

    Time: 180 ms (64.71%), Space: 39.4 MB (92.94%) - LeetHub

---
+++ 1533-find-the-index-of-the-large-integer/1533-find-the-index-of-the-large-integer.cpp
@@ -0,0 +1,47 @@
+/**
+ * // This is the ArrayReader's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class ArrayReader {
+ *   public:
+ *     // Compares the sum of arr[l..r] with the sum of arr[x..y] 
+ *     // return 1 if sum(arr[l..r]) > sum(arr[x..y])
+ *     // return 0 if sum(arr[l..r]) == sum(arr[x..y])
+ *     // return -1 if sum(arr[l..r]) < sum(arr[x..y])
+ *     int compareSub(int l, int r, int x, int y);
+ *
+ *     // Returns the length of the array
+ *     int length();
+ * };
+ */
+
+class Solution {
+public:
+    int getIndex(ArrayReader &reader) {
+        int low = 0,high = reader.length()-1;
+        
+        while(low<high)
+        {
+            int mid = low+(high-low)/2;
+            int currentLen = high-low+1;
+            int compareRanges = 0;
+            if(currentLen & 1)
+                compareRanges = reader.compareSub(low,mid,mid,high);
+            else
+                compareRanges = reader.compareSub(low,mid,mid+1,high);
+
+            if(compareRanges==1)
+            {
+                high = mid;
+            }
+            else if(compareRanges==-1)
+            {
+                low = mid+1;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+        return low;
+    }
+};
