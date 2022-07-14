5e01e59d7946019c46272f53b83a96530e9d7015
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-14 15:30:00 +0530

    Time: 33 ms, Memory: 10 MB - LeetHub

---
+++ search-in-a-sorted-array-of-unknown-size/search-in-a-sorted-array-of-unknown-size.cpp
@@ -0,0 +1,37 @@
+/**
+ * // This is the ArrayReader's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * class ArrayReader {
+ *   public:
+ *     int get(int index);
+ * };
+ */
+
+class Solution
+{
+
+    int getIndex(int low, int high, int target, const ArrayReader &reader)
+    {
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            int currentSecret = reader.get(mid);
+            if (currentSecret == target)
+                return mid;
+            else if (currentSecret < target)
+                low++;
+            else
+                high--;
+        }
+        return -1;
+    }
+
+    public:
+        int search(const ArrayReader &reader, int target)
+        {
+            int low = 0, high = 1;
+            while (reader.get(high) < target)
+                low=high,high <<= 1;
+            return getIndex(low, high, target, reader);
+        }
+};
