03d96a51c7c4e7dd8bd041ebb0cd002fe1956ce5
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-16 15:47:46 +0530

    Time: 180 ms (78.87%), Space: 252.4 MB (86.97%) - LeetHub

---
+++ 1506-find-root-of-n-ary-tree/1506-find-root-of-n-ary-tree.cpp
@@ -0,0 +1,42 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* findRoot(vector<Node*> tree) {
+        if(tree.empty())
+            return NULL;
+        int nodeAppearOnce = 0;
+        for(auto treeNode:tree)
+        {
+            nodeAppearOnce ^= treeNode->val;
+            for(auto child:treeNode->children)
+            {
+                nodeAppearOnce ^= child->val;
+            }
+        }
+        
+        for(auto treeNode : tree)
+            if(nodeAppearOnce == treeNode->val)
+                return treeNode;
+        
+        return NULL;
+    }
+};
