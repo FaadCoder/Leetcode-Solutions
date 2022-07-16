406b38a4fc569217c24e047679d0c9f2788c844e
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-16 09:33:14 +0530

    Time: 22 ms (35.31%), Space: 7.2 MB (59.96%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -4,6 +4,11 @@ class Solution {
     
     int dp[51][51][51];
     
+    bool isValid(int row,int col,int rows,int cols)
+    {
+        return (row>=0 and row<rows and col>=0 and col<cols);
+    }
+    
     int findPathsHelper(int m, int n, int maxMove, int r, int c) {
         if(r<0 or r>=m or c<0 or c>=n)
             return maxMove>=0;
@@ -19,9 +24,35 @@ class Solution {
         }
         return dp[maxMove][r][c] = paths;
     }
+    
+    int bottomUp(int m, int n, int maxMove, int r, int c)
+    {
+        int cache[51][51][51];
+        memset(cache,0,sizeof(cache));
+        for(int move = 1; move<=maxMove; move++)
+        {
+            for(int i=0;i<m;i++)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    for(auto &dir:dirs)
+                    {
+                        if(!isValid(i+dir.first,j+dir.second,m,n))
+                            cache[move][i][j]++;
+                        else
+                        {
+                            cache[move][i][j] = (cache[move][i][j] + cache[move-1][i+dir.first][j+dir.second])%MOD;
+                        }
+                    }
+                }
+            }
+        }
+        return cache[maxMove][r][c];
+    }
+    
 public:
     int findPaths(int m, int n, int maxMove, int r, int c) {
         memset(dp,-1,sizeof(dp));
-        return findPathsHelper(m,n,maxMove,r,c);
+        return bottomUp(m,n,maxMove,r,c);
     }
 };
