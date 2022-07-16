ee9a8896be5459477096e322845ef6d174a2e765
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-16 21:33:31 +0530

    Time: 13 ms (49.56%), Space: 10.1 MB (46.93%) - LeetHub

--- 484-find-permutation/484-find-permutation.cpp
+++ 484-find-permutation/484-find-permutation.cpp
@@ -1,23 +1,25 @@
-class Solution {
-public:
-    vector<int> findPermutation(string s) {
-        vector<int> permutation;
-        int start = 0;
-        int end = s.length()+1;
-        for(int i=start;i<end;i++)
-            permutation.push_back(i+1);
-        
-        for(int i=0;i<s.length();i++)
+class Solution
+{
+    public:
+        vector<int> findPermutation(string s)
         {
-            if(s[i]=='D')
-           {
-                int h=i;
-                while(i<s.length() and s[i]=='D')
-                    i++;
-                reverse(permutation.begin()+h,permutation.begin()+i+1);
+            vector<int> permutation;
+            int start = 0;
+            int end = s.length() + 1;
+            for (int i = start; i < end; i++)
+                permutation.push_back(i + 1);
+
+            for (int i = 0; i < s.length(); i++)
+            {
+                if (s[i] == 'D')
+                {
+                    int h = i;
+                    while (i < s.length() and s[i] == 'D')
+                        i++;
+                    reverse(permutation.begin() + h, permutation.begin() + i + 1);
+                }
             }
+
+            return permutation;
         }
-        
-        return permutation;
-    }
 };
