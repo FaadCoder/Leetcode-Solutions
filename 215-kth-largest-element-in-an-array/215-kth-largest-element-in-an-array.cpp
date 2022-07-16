bf0eb3aea241972fe76a0a5762215ec7e925b6f5
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-16 22:20:03 +0530

    Time: 339 ms (5.00%), Space: 45.6 MB (8.87%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,14 +1,35 @@
 class Solution {
-public:
-    int findKthLargest(vector<int>& nums, int k) 
+    
+    int getPivot(vector<int> &nums,int low,int high)
     {
-        priority_queue<int,vector<int>,greater<int>> pq;
-        for(int num:nums)
+        int pivotElement = nums[high];
+        int pivotIndex = low;
+        
+        for(int idx=low;idx<=high;idx++)
         {
-            pq.push(num);
-            if(pq.size() > k)
-                pq.pop();
+            if(nums[idx]<pivotElement)
+            {
+                swap(nums[idx],nums[pivotIndex]);
+                pivotIndex++;
+            }
         }
-        return pq.top();
+        swap(nums[high],nums[pivotIndex]);
+        return pivotIndex;
+    }
+    
+    int getKthLargestElement(vector<int> &nums,int low,int high,int k)
+    {
+        int pivot = getPivot(nums,low,high);
+        if(pivot==k)
+            return nums[pivot];
+        if(pivot>k)
+            return getKthLargestElement(nums,low,pivot-1,k);
+        return getKthLargestElement(nums,pivot+1,high,k);
+    }
+    
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        return getKthLargestElement(nums,0,size(nums)-1,size(nums)-k);
+            
     }
 };
