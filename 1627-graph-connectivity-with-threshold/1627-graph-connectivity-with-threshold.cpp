11c4117efcc29a0b347281ca8b7139f238356be1
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-16 09:55:44 +0530

    Time: 599 ms (5.78%), Space: 72.9 MB (29.48%) - LeetHub

--- 1627-graph-connectivity-with-threshold/1627-graph-connectivity-with-threshold.cpp
+++ 1627-graph-connectivity-with-threshold/1627-graph-connectivity-with-threshold.cpp
@@ -1,74 +1,61 @@
-class UnionFind{
-  unordered_map<int,int> parent,size;
-public:
-    UnionFind(int n)
-    {
-        for(int i=0;i<=n;i++)
-            parent[i] = i,size[i] = 1;
-    }
-    
+class UnionFind
+{
+    unordered_map<int, int> parent, size;
+    public:
+        UnionFind(int n)
+        {
+            for (int i = 0; i <= n; i++)
+                parent[i] = i, size[i] = 1;
+        }
+
     int findParent(int child)
     {
-        if(child==parent[child])
+        if (child == parent[child])
             return child;
         return parent[child] = findParent(parent[child]);
     }
-    
-    bool isConnected(int set1,int set2)
+
+    bool isConnected(int set1, int set2)
     {
-        return findParent(set1)==findParent(set2);
+        return findParent(set1) == findParent(set2);
     }
-    
-    bool unionSet(int set1,int set2)
+
+    bool unionSet(int set1, int set2)
     {
         set1 = findParent(set1);
         set2 = findParent(set2);
-        
-        if(set1==set2)
+
+        if (set1 == set2)
             return false;
-        
-        if(size[set1]<size[set2])
-            swap(set1,set2);
+
+        if (size[set1] < size[set2])
+            swap(set1, set2);
         parent[set2] = set1;
-        size[set1]+=size[set2];
+        size[set1] += size[set2];
         return true;
     }
-        
 };
-class Solution {
-public:
-    vector<bool> areConnected(int n, int threshold, vector<vector<int>>& queries) {
-        UnionFind unionFind(n);
-        
-        for(int city1=1;city1<=n;city1++)
+class Solution
+{
+    public:
+        vector<bool> areConnected(int n, int threshold, vector<vector < int>> &queries)
         {
-            for(int city2=2*city1;city2<=n;city2+=city1)
+            UnionFind unionFind(n);
+
+            for (int city1 = 1; city1 <= n; city1++)
             {
-                if(city1>threshold)
-                    unionFind.unionSet(city2,city1);
-            }   
+                for (int city2 = 2 * city1; city2 <= n; city2 += city1)
+                {
+                    if (city1 > threshold)
+                        unionFind.unionSet(city2, city1);
+                }
+            }
+
+            vector<bool> queryAns;
+            for (auto &query: queries)
+            {
+                queryAns.push_back(unionFind.isConnected(query[0], query[1]));
+            }
+            return queryAns;
         }
-        
-        vector<bool> queryAns;
-        for(auto &query:queries)
-        {
-            queryAns.push_back(unionFind.isConnected(query[0],query[1]));
-        }
-        return queryAns;
-    }
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
