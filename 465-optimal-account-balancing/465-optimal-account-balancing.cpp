eb951b5657728a50e3291b73c6c5a9af231f94c3
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-17 21:01:22 +0530

    Time: 26 ms (53.60%), Space: 7.1 MB (56.08%) - LeetHub

--- 465-optimal-account-balancing/465-optimal-account-balancing.cpp
+++ 465-optimal-account-balancing/465-optimal-account-balancing.cpp
@@ -1,39 +1,41 @@
-class Solution {
+class Solution
+{
 
-    int getMinimumTransactions(int idx,vector<int> &balance)
+    int getMinimumTransactions(int idx, vector<int> &balance)
     {
-        if(idx>=balance.size())
+        if (idx >= balance.size())
             return 0;
-        if(balance[idx]==0)
-            return getMinimumTransactions(idx+1,balance);
+        if (balance[idx] == 0)
+            return getMinimumTransactions(idx + 1, balance);
         int currentBalance = balance[idx];
-        int res = INT_MAX/2;
-        
-        for(int i=idx+1;i<balance.size();i++)
+        int res = INT_MAX / 2;
+
+        for (int i = idx + 1; i < balance.size(); i++)
         {
             int nextBalance = balance[i];
-            if(currentBalance*nextBalance<0)
+            if (currentBalance * nextBalance < 0)
             {
-                balance[i]+=currentBalance;
-                res = min(res,1+getMinimumTransactions(idx+1,balance));
-                balance[i]-=currentBalance;
+                balance[i] += currentBalance;
+                res = min(res, 1 + getMinimumTransactions(idx + 1, balance));
+                balance[i] -= currentBalance;
             }
         }
         return res;
     }
-    
+
     public:
-    int minTransfers(vector<vector<int>>& transactions) {
-        unordered_map<int,int> currentBalance;
-        for(auto &transaction:transactions)
+        int minTransfers(vector<vector < int>> &transactions)
         {
-            currentBalance[transaction[0]]-=transaction[2];
-            currentBalance[transaction[1]]+=transaction[2];
+            unordered_map<int, int> currentBalance;
+            for (auto &transaction: transactions)
+            {
+                currentBalance[transaction[0]] -= transaction[2];
+                currentBalance[transaction[1]] += transaction[2];
+            }
+            vector<int> balance;
+            for (auto it: currentBalance)
+                balance.push_back(it.second);
+
+            return getMinimumTransactions(0, balance);
         }
-        vector<int> balance;
-        for(auto it:currentBalance)
-            balance.push_back(it.second);
-        
-        return getMinimumTransactions(0,balance);
-    }
 };
