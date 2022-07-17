d51a4a1c0cbd36bb4557641f8268a0b55467124c
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-17 19:08:01 +0530

    Time: 178 ms (33.88%), Space: 20.5 MB (86.51%) - LeetHub

--- 361-bomb-enemy/361-bomb-enemy.cpp
+++ 361-bomb-enemy/361-bomb-enemy.cpp
@@ -1,56 +1,57 @@
-class Solution {
-    
-    int getRowHits(vector<vector<char>>& grid,int row,int col)
+class Solution
+{
+
+    int getRowHits(vector<vector < char>> &grid, int row, int col)
     {
         int cols = grid[0].size();
         int rowHits = 0;
-        while(col<cols and grid[row][col]!='W')
+        while (col < cols and grid[row][col] != 'W')
         {
-            rowHits += grid[row][col]=='E';
+            rowHits += grid[row][col] == 'E';
             col++;
         }
         return rowHits;
     }
-    
-    
-    int getColHits(vector<vector<char>>& grid,int row,int col)
+
+    int getColHits(vector<vector < char>> &grid, int row, int col)
     {
         int rows = grid.size();
         int colHits = 0;
-        while(row<rows and grid[row][col]!='W')
+        while (row < rows and grid[row][col] != 'W')
         {
-            colHits += grid[row][col]=='E';
+            colHits += grid[row][col] == 'E';
             row++;
         }
         return colHits;
     }
-    
-public:
-    int maxKilledEnemies(vector<vector<char>>& grid) {
-        int rows = grid.size();
-        int cols = grid[0].size();
-        
-        int rowHits = 0;
-        vector<int> colHits(cols,0);
-        int maxHits = 0;
-        for(int row=0;row<rows;row++)
+
+    public:
+        int maxKilledEnemies(vector<vector < char>> &grid)
         {
-            for(int col=0;col<cols;col++)
+            int rows = grid.size();
+            int cols = grid[0].size();
+
+            int rowHits = 0;
+            vector<int> colHits(cols, 0);
+            int maxHits = 0;
+            for (int row = 0; row < rows; row++)
             {
-                if(col==0 or grid[row][col-1]=='W')
+                for (int col = 0; col < cols; col++)
                 {
-                    rowHits = getRowHits(grid,row,col);
+                    if (col == 0 or grid[row][col - 1] == 'W')
+                    {
+                        rowHits = getRowHits(grid, row, col);
+                    }
+
+                    if (row == 0 or grid[row - 1][col] == 'W')
+                    {
+                        colHits[col] = getColHits(grid, row, col);
+                    }
+
+                    if (grid[row][col] == '0')
+                        maxHits = max(rowHits + colHits[col], maxHits);
                 }
-                
-                if(row==0 or grid[row-1][col]=='W')
-                {
-                    colHits[col] = getColHits(grid,row,col);
-                }
-                
-                if(grid[row][col]=='0')
-                    maxHits = max(rowHits+colHits[col],maxHits);
             }
+            return maxHits;
         }
-        return maxHits;
-    }
 };
