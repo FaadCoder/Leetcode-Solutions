6fae8ea95c138171ab2b326dd018a67faff5753e
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-17 20:06:39 +0530

    Time: 22 ms (60.34%), Space: 13 MB (18.10%) - LeetHub

--- 1842-next-palindrome-using-same-digits/1842-next-palindrome-using-same-digits.cpp
+++ 1842-next-palindrome-using-same-digits/1842-next-palindrome-using-same-digits.cpp
@@ -7,14 +7,16 @@ Algorithm
 
 */
 
-class Solution {
-public:
-    string nextPalindrome(string num) {
-        string mid = string(num.length()%2,num[num.length()/2]);
-        string firstHalf = num.substr(0,num.length()/2);
-        if(!next_permutation(begin(firstHalf),end(firstHalf)))
-            return "";
-        
-        return firstHalf+mid+string(firstHalf.rbegin(),firstHalf.rend());
-    }
+class Solution
+{
+    public:
+        string nextPalindrome(string num)
+        {
+            string mid = string(num.length() % 2, num[num.length() / 2]);
+            string firstHalf = num.substr(0, num.length() / 2);
+            if (!next_permutation(begin(firstHalf), end(firstHalf)))
+                return "";
+
+            return firstHalf + mid + string(firstHalf.rbegin(), firstHalf.rend());
+        }
 };
