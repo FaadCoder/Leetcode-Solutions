66f1d5640c9548dd1db3d320497bd47477103ae1
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-17 20:06:32 +0530

    Time: 11 ms (97.41%), Space: 13 MB (28.45%) - LeetHub

---
+++ 1842-next-palindrome-using-same-digits/1842-next-palindrome-using-same-digits.cpp
@@ -0,0 +1,20 @@
+/*
+Algorithm
+- concentrate on first half
+- Find the next permutation of first half
+- Take care of odd and even length string
+- Palindrome string  = Next Permutation of first half + Mid character + reverse(Next Permutation of first half)
+
+*/
+
+class Solution {
+public:
+    string nextPalindrome(string num) {
+        string mid = string(num.length()%2,num[num.length()/2]);
+        string firstHalf = num.substr(0,num.length()/2);
+        if(!next_permutation(begin(firstHalf),end(firstHalf)))
+            return "";
+        
+        return firstHalf+mid+string(firstHalf.rbegin(),firstHalf.rend());
+    }
+};
