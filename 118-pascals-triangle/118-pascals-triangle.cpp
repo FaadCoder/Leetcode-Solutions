df0019e60e1772bcb92bc8885706a7ebbdcdc6cd
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-19 09:17:10 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (71.06%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -1,21 +1,23 @@
-class Solution {
-public:
-    vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> pascalRows;
-        pascalRows.push_back({1});
-        for(int row = 1; row<numRows;row++)
+class Solution
+{
+    public:
+        vector<vector < int>> generate(int numRows)
         {
-            vector<int> &previousRow = pascalRows.back();
-            vector<int> newRow(row+1);
-            newRow[0] = previousRow[0];
-            newRow[row] = previousRow.back();
-            
-            for(int idx=1;idx<row;idx++)
-                newRow[idx] = previousRow[idx]+previousRow[idx-1];
-            
-            pascalRows.push_back(newRow);
+            vector<vector < int>> pascalRows;
+            pascalRows.push_back({ 1 });
+            for (int row = 1; row < numRows; row++)
+            {
+                vector<int> &previousRow = pascalRows.back();
+                vector<int> newRow(row + 1);
+                newRow[0] = previousRow[0];
+                newRow[row] = previousRow.back();
+
+                for (int idx = 1; idx < row; idx++)
+                    newRow[idx] = previousRow[idx] + previousRow[idx - 1];
+
+                pascalRows.push_back(newRow);
+            }
+
+            return pascalRows;
         }
-        
-        return pascalRows;
-    }
 };
