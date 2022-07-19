60acd73f84f08e45176652152db402f6178f320f
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-19 12:22:31 +0530

    Time: 690 ms (89.14%), Space: 170.9 MB (63.64%) - LeetHub

--- 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.cpp
@@ -1,37 +1,59 @@
-class Solution {
-    typedef array<int, 26> T;
-public:
-    int largestPathValue(string C, vector<vector<int>>& E) {
-        unordered_map<int, vector<int>> G;
-        vector<int> indegree(C.size());
-        for (auto &e : E) {
-            G[e[0]].push_back(e[1]); // build graph
-            indegree[e[1]]++; // count indegrees
-        }
-        vector<T> cnt(C.size(), T{}); // cnt[i][j] is the maximum count of j-th color from the ancester nodes to node i.
-        queue<int> q;
-        for (int i = 0; i < C.size(); ++i) {
-            if (indegree[i] == 0) { // if this node has 0 indegree, we can use it as a source node
-                q.push(i);
-                cnt[i][C[i] - 'a'] = 1; // the count of the current color should be 1
+struct GraphNode
+{
+    GraphNode(): inDegree(0), neighbors(), count(vector<int> (26, 0)) {}
+    int inDegree;
+    vector<int> neighbors;
+    vector<int> count;
+};
+class Solution
+{
+    public:
+        int largestPathValue(string colors, vector<vector < int>> &edges)
+        {
+            int n = colors.size();
+            vector<GraphNode> graph(n);
+            for (auto &edge: edges)
+            {
+                int f = edge[0], t = edge[1];
+                graph[f].neighbors.emplace_back(t);
+                ++graph[t].inDegree;
             }
-        }
-        int ans = 0, seen = 0;
-        while (q.size()) {
-            auto u = q.front();
-            q.pop();
-            int val = *max_element(begin(cnt[u]), end(cnt[u])); // we use the maximum of all the maximum color counts as the color value.
-            ans = max(ans, val);
-            ++seen;
-            for (int v : G[u]) {
-                for (int i = 0; i < 26; ++i) {
-                    cnt[v][i] = max(cnt[v][i], cnt[u][i] + (i == C[v] - 'a')); // try to use node `u` to update all the color counts of node `v`.
-                }
-                if (--indegree[v] == 0) {
-                    q.push(v);
+            queue<int> q;
+            for (int i = 0; i < n; ++i)
+            {
+                if (graph[i].inDegree == 0)
+                {
+                    q.emplace(i);
                 }
             }
+            if (q.empty())
+            {
+                return -1;
+            }
+            int ans = 0;
+            int popped = 0;
+            while (!q.empty())
+            {
+                int id = q.front();
+                q.pop();
+                ++popped;
+                ++graph[id].count[colors[id] - 'a'];
+                for (int i = 0; i < 26; ++i)
+                {
+                    ans = max(ans, graph[id].count[i]);
+                }
+                for (int neighborId: graph[id].neighbors)
+                {
+                    for (int i = 0; i < 26; ++i)
+                    {
+                        graph[neighborId].count[i] = max(graph[neighborId].count[i], graph[id].count[i]);
+                    }
+                    if (--graph[neighborId].inDegree == 0)
+                    {
+                        q.emplace(neighborId);
+                    }
+                }
+            }
+            return popped == n ? ans : -1;
         }
-        return seen < C.size() ? -1 : ans;
-    }
 };
