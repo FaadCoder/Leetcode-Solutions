c3933ca0b63a4edaf10e4bf8fbfbe21410de79dd
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-20 15:19:56 +0530

    Time: 33 ms (48.99%), Space: 7.7 MB (83.74%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        if(s.length()<t.length())
+            return "";
+        if(s==t)
+            return s;
+        
+        int start = 0;
+        unordered_map<char,int> freqHashMap;
+        for(char &ch:t)
+            freqHashMap[ch]++;
+        
+        int count = freqHashMap.size();
+        
+        int windowLength = INT_MAX;
+        int windowStart = -1;
+        for(int end = 0; end<s.length(); end++)
+        {
+            if(freqHashMap.count(s[end]))
+            {
+                freqHashMap[s[end]]--;
+                if(freqHashMap[s[end]]==0)
+                count--;
+            }
+            
+            while(count==0 and start<=end)
+            {
+                if(end-start+1<windowLength)
+                {
+                    windowLength = end-start+1;
+                    windowStart = start;
+                }
+                if(freqHashMap.count(s[start]))
+                {
+                    if(freqHashMap[s[start]]==0)
+                    count++;
+                    freqHashMap[s[start]]++;
+                }
+                start++;
+            }
+        }
+        
+        return (windowStart!=-1)?s.substr(windowStart,windowLength):"";
+    }
+};
