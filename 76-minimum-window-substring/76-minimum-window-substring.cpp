b6815004cd727e053cf39864e4c8cc1882918a0c
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-20 15:20:10 +0530

    Time: 41 ms (37.63%), Space: 7.7 MB (83.74%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -1,46 +1,48 @@
-class Solution {
-public:
-    string minWindow(string s, string t) {
-        if(s.length()<t.length())
-            return "";
-        if(s==t)
-            return s;
-        
-        int start = 0;
-        unordered_map<char,int> freqHashMap;
-        for(char &ch:t)
-            freqHashMap[ch]++;
-        
-        int count = freqHashMap.size();
-        
-        int windowLength = INT_MAX;
-        int windowStart = -1;
-        for(int end = 0; end<s.length(); end++)
+class Solution
+{
+    public:
+        string minWindow(string s, string t)
         {
-            if(freqHashMap.count(s[end]))
+            if (s.length() < t.length())
+                return "";
+            if (s == t)
+                return s;
+
+            int start = 0;
+            unordered_map<char, int> freqHashMap;
+            for (char &ch: t)
+                freqHashMap[ch]++;
+
+            int count = freqHashMap.size();
+
+            int windowLength = INT_MAX;
+            int windowStart = -1;
+            for (int end = 0; end < s.length(); end++)
             {
-                freqHashMap[s[end]]--;
-                if(freqHashMap[s[end]]==0)
-                count--;
-            }
-            
-            while(count==0 and start<=end)
-            {
-                if(end-start+1<windowLength)
+                if (freqHashMap.count(s[end]))
                 {
-                    windowLength = end-start+1;
-                    windowStart = start;
+                    freqHashMap[s[end]]--;
+                    if (freqHashMap[s[end]] == 0)
+                        count--;
                 }
-                if(freqHashMap.count(s[start]))
+
+                while (count == 0 and start <= end)
                 {
-                    if(freqHashMap[s[start]]==0)
-                    count++;
-                    freqHashMap[s[start]]++;
+                    if (end - start + 1 < windowLength)
+                    {
+                        windowLength = end - start + 1;
+                        windowStart = start;
+                    }
+                    if (freqHashMap.count(s[start]))
+                    {
+                        if (freqHashMap[s[start]] == 0)
+                            count++;
+                        freqHashMap[s[start]]++;
+                    }
+                    start++;
                 }
-                start++;
             }
+
+            return (windowStart != -1) ? s.substr(windowStart, windowLength) : "";
         }
-        
-        return (windowStart!=-1)?s.substr(windowStart,windowLength):"";
-    }
 };
