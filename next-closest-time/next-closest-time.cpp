bd801c20717f120d3819c6f6c05e76edf955899e
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-20 14:30:29 +0530

    Time: 3 ms, Memory: 6.1 MB - LeetHub

---
+++ next-closest-time/next-closest-time.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+    public:
+        string nextClosestTime(string time)
+        {
+            unordered_set<char> hashSet(begin(time), end(time));
+            int hours = (time[0] - '0') *10 + (time[1] - '0');
+            int minutes = (time[3] - '0') *10 + (time[4] - '0');
+
+            while (true)
+            {
+                minutes += 1;
+                if (minutes == 60)
+                {
+                    minutes = 0;
+                    hours = (hours + 1) % 24;
+                }
+
+                string newTime = string(1, '0' + hours / 10) + string(1, '0' + hours % 10);
+                newTime += ":";
+                newTime += string(1, '0' + minutes / 10) + string(1, '0' + minutes % 10);
+                bool isValid = true;
+                for (char ch: newTime)
+                {
+                    if (!hashSet.count(ch))
+                    {
+                        isValid = false;
+                        break;
+                    }
+                }
+                if (isValid)
+                    return newTime;
+            }
+
+            return "";
+        }
+};
