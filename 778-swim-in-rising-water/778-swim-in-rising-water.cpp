b69f35e61c0b7fb0842aa584d1ac3c2a3081eba5
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-21 22:00:30 +0530

    Time: 26 ms (72.60%), Space: 10.4 MB (50.19%) - LeetHub

--- 778-swim-in-rising-water/778-swim-in-rising-water.cpp
+++ 778-swim-in-rising-water/778-swim-in-rising-water.cpp
@@ -1,73 +1,83 @@
-class Solution
-{
-    int rows, cols;
-    int dirs[4][2] = {
-		{ 1,
-            0 },
-        { 0,
-            1 },
-        { -1,
-            0 },
-        { 0,
-            -1 }
-    };
-
-    int getMinimumTimeToReachBottom(vector<vector < int>> &grid)
+struct HeapNode{
+    int x;
+    int y;
+    int val;
+    HeapNode(int _x,int _y,int _val)
     {
-        if (rows == 0 or cols == 0)
-            return -1;
+        x = _x;
+        y = _y;
+        val = _val;
+    }
+};
 
-        bool visited[rows][cols];
-        memset(visited, false, sizeof(visited));
+struct Compare{
+    bool operator()(HeapNode &node1,HeapNode &node2){
+        return node1.val > node2.val;
+    };
+};
 
-        priority_queue<vector < int>, vector< vector< int >>, greater<vector< int>>> minHeap;
-        visited[0][0] = true;
-
-        auto isValid =[& ](int row, int col)
-        {
-            return row >= 0 and row < rows and col >= 0 and col < cols and!visited[row][col];
+class Solution {
+    int rows,cols;
+    vector<pair<int,int>> dirs = {{1,0},{-1,0},{0,1},{0,-1}};
+    
+public:
+    int swimInWater(vector<vector<int>>& grid) 
+    {
+        rows = grid.size();
+        cols = grid[0].size();
+    
+        
+        auto getOneDimensionalCoord = [&](int row,int col){
+            return row*cols + col;
         };
-
-        minHeap.push({ grid[0][0],
-            0,
-            0 });
-
-        int minTime = grid[0][0];
-
-        while (!minHeap.empty())
+        
+        
+        auto isValid = [&](int row,int col){
+            return row>=0 and row<rows and col>=0 and col<cols;
+        };
+        
+        priority_queue<HeapNode,vector<HeapNode>,Compare> minHeap;
+        
+        minHeap.push(HeapNode(0,0,grid[0][0]));
+        unordered_set<int> vis;
+        vis.insert(getOneDimensionalCoord(0,0));
+        
+        while(!minHeap.empty())
         {
-            auto top = minHeap.top();
+            auto topNode = minHeap.top();
             minHeap.pop();
-            int currentTime = top[0];
-            int row = top[1];
-            int col = top[2];
-            minTime = max(minTime, currentTime);
-
-            if (row == rows - 1 and col == cols - 1)
-                return minTime;
-
-            for (int dir = 0; dir < 4; dir++)
+            int x = topNode.x;
+            int y = topNode.y;
+            int val = topNode.val;
+            
+            if(x==rows-1 and y==cols-1)
+                return val;
+            
+            for(pair<int,int> &dir:dirs)
             {
-                int newRow = row + dirs[dir][0];
-                int newCol = col + dirs[dir][1];
-                if (isValid(newRow, newCol))
+                int nextX = x+dir.first;
+                int nextY = y+dir.second;
+                if(isValid(nextX,nextY) and !vis.count(getOneDimensionalCoord(nextX,nextY)))
                 {
-                    visited[newRow][newCol] = true;
-                    minHeap.push({ grid[newRow][newCol],
-                        newRow,
-                        newCol });
+                    vis.insert(getOneDimensionalCoord(nextX,nextY));
+                    minHeap.push(HeapNode(nextX,nextY,max(val,grid[nextX][nextY])));
                 }
             }
         }
-
+        
         return -1;
+        
     }
-
-    public:
-        int swimInWater(vector<vector < int>> &grid)
-        {
-            rows = grid.size();
-            cols = grid[0].size();
-            return getMinimumTimeToReachBottom(grid);
-        }
 };
+
+
+
+
+
+
+
+
+
+
+
+
