4fd8db12114b3c348d3829935ea9d68bf617855e
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-24 11:59:18 +0530

    Time: 93 ms (36.87%), Space: 21.2 MB (45.30%) - LeetHub

--- 505-the-maze-ii/505-the-maze-ii.cpp
+++ 505-the-maze-ii/505-the-maze-ii.cpp
@@ -1,78 +1,59 @@
-struct Node{
-    int row,col,d;
-    Node(int _d,int _row,int _col)
-    {
-        row = _row;
-        col = _col;
-        d = _d;
-    }
-};
-
-struct compare{
-    bool operator()(Node &node1,Node &node2)
-    {
-        return node1.d>node2.d;
-    }
-};
-
 class Solution {
     int rows,cols;
+    vector<pair<int,int>> dirs = {{1,0},{-1,0},{0,1},{0,-1}};
     
-    vector<vector<int>> dirs = {{1,0},{-1,0},{0,-1},{0,1}};
-    
+    bool isValid(int row,int col)
+    {
+        return row>=0 and row<rows and col>=0 and col<cols;
+    }
     
 public:
-    int shortestDistance(vector<vector<int>>& maze, 
-                         vector<int>& start, 
-                         vector<int>& destination) 
-    {
+    int shortestDistance(vector<vector<int>>& maze, vector<int>& start, vector<int>& dest) {
         rows = maze.size();
         cols = maze[0].size();
         
-        auto isValid = [&](int row,int col){
-            return (row>=0 and row<rows and col>=0 and col<cols);
-        };
-        
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> minHeap;
         vector<vector<int>> dis(rows,vector<int>(cols,INT_MAX));
-        
         dis[start[0]][start[1]] = 0;
+        minHeap.push({0,start[0],start[1]});
         
-        priority_queue<Node,vector<Node>,compare> bfsQueue;
-        
-        bfsQueue.push(Node(0,start[0],start[1]));
-        
-        while(!bfsQueue.empty())
+        while(!minHeap.empty())
         {
-            auto front = bfsQueue.top();
-            bfsQueue.pop();
-            int row = front.row;
-            int col = front.col;
-            int cost = front.d;
-            
+            auto top = minHeap.top();
+            minHeap.pop();
+            int row = top[1];
+            int col = top[2];
+            int cost = top[0];
             if(dis[row][col]<cost)
                 continue;
             
-            if(row==destination[0] and col==destination[1])
+            if(row==dest[0] and col==dest[1])
                 return cost;
             
-            for(auto &dir:dirs)
+            for(auto [x,y] : dirs)
             {
-                int newRow = front.row;
-                int newCol = front.col;
-                int count = 0;
-                while(isValid(newRow+dir[0],newCol+dir[1]) and maze[newRow+dir[0]][newCol+dir[1]]==0)
+                int nextRow = row+x;
+                int nextCol = col+y;
+                int nextCost = 0;
+                while(isValid(nextRow,nextCol) and maze[nextRow][nextCol]==0)
                 {
-                    newRow+=dir[0];
-                    newCol+=dir[1];
-                    count++;
+                    nextCost+=1;
+                    nextRow+=x;
+                    nextCol+=y;
                 }
-                if(cost+count<dis[newRow][newCol])
+                nextRow-=x;
+                nextCol-=y;
+                if(dis[nextRow][nextCol]>cost + nextCost)
                 {
-                    dis[newRow][newCol] = cost+count;
-                    bfsQueue.push(Node(dis[newRow][newCol],newRow,newCol));
+                    dis[nextRow][nextCol] = cost + nextCost;
+                    minHeap.push({cost+nextCost,nextRow,nextCol});
                 }
+                    
+                    
             }
+            
         }
+        
         return -1;
     }
 };
