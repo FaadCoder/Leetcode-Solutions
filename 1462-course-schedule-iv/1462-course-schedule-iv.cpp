deb853e7c0b2f90f848de245600bcae360dfc1d3
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-26 21:07:13 +0530

    Time: 573 ms (33.45%), Space: 66.2 MB (52.40%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.cpp
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -1,47 +1,47 @@
-class Solution {
-    
-    void applyFloydWardshall(vector<vector<bool>> &isReachable,vector<vector<int>>& prerequisites)
+class Solution
+{
+
+    void applyFloydWardshall(vector<vector < bool>> &isReachable, vector< vector< int>> &prerequisites)
     {
         int numCourses = isReachable.size();
-        for(int vertex = 0; vertex<numCourses; vertex++)
+        for (int vertex = 0; vertex < numCourses; vertex++)
             isReachable[vertex][vertex] = true;
-        
-        for(auto prerequisite:prerequisites)
+
+        for (auto prerequisite: prerequisites)
         {
             int from = prerequisite[0];
             int to = prerequisite[1];
             isReachable[from][to] = true;
         }
-        
-        for(int helperVertex = 0; helperVertex<numCourses; helperVertex++)
+
+        for (int helperVertex = 0; helperVertex < numCourses; helperVertex++)
         {
-            for(int from = 0; from<numCourses; from++)
+            for (int from = 0; from < numCourses; from++)
             {
-                for(int to = 0; to<numCourses; to++)
+                for (int to = 0; to < numCourses; to++)
                 {
-                    isReachable[from][to] = isReachable[from][to] or (isReachable[from][helperVertex] and isReachable[helperVertex][to]);
+                    isReachable[from][to] = isReachable[from][to] or(isReachable[from][helperVertex] and isReachable[helperVertex][to]);
                 }
             }
         }
-        
     }
-    
-    vector<bool> getQueriesResult(vector<vector<bool>> &isReachable,vector<vector<int>>& queries)
+
+    vector<bool> getQueriesResult(vector<vector < bool>> &isReachable, vector< vector< int>> &queries)
     {
         vector<bool> queriesResult;
-        
-        for(auto query:queries)
+
+        for (auto query: queries)
             queriesResult.push_back(isReachable[query[0]][query[1]]);
-        
+
         return queriesResult;
     }
-    
-public:
-    vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
-        vector<vector<bool>> isReachable(numCourses,vector<bool>(numCourses,false));
-        
-        applyFloydWardshall(isReachable,prerequisites);
-        return getQueriesResult(isReachable,queries);
-        
-    }
+
+    public:
+        vector<bool> checkIfPrerequisite(int numCourses, vector<vector < int>> &prerequisites, vector< vector< int>> &queries)
+        {
+            vector<vector < bool>> isReachable(numCourses, vector<bool> (numCourses, false));
+
+            applyFloydWardshall(isReachable, prerequisites);
+            return getQueriesResult(isReachable, queries);
+        }
 };
