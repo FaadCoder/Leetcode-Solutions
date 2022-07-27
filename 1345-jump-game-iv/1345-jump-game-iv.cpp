8f007b6e3652a920cfa225cb3fc6f2bd795e7e85
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-27 11:25:35 +0530

    Time: 817 ms (7.47%), Space: 124.5 MB (9.16%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.cpp
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -1,54 +1,57 @@
-class Solution {
-public:
-    int minJumps(vector<int>& arr) 
-    {
-        unordered_map<int,vector<int>> indicesMap;
-        for(int idx=0; idx<arr.size(); idx++)
-            indicesMap[arr[idx]].push_back(idx);
-       
-        queue<int> bfsQueue;
-        bfsQueue.push(0);
-        
-        unordered_set<int> vis;
-        vis.insert(0);
-        
-        int level = 0;
-        
-        while(!bfsQueue.empty())
+class Solution
+{
+    public:
+        int minJumps(vector<int> &arr)
         {
-            int size = bfsQueue.size();
-            while(size--)
+            unordered_map<int, vector < int>> indicesMap;
+            for (int idx = 0; idx < arr.size(); idx++)
+                indicesMap[arr[idx]].push_back(idx);
+
+            queue<int> bfsQueue;
+            bfsQueue.push(0);
+
+            unordered_set<int> vis;
+            vis.insert(0);
+
+            int level = 0;
+
+            while (!bfsQueue.empty())
             {
-                int parent = bfsQueue.front();
-                bfsQueue.pop();
-                if(parent==arr.size()-1)
-                    return level;
-               
-                if(parent-1>=0 and !vis.count(parent-1))
+                int size = bfsQueue.size();
+                while (size--)
                 {
-                    vis.insert(parent-1);
-                    bfsQueue.push(parent-1);
-                }
-                
-                if(parent+1<arr.size() and !vis.count(parent+1))
-                {
-                    vis.insert(parent+1);
-                    bfsQueue.push(parent+1);
-                }
-                
-                for(auto sameIndex:indicesMap[arr[parent]])
-                {
-                    if(!vis.count(sameIndex))
+                    int parent = bfsQueue.front();
+                    bfsQueue.pop();
+                    if (parent == arr.size() - 1)
+                        return level;
+
+                    if (parent - 1 >= 0 and!vis.count(parent - 1))
                     {
-                        vis.insert(sameIndex);
-                        bfsQueue.push(sameIndex);
+                        vis.insert(parent - 1);
+                        bfsQueue.push(parent - 1);
                     }
+
+                    if (parent + 1 < arr.size() and!vis.count(parent + 1))
+                    {
+                        vis.insert(parent + 1);
+                        bfsQueue.push(parent + 1);
+                    }
+
+                    for (auto sameIndex: indicesMap[arr[parent]])
+                    {
+                        if (!vis.count(sameIndex))
+                        {
+                            vis.insert(sameIndex);
+                            bfsQueue.push(sameIndex);
+                        }
+                    }
+
+                   	// Main Line to Avoid TLE
+                    indicesMap.erase(arr[parent]);
                 }
-                indicesMap.erase(arr[parent]);
+                level++;
             }
-            level++;
+
+            return -1;
         }
-        
-        return -1;
-    }
 };
