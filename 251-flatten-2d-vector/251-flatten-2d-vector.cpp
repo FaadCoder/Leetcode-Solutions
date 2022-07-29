64ba2c4e4256375de1d8d84bd25fd89b1b932272
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-29 09:40:30 +0530

    Time: 28 ms (81.34%), Space: 19.4 MB (64.86%) - LeetHub

--- 251-flatten-2d-vector/251-flatten-2d-vector.cpp
+++ 251-flatten-2d-vector/251-flatten-2d-vector.cpp
@@ -1,30 +1,20 @@
 class Vector2D {
-    int row = 0;
-    int col = 0;
-    vector<vector<int>> vec;
+    vector<vector<int>>::iterator i, iEnd;
+    int j = 0;
 public:
-    Vector2D(vector<vector<int>>& vec) {
-        this->vec = vec;
-        row = 0;
-        col = 0;
+    Vector2D(vector<vector<int>>& vec2d) {
+        i = vec2d.begin();
+        iEnd = vec2d.end();
     }
-    
+
     int next() {
         hasNext();
-        return vec[row][col++];
+        return (*i)[j++];
     }
-    
+
     bool hasNext() {
-        while(row<vec.size() and vec[row].size()==col)
-            row++,col=0;
-        
-        return (row<vec.size());
+        while (i != iEnd && j == (*i).size())
+            i++, j = 0;
+        return i != iEnd;
     }
 };
-
-/**
- * Your Vector2D object will be instantiated and called as such:
- * Vector2D* obj = new Vector2D(vec);
- * int param_1 = obj->next();
- * bool param_2 = obj->hasNext();
- */
