4d40979036c112602afad80db4154838cdbb20a4
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-07-31 12:47:25 +0530

    Time: 10 ms (96.14%), Space: 10.2 MB (65.23%) - LeetHub

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.cpp
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.cpp
@@ -10,28 +10,16 @@ public:
 */
 
 class Solution {
-    unordered_set<int> hashSet;
-    
-    void populatePath(Node *node)
-    {
-        while(node)
-        {
-            hashSet.insert(node->val);
-            node = node->parent;
-        }
-    }
-    
     
 public:
     Node* lowestCommonAncestor(Node* p, Node * q) {
         
-        populatePath(p);
-        while(q)
+       Node *a = p, *b = q;
+        while(a!=b)
         {
-            if(hashSet.count(q->val))
-                return q;
-            q = q->parent;
+            a = (a)?a->parent:q;
+            b = (b)?b->parent:p;
         }
-        return NULL;
+        return a;
     }
 };
