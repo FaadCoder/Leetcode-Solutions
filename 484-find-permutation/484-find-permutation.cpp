8682b17a20d2a496159a30f7266d491147ad4d9c
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-08-01 09:48:08 +0530

    Time: 9 ms (70.83%), Space: 9.5 MB (81.94%) - LeetHub

--- 484-find-permutation/484-find-permutation.cpp
+++ 484-find-permutation/484-find-permutation.cpp
@@ -1,25 +1,48 @@
-class Solution
-{
-    public:
-        vector<int> findPermutation(string s)
+/*
+Algorithm:
+
+- Get the sorted array first of size s.length()+1
+- Then reverse each consecutive D's(Probably 1 D also occur)
+Lets say
+
+D I I D D
+[1,2,3,4,5,6]
+steps:
+
+[D] I I D D
+[2,1,3,4,5,6]
+ 
+ D I I [D D]
+[2,1,3,4,6,5]
+
+
+*/
+class Solution {
+    
+    void reverseVector(vector<int> &permutation, int start, int end)
+    {
+        while(start<end)
         {
-            vector<int> permutation;
-            int start = 0;
-            int end = s.length() + 1;
-            for (int i = start; i < end; i++)
-                permutation.push_back(i + 1);
-
-            for (int i = 0; i < s.length(); i++)
-            {
-                if (s[i] == 'D')
-                {
-                    int h = i;
-                    while (i < s.length() and s[i] == 'D')
-                        i++;
-                    reverse(permutation.begin() + h, permutation.begin() + i + 1);
-                }
-            }
-
-            return permutation;
+            swap(permutation[start++],permutation[end--]);
         }
+    }
+
+    
+public:
+    vector<int> findPermutation(string s) {
+        vector<int> permutation(s.length()+1);
+        iota(begin(permutation),end(permutation),1);
+        
+        for(int idx = 0; idx<s.length(); idx++)
+        {
+            if(s[idx]=='D')
+            {
+                int start = idx;
+                while(idx < s.length() and s[idx]=='D')
+                    idx++;
+                reverse(begin(permutation)+start,begin(permutation)+idx+1);
+            }
+        }
+        return permutation;
+    }
 };
