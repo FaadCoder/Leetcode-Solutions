b533ffb26f6e66d2432eebccb2f64ca593b483aa
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-08-02 21:07:06 +0530

    Time: 45 ms (33.02%), Space: 16.5 MB (18.70%) - LeetHub

---
+++ 1377-frog-position-after-t-seconds/1377-frog-position-after-t-seconds.cpp
@@ -0,0 +1,39 @@
+class Solution {
+    unordered_map<int,vector<int>> graph;
+    
+    double getProb(int src,int target,int t,vector<bool> &vis)
+    {
+        if(src!=1 and graph[src].size()==1 or t==0)
+            return src==target;
+        
+        vis[src] = true;
+        
+        double prob = 0;
+        
+        for(auto child:graph[src])
+        {
+            if(!vis[child])
+            {
+                int countNodes = graph[src].size()-(src!=1);
+                prob += (1.0/countNodes) * getProb(child,target,t-1,vis);
+                
+            }
+        }
+        
+        return prob;
+        
+    }
+    
+public:
+    double frogPosition(int n, vector<vector<int>>& edges, int t, int target) {
+        if (n == 1) return 1.0;
+        
+        for(auto edge:edges)
+        {
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+        vector<bool> vis(n+1,false);
+        return getProb(1,target,t,vis);
+    }
+};
