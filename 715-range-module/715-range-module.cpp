68b9cc42e40964e6c9035a98e014784680da499a
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-08-03 20:47:03 +0530

    Time: 303 ms (65.12%), Space: 71.1 MB (71.17%) - LeetHub

---
+++ 715-range-module/715-range-module.cpp
@@ -0,0 +1,43 @@
+class RangeModule {
+public:
+    void addRange(int left, int right) {
+        auto [begin, end] = intervals_.equal_range({ left, right });
+        
+        if (begin != end) {
+            left = min(left, begin->first);
+            right = max(right, prev(end)->second);
+            intervals_.erase(begin, end);
+        }
+        
+        intervals_.emplace(left, right);
+    }
+    
+    bool queryRange(int left, int right) const {
+        auto [begin, end] = intervals_.equal_range({ left, right });
+        return begin != end && left >= begin->first && right <= begin->second;
+    }
+    
+    void removeRange(int left, int right) {
+        auto [begin, end] = intervals_.equal_range({ left, right });
+        if (begin != end) {
+            auto beginFirst = begin->first;
+            auto endSecond = prev(end)->second;
+            intervals_.erase(begin, end);
+            if (beginFirst < left) {
+                intervals_.emplace(beginFirst, left);
+            }
+            if (endSecond > right) {
+                intervals_.emplace(right, endSecond);
+            }
+        }
+    }
+    
+private:
+    using Interval = pair<int, int>;
+    struct IntervalCompare {
+        bool operator()(const Interval& l, const Interval& r) const {
+            return l.second < r.first;
+        }
+    };
+    set<Interval, IntervalCompare> intervals_;
+};
