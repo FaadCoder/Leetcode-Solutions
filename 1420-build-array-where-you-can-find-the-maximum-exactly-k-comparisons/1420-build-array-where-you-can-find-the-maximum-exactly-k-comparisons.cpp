6209acb3c193fb4c12ee9e9a47a43d0b3a5da1dc
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-08-07 21:57:35 +0530

    Time: 449 ms (10.20%), Space: 10.1 MB (25.00%) - LeetHub

---
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -0,0 +1,39 @@
+class Solution {
+    const int MOD = 1e9+7;
+    vector<vector<vector<int>>> dp;
+    
+    int add(int a,int b)
+    {
+        return (a%MOD + b%MOD)%MOD;
+    }
+    
+    int getTotalWays(int n,int m,int k,int mx=0)
+    {
+        if(n==0)
+        {
+            return k==0;
+        }
+        
+        if(dp[n][mx][k]!=-1)
+            return dp[n][mx][k];
+        
+        
+        int totalWays = 0;
+        
+        for(int num=1 ;num<=m; num++)
+        {
+            if(num<=mx)
+                totalWays = add(totalWays,getTotalWays(n-1,m,k,mx));
+            else if(mx<num and k-1>=0)
+                totalWays = add(totalWays,getTotalWays(n-1,m,k-1,num));
+        }
+        
+        return dp[n][mx][k] = totalWays;
+    }
+    
+public:
+    int numOfArrays(int n, int m, int k) {
+        dp.resize(n+1,vector<vector<int>>(m+1,vector<int>(k+1,-1)));
+        return getTotalWays(n,m,k);
+    }
+};
