431ef17855a20cb9e773dc7ec62f9fd5fa60c166
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-08-07 21:57:48 +0530

    Time: 549 ms (7.24%), Space: 10.2 MB (23.68%) - LeetHub

--- 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.cpp
@@ -1,39 +1,40 @@
-class Solution {
-    const int MOD = 1e9+7;
-    vector<vector<vector<int>>> dp;
-    
-    int add(int a,int b)
+class Solution
+{
+    const int MOD = 1e9 + 7;
+    vector<vector<vector< int>>> dp;
+
+    int add(int a, int b)
     {
-        return (a%MOD + b%MOD)%MOD;
+        return (a % MOD + b % MOD) % MOD;
     }
-    
-    int getTotalWays(int n,int m,int k,int mx=0)
+
+    int getTotalWays(int n, int m, int k, int mx = 0)
     {
-        if(n==0)
+        if (n == 0)
         {
-            return k==0;
+            return k == 0;
         }
-        
-        if(dp[n][mx][k]!=-1)
+
+        if (dp[n][mx][k] != -1)
             return dp[n][mx][k];
-        
-        
+
         int totalWays = 0;
-        
-        for(int num=1 ;num<=m; num++)
+
+        for (int num = 1; num <= m; num++)
         {
-            if(num<=mx)
-                totalWays = add(totalWays,getTotalWays(n-1,m,k,mx));
-            else if(mx<num and k-1>=0)
-                totalWays = add(totalWays,getTotalWays(n-1,m,k-1,num));
+            if (num <= mx)
+                totalWays = add(totalWays, getTotalWays(n - 1, m, k, mx));
+            else if (mx < num and k - 1 >= 0)
+                totalWays = add(totalWays, getTotalWays(n - 1, m, k - 1, num));
         }
-        
+
         return dp[n][mx][k] = totalWays;
     }
-    
-public:
-    int numOfArrays(int n, int m, int k) {
-        dp.resize(n+1,vector<vector<int>>(m+1,vector<int>(k+1,-1)));
-        return getTotalWays(n,m,k);
-    }
+
+    public:
+        int numOfArrays(int n, int m, int k)
+        {
+            dp.resize(n + 1, vector<vector < int>> (m + 1, vector<int> (k + 1, -1)));
+            return getTotalWays(n, m, k);
+        }
 };
