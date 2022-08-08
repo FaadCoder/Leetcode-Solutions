4c60cff7b9b6a1bf7f40276ce8afa7aa27198590
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-08-08 10:04:42 +0530

    Time: 16 ms (81.01%), Space: 10.3 MB (88.72%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,86 +1,79 @@
 class Solution
 {
-    vector<vector<int>> topDownDP;
+    vector<vector < int>> topDownDP;
     private:
-        // 1. TOP DOWN DP Solution  
-        int lengthOfLISTopDown(vector<int> &nums,int idx, int prev)
+
+       	// 1. TOP DOWN DP Solution  
+        int lengthOfLISTopDown(vector<int> &nums, int idx, int prev)
         {
-            if(idx==nums.size())
+            if (idx == nums.size())
                 return 0;
-            
-            if(topDownDP[idx][prev+10001]!=-1)
-                return topDownDP[idx][prev+10001];
-            
-            
-            int excludeCurrentElement = lengthOfLISTopDown(nums,idx+1,prev);
+
+            if (topDownDP[idx][prev + 10001] != -1)
+                return topDownDP[idx][prev + 10001];
+
+            int excludeCurrentElement = lengthOfLISTopDown(nums, idx + 1, prev);
             int includeCurrentElement = 0;
-            if(nums[idx]>prev)
-                includeCurrentElement = 1+lengthOfLISTopDown(nums,idx+1,nums[idx]);
-            
-                return topDownDP[idx][prev+10001] = max(excludeCurrentElement, includeCurrentElement);
+            if (nums[idx] > prev)
+                includeCurrentElement = 1 + lengthOfLISTopDown(nums, idx + 1, nums[idx]);
+
+            return topDownDP[idx][prev + 10001] = max(excludeCurrentElement, includeCurrentElement);
         }
-    
-    
-        // 2. Bottom UP DP Solution
-        int lengthOfLISBottomUP(vector<int> &nums)
+
+   	// 2. Bottom UP DP Solution
+    int lengthOfLISBottomUP(vector<int> &nums)
+    {
+        int elements = nums.size();
+        vector<int> lisEndingAtIndex(elements, 1);
+
+        for (int currIdx = 1; currIdx < elements; currIdx++)
         {
-            int elements = nums.size();
-            vector<int> lisEndingAtIndex(elements,1);
-            
-            for(int currIdx = 1; currIdx<elements; currIdx++)
+            int currElement = nums[currIdx];
+            for (int prevIdx = 0; prevIdx < currIdx; prevIdx++)
             {
-                int currElement = nums[currIdx];
-                for(int prevIdx = 0; prevIdx<currIdx; prevIdx++)
+                int prevElement = nums[prevIdx];
+                if (currElement > prevElement and lisEndingAtIndex[currIdx] < lisEndingAtIndex[prevIdx] + 1)
                 {
-                    int prevElement = nums[prevIdx];
-                    if(currElement > prevElement and lisEndingAtIndex[currIdx] < lisEndingAtIndex[prevIdx] + 1)
-                    {
-                        lisEndingAtIndex[currIdx] = lisEndingAtIndex[prevIdx] + 1;
-                    }
+                    lisEndingAtIndex[currIdx] = lisEndingAtIndex[prevIdx] + 1;
                 }
             }
-            
-            return *max_element(lisEndingAtIndex.begin(),lisEndingAtIndex.end());
-            
         }
-        
-    
-        // 3. Binary Search Solution
-        int lengthOfLISBinarySearch(vector<int> &nums)
+
+        return *max_element(lisEndingAtIndex.begin(), lisEndingAtIndex.end());
+    }
+
+   	// 3. Binary Search Solution
+    int lengthOfLISBinarySearch(vector<int> &nums)
+    {
+        vector<int> monotonicVector;
+
+        for (int num: nums)
         {
-            vector<int> monotonicVector;
-
-            for (int num: nums)
+            auto it = lower_bound(begin(monotonicVector), end(monotonicVector), num);
+            if (it == monotonicVector.end())
             {
-                auto it = lower_bound(begin(monotonicVector), end(monotonicVector), num);
-                if (it == monotonicVector.end())
-                {
-                    monotonicVector.push_back(num);
-                }
-                else
-                {
-                    *it = num;
-                }
+                monotonicVector.push_back(num);
             }
-
-            return (int) monotonicVector.size();
+            else
+            {
+                *it = num;
+            }
         }
-        
+
+        return (int) monotonicVector.size();
+    }
+
     public:
         int lengthOfLIS(vector<int> &nums)
         {
-            /*
-            1. TOP DOWN DP
-            topDownDP.resize(nums.size()+1,vector<int>(20004,-1));
-            return lengthOfLISTopDown(nums,0,-10001);
-            */
-            
-            
-            //2. Bottom UP DP
-                return lengthOfLISBottomUP(nums);
-            
-            
-            // 3. Binary Search Solution
-            // return lengthOfLISBinarySearch(nums);
+           	// 1. TOP DOWN DP
+           	//topDownDP.resize(nums.size()+1,vector < int>(20004,-1));
+           	//return lengthOfLISTopDown(nums,0,-10001);
+
+           	//2. Bottom UP DP
+           	//  return lengthOfLISBottomUP(nums);
+
+           	// 3. Binary Search Solution
+            return lengthOfLISBinarySearch(nums);
         }
 };
