deaf652a5f103fcea8fe8545cc33cc6102c50187
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-08-08 10:28:09 +0530

    Time: 8 ms (53.14%), Space: 12.4 MB (61.13%) - LeetHub

---
+++ 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.cpp
@@ -0,0 +1,66 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    unordered_map<int,vector<int>> distanceToNodesMap;
+    int minDistance,maxDistance;
+    
+    void populateHorizontalDistances(TreeNode *root)
+    {
+        if(!root)
+            return;
+        
+        queue<pair<int,TreeNode *>> bfsQueue;
+        bfsQueue.push({0,root});
+        
+        while(!bfsQueue.empty())
+        {
+            int nodesAtCurrentLevel = bfsQueue.size();
+            while(nodesAtCurrentLevel>0)
+            {
+                nodesAtCurrentLevel-=1;
+                
+                auto [currentDistance, currNode] = bfsQueue.front();
+                bfsQueue.pop();
+                
+                minDistance = min(minDistance, currentDistance);
+                maxDistance = max(maxDistance, currentDistance);
+                
+                distanceToNodesMap[currentDistance].push_back(currNode->val);
+                
+                if(currNode->left)
+                    bfsQueue.push({currentDistance-1, currNode->left});
+                
+                if(currNode->right)
+                    bfsQueue.push({currentDistance+1, currNode->right});
+                
+            }
+        }
+        
+    }
+    
+public:
+    vector<vector<int>> verticalOrder(TreeNode* root) {
+        minDistance = INT_MAX;
+        maxDistance = INT_MIN;
+        populateHorizontalDistances(root);
+        
+        vector<vector<int>> verticalOrderStore;
+        
+        for(int dis = minDistance; dis<=maxDistance; dis++)
+        {
+            if(distanceToNodesMap[dis].size())
+                verticalOrderStore.push_back(distanceToNodesMap[dis]);
+        }
+        
+        return verticalOrderStore;
+    }
+};
