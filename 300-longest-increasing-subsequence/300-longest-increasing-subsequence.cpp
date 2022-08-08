31c9f062abf219cd02d736f5f66986bc6de06e50
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-08-08 09:50:35 +0530

    Time: 2708 ms (5.00%), Space: 555.5 MB (8.95%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,22 +1,50 @@
-class Solution {
-public:
-    int lengthOfLIS(vector<int>& nums) 
-    {
-        vector<int> monotonicVector;
-        
-        for(int num:nums)
+class Solution
+{
+    vector<vector<int>> topDownDP;
+    private:
+            
+        int lengthOfLISTopDown(vector<int> &nums,int idx, int prev)
         {
-            auto it = lower_bound(begin(monotonicVector),end(monotonicVector),num);
-            if(it==monotonicVector.end())
-            {
-                monotonicVector.push_back(num);
-            }
-            else
-            {
-                *it = num;
-            }
+            if(idx==nums.size())
+                return 0;
+            
+            if(topDownDP[idx][prev+10001]!=-1)
+                return topDownDP[idx][prev+10001];
+            
+            
+            int excludeCurrentElement = lengthOfLISTopDown(nums,idx+1,prev);
+            int includeCurrentElement = 0;
+            if(nums[idx]>prev)
+                includeCurrentElement = 1+lengthOfLISTopDown(nums,idx+1,nums[idx]);
+            
+                return topDownDP[idx][prev+10001] = max(excludeCurrentElement, includeCurrentElement);
         }
         
-        return (int)monotonicVector.size();
-    }
+        int lengthOfLISBinarySearch(vector<int> &nums)
+        {
+            vector<int> monotonicVector;
+
+            for (int num: nums)
+            {
+                auto it = lower_bound(begin(monotonicVector), end(monotonicVector), num);
+                if (it == monotonicVector.end())
+                {
+                    monotonicVector.push_back(num);
+                }
+                else
+                {
+                    *it = num;
+                }
+            }
+
+            return (int) monotonicVector.size();
+        }
+        
+    public:
+        int lengthOfLIS(vector<int> &nums)
+        {
+            topDownDP.resize(nums.size()+1,vector<int>(20004,-1));
+            
+            return lengthOfLISTopDown(nums,0,-10001);
+        }
 };
