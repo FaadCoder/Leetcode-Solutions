26cac6213f252ea51e9c1cf9de7bdc0d15d60a44
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-08-08 10:01:19 +0530

    Time: 297 ms (66.00%), Space: 10.6 MB (28.08%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -2,7 +2,7 @@ class Solution
 {
     vector<vector<int>> topDownDP;
     private:
-            
+        // 1. TOP DOWN DP Solution  
         int lengthOfLISTopDown(vector<int> &nums,int idx, int prev)
         {
             if(idx==nums.size())
@@ -19,7 +19,33 @@ class Solution
             
                 return topDownDP[idx][prev+10001] = max(excludeCurrentElement, includeCurrentElement);
         }
+    
+    
+        // 2. Bottom UP DP Solution
+        int lengthOfLISBottomUP(vector<int> &nums)
+        {
+            int elements = nums.size();
+            vector<int> lisEndingAtIndex(elements,1);
+            
+            for(int currIdx = 1; currIdx<elements; currIdx++)
+            {
+                int currElement = nums[currIdx];
+                for(int prevIdx = 0; prevIdx<currIdx; prevIdx++)
+                {
+                    int prevElement = nums[prevIdx];
+                    if(currElement > prevElement and lisEndingAtIndex[currIdx] < lisEndingAtIndex[prevIdx] + 1)
+                    {
+                        lisEndingAtIndex[currIdx] = lisEndingAtIndex[prevIdx] + 1;
+                    }
+                }
+            }
+            
+            return *max_element(lisEndingAtIndex.begin(),lisEndingAtIndex.end());
+            
+        }
         
+    
+        // 3. Binary Search Solution
         int lengthOfLISBinarySearch(vector<int> &nums)
         {
             vector<int> monotonicVector;
@@ -43,8 +69,18 @@ class Solution
     public:
         int lengthOfLIS(vector<int> &nums)
         {
+            /*
+            1. TOP DOWN DP
             topDownDP.resize(nums.size()+1,vector<int>(20004,-1));
-            
             return lengthOfLISTopDown(nums,0,-10001);
+            */
+            
+            
+            //2. Bottom UP DP
+                return lengthOfLISBottomUP(nums);
+            
+            
+            // 3. Binary Search Solution
+            // return lengthOfLISBinarySearch(nums);
         }
 };
