a3e98ac3a7053375aec878a9c909701cc7a4011e
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-08-13 10:45:01 +0530

    Time: 0 ms (100.00%), Space: 6 MB (44.98%) - LeetHub

--- 779-k-th-symbol-in-grammar/779-k-th-symbol-in-grammar.cpp
+++ 779-k-th-symbol-in-grammar/779-k-th-symbol-in-grammar.cpp
@@ -1,17 +1,18 @@
-class Solution {
-public:
-    int kthGrammar(int n, int k) {
-        if(k==1)
-            return 0;
-        if(n==1)
-            return 0;
-        
-        int symbols = (1<<(n-1));
-        int kthSymbol = 0;
-        if(k<=(symbols/2))
-            kthSymbol = kthGrammar(n-1, k);
-        else
-            kthSymbol = !kthGrammar(n-1, k - (symbols>>1));
-        return kthSymbol;
-    }
+class Solution
+{
+    public:
+        int kthGrammar(int n, int k)
+        {
+            if (k == 1)
+                return 0;
+            if (n == 1)
+                return 0;
+
+            int symbols = (1 << (n - 1));
+
+            if (k <= (symbols / 2))
+                return kthGrammar(n - 1, k);
+
+            return !kthGrammar(n - 1, k - (symbols >> 1));
+        }
 };
