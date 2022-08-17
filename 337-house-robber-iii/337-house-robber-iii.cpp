de5c14346a7bfc18faeecc539dc396d536343a96
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-08-17 15:40:59 +0530

    Time: 38 ms (28.77%), Space: 25.3 MB (37.27%) - LeetHub

---
+++ 337-house-robber-iii/337-house-robber-iii.cpp
@@ -0,0 +1,33 @@
+/**
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
+ */
+class Solution
+{
+    map<TreeNode *, int> dp;
+    
+    public:
+        int rob(TreeNode *root)
+        {
+            if (!root)
+                return 0;
+            
+            if(dp.count(root))
+                return dp[root];
+            
+            int left = rob(root->left);
+            int right = rob(root->right);
+            
+            int curr =  root->val + ((root->left) ? rob(root->left->left) + rob(root->left->right) : 0 ) + ((root->right) ? rob(root->right->left) + rob(root->right->right) : 0);
+            
+            return dp[root] = max(curr, left + right);
+            
+        }
+};
