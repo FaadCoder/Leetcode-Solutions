c44802ce870ea630d4832f579e87b743d14a8a08
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-08-18 19:40:55 +0530

    Time: 300 ms (94.14%), Space: 119.4 MB (33.15%) - LeetHub

--- 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.cpp
+++ 373-find-k-pairs-with-smallest-sums/373-find-k-pairs-with-smallest-sums.cpp
@@ -1,39 +1,43 @@
-struct Pair{
+struct Pair
+{
     int num1;
     int num2;
     int num2Idx;
-    
-    Pair(int _num1, int _num2, int _num2Idx): num1(_num1), num2(_num2), num2Idx(_num2Idx){}
+
+    Pair(int _num1, int _num2, int _num2Idx): num1(_num1), num2(_num2), num2Idx(_num2Idx) {}
 };
 
-struct Comparator{
-    bool operator()(Pair &p1, Pair &p2){
+struct Comparator
+{
+    bool operator()(Pair &p1, Pair &p2)
+    {
         return p1.num1 + p1.num2 > p2.num1 + p2.num2;
     }
 };
 
-class Solution {
-    
-    
-public:
-    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-        priority_queue<Pair, vector<Pair>, Comparator> minHeap;
-        vector<vector<int>> ans;
-        
-        for(int num1:nums1)
-            minHeap.push(Pair(num1, nums2[0], 0));
-        
-        while(k > 0 and !minHeap.empty())
+class Solution
+{
+    public:
+        vector<vector < int>> kSmallestPairs(vector<int> &nums1, vector<int> &nums2, int k)
         {
-            k-=1;
-            auto top = minHeap.top();
-            minHeap.pop();
-            ans.push_back({top.num1, top.num2});
-            
-            if(top.num2Idx + 1 < nums2.size())
-                minHeap.push(Pair(top.num1, nums2[top.num2Idx + 1], top.num2Idx + 1));
+            priority_queue<Pair, vector < Pair>, Comparator> minHeap;
+            vector<vector < int>> ans;
+
+            for (int num1: nums1)
+                minHeap.push(Pair(num1, nums2[0], 0));
+
+            while (k > 0 and!minHeap.empty())
+            {
+                k -= 1;
+                auto top = minHeap.top();
+                minHeap.pop();
+                ans.push_back({ top.num1,
+                    top.num2 });
+
+                if (top.num2Idx + 1 < nums2.size())
+                    minHeap.push(Pair(top.num1, nums2[top.num2Idx + 1], top.num2Idx + 1));
+            }
+
+            return ans;
         }
-        
-        return ans;
-    }
 };
