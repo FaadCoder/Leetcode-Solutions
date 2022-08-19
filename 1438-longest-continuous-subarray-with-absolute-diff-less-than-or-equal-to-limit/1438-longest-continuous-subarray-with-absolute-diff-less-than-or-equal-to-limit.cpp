e963a3c1bb3a0cb74a521288602dce8a944466ff
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-08-19 22:12:14 +0530

    Time: 259 ms (44.04%), Space: 52.2 MB (58.48%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.cpp
@@ -5,14 +5,24 @@ public:
         int len = 1;
         
         int start = 0;
-        multiset<int> orderedSet;
+        deque<int> maxDeque, minDeque;
         
         for(int end = 0; end < nums.size(); end++)
         {
-            orderedSet.insert(nums[end]);
-            while(orderedSet.size() and abs(*orderedSet.begin() - *orderedSet.rbegin()) > limit)
+            
+            while(!maxDeque.empty() and nums[end] > maxDeque.back()) maxDeque.pop_back();
+            while(!minDeque.empty() and nums[end] < minDeque.back()) minDeque.pop_back();
+            
+            maxDeque.push_back(nums[end]);
+            minDeque.push_back(nums[end]);
+            
+            while(start < end and maxDeque.size() and minDeque.size() and abs(maxDeque.front() - minDeque.front()) > limit)
             {
-                orderedSet.erase(orderedSet.find(nums[start]));
+                if(maxDeque.front() == nums[start])
+                    maxDeque.pop_front();
+                if(minDeque.front() == nums[start])
+                    minDeque.pop_front(); 
+                
                 start+=1;
             }
             len = max(len, end - start + 1);
