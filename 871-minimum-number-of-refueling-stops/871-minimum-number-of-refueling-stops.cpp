f13a65f176631fe51c7eaf52c6e9bb7ee852b473
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-08-20 12:48:37 +0530

    Time: 43 ms (62.19%), Space: 16.2 MB (51.25%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
     int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
-        int maxReach = startFuel;
         
-        priority_queue<int> fuelMaxHeap;
-        int station = 0;
-        int stops = 0;
+        // Represents the current distance travelled.
+        int currentDistance = startFuel;
         
-        while(maxReach < target)
+        // idx -> represent the stations current idx
+        int idx = 0;
+        
+        //Max heap to store max fuel
+        priority_queue<int> maxHeap;
+        
+        // Store min Stops
+        int minStops = 0;
+        
+        while(currentDistance < target)
         {
-            while(station < stations.size() and stations[station][0]<=maxReach)
-                fuelMaxHeap.push(stations[station++][1]);
+            // Store fuels of all the stations whose distance is less than equals to currentDistance
+            while(idx < stations.size() and stations[idx][0] <= currentDistance)
+                maxHeap.push(stations[idx++][1]);
             
-            if(fuelMaxHeap.empty())
+            // If there are no stations available such that we can reach target from current distance
+            if(maxHeap.empty())
                 return -1;
-            maxReach += fuelMaxHeap.top();
-            fuelMaxHeap.pop();
-            stops++;
+            
+            // Use the Max fuel from available fuels in order to cover max distance
+            currentDistance += maxHeap.top();
+            maxHeap.pop();
+            
+            // we have refilled fuel above
+            minStops += 1;
         }
-        return stops;
-    } 
+        
+        return minStops;
+    }
 };
