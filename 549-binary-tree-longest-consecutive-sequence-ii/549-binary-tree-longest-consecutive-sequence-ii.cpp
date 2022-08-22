ce3a6d3d70440b4f6b526984c65ba3c3d21334b7
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-08-22 09:41:17 +0530

    Time: 15 ms (84.81%), Space: 22.9 MB (75.53%) - LeetHub

--- 549-binary-tree-longest-consecutive-sequence-ii/549-binary-tree-longest-consecutive-sequence-ii.cpp
+++ 549-binary-tree-longest-consecutive-sequence-ii/549-binary-tree-longest-consecutive-sequence-ii.cpp
@@ -1,35 +1,39 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
+    
+    int longestLen = 0;
+    
+    pair<int, int> consecutivePath(TreeNode *root)
+    {
+        if(!root)
+            return {0,0};
+        int increment = 1, decrement = 1;
+        
+        if(root->left)
+        {
+            auto leftSubtreePath = consecutivePath(root->left);
+            if(root->val == root->left->val + 1)
+                decrement = max(decrement, leftSubtreePath.second + 1);
+            else if(root->val == root->left->val - 1)
+                increment = max(increment, leftSubtreePath.first + 1);
+        }
+        
+        if(root->right)
+        {
+            auto rightSubtreePath = consecutivePath(root->right);
+            if(root->val == root->right->val + 1)
+                decrement = max(decrement, rightSubtreePath.second + 1);
+            else if(root->val == root->right->val - 1)
+                increment = max(increment, rightSubtreePath.first + 1);    
+        }
+        
+        longestLen = max(longestLen, increment + decrement - 1);
+        
+        return {increment, decrement};
+    }
+    
 public:
-    pair<int,int> traverse(TreeNode *t, int &ans) {
-    if (!t) return make_pair(0, 0);
-    pair<int,int> left = traverse(t->left, ans);
-    pair<int,int> right = traverse(t->right, ans);
-    int inc=1, dec=1;
-    if (left.first && t->left->val+1==t->val)
-        inc = max(inc, left.first+1);
-    if (right.first && t->right->val+1==t->val)
-        inc = max(inc, right.first+1);
-    if (left.second && t->left->val-1==t->val)
-        dec = max(dec, left.second+1);
-    if (right.second && t->right->val-1==t->val)
-        dec = max(dec, right.second+1);
-    ans = max(ans, inc+dec-1);
-    return make_pair(inc, dec);
-}
-int longestConsecutive(TreeNode* root) {
-    int ans = 0;
-    traverse(root, ans);
-    return ans;
-}
+    int longestConsecutive(TreeNode* root) {
+        consecutivePath(root);
+        return longestLen;
+    }
 };
