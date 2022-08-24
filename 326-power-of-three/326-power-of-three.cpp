45f955a199a12a8c2288ebc19b5d9396677dfc9a
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-08-24 09:35:53 +0530

    Time: 16 ms (94.46%), Space: 5.8 MB (88.51%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n == 0)
-            return false;
-        
-        return (ceil(log10(n)/log10(3)) == floor(log10(n)/log10(3)));
+         return n > 0 && 1162261467 % n == 0;
     }
 };
