c33e734e4cdbcbe888832e805ea84a5b136a9502
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-08-28 13:11:37 +0530

    Time: 15 ms (69.45%), Space: 9.7 MB (7.49%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -1,53 +1,20 @@
 class Solution {
-    int rows, cols;
-    
-    void countSort(vector<int> &temp)
-    {
-        int count[101] = {0};
-        
-        for(int num : temp)
-            count[num] += 1;
-        
-        int idx = 0;
-        
-        for(int num = 1; num <= 100; num++)
-        {
-            while(count[num] > 0 )
-            {
-                temp[idx++] = num;
-                count[num] -= 1;
-            }
-        }
-    }
-    
-    void sortDiagonal(int row, int col, vector<vector<int>> &mat)
-    {
-        int startRow = row;
-        int startCol = col;
-        vector<int> temp;
-      
-        while(row<rows and col<cols)
-            temp.push_back(mat[row++][col++]);
-        
-        countSort(temp);
-        
-        for(int num : temp)
-        {
-            mat[startRow][startCol] = num;
-            startRow += 1;
-            startCol += 1;
-        }
-    }
-    
 public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
-        rows = mat.size();
-        cols = mat[0].size();
-        
-        for(int row = 0; row < rows; row++)
-           for(int col = 0; col < cols; col++)
-                sortDiagonal(row, col, mat);
-        
+        map<int, priority_queue<int,vector<int>,greater<int>>> mp;
+        int m = mat.size();
+        int n = mat[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                mp[i-j].push(mat[i][j]);
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                mat[i][j] = mp[i-j].top();
+                mp[i-j].pop();
+            }
+        }
         return mat;
     }
 };
