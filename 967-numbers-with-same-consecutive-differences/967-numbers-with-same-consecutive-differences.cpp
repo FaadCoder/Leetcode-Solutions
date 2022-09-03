4b55e526b092845231dafece5d77221258ce8f0f
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-09-03 09:33:26 +0530

    Time: 6 ms (42.98%), Space: 7.2 MB (82.84%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.cpp
@@ -1,48 +1,27 @@
 class Solution {
-    unordered_set<string> vis;
-    
-    unordered_set<int> visHashSet;
-    
-    vector<int> ans;
-    
-    void populateNumsWithString(int n, int k, string temp="")
-    {
-        if(temp.length() == n and !vis.count(temp))
+public:
+    vector<int> numsSameConsecDiff(int n, int k) {
+        vector<int> currSet(9);
+        iota(begin(currSet), end(currSet), 1);
+        
+        for(int len = 2; len <= n; len++)
         {
-            if(temp.length() > 1 and temp[0]=='0')
-                return;
-            vis.insert(temp);
-            ans.push_back(stoi(temp));
-            return;
+            vector<int> nextSet;
+            
+            for(int num : currSet)
+            {
+                int lastDigit = num % 10;
+                
+                if(lastDigit + k <= 9)
+                    nextSet.push_back(num * 10 + lastDigit + k);
+                
+                if(k > 0 and lastDigit - k >= 0)
+                    nextSet.push_back(num * 10 + lastDigit - k);
+            }
+            
+            currSet = nextSet;
         }
         
-        for(int num = 0; num <= 9; num++)
-        {
-            if(temp.empty())
-                populateNumsWithString(n, k, temp + to_string(num));
-            else
-            {
-                int prevNum = temp.back() - '0';
-                if(abs(prevNum - num) == k)
-                    populateNumsWithString(n, k, temp + to_string(num));
-            }
-        }
-    }
-    
-public:
-    vector<int> numsSameConsecDiff(int N, int K) {
-        vector<int> cur = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        for (int i = 2; i <= N; ++i) {
-            vector<int> cur2;
-            for (int x : cur) {
-                int y = x % 10;
-                if (y + K < 10)
-                    cur2.push_back(x * 10 + y + K);
-                if (K > 0 && y - K >= 0)
-                    cur2.push_back(x * 10 + y - K);
-            }
-            cur = cur2;
-        }
-        return cur;
+        return currSet;
     }
 };
