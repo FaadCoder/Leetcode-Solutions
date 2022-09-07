ea6d129657b7d29e361fcf90b6e4b72b389886d6
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-09-07 09:11:31 +0530

    Time: 72 ms (11.29%), Space: 66.4 MB (12.56%) - LeetHub

--- 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.cpp
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.cpp
@@ -1,37 +1,35 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
+class Solution
+{
 
-    string getString(TreeNode *root)
-    {
-        if(!root)
-            return "";
-        
-        if(!root->left and !root->right)
-            return to_string(root->val);
-        
-        string leftSubtree = getString(root->left);
-        string rightSubtree = getString(root->right);
-        
-        string str = to_string(root->val) + "(" + leftSubtree + ")" ;
-        
-        if(!rightSubtree.empty())
-            str += "(" + rightSubtree + ")";
-
-        return str;
-    }
-    
     public:
-    string tree2str(TreeNode* root) {
-        return getString(root);
-    }
+        string tree2str(TreeNode *root)
+        {
+            if (!root)
+                return "";
+
+            string str = to_string(root->val);
+            
+            if (!root->left and!root->right)
+                return str;
+
+            string leftSubtree = tree2str(root->left);
+            string rightSubtree = tree2str(root->right);
+            str += "(" + leftSubtree + ")";
+            
+            if (!rightSubtree.empty())
+                str += "(" + rightSubtree + ")";
+
+            return str;
+        }
 };
