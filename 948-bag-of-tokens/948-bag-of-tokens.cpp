80af7960d05f250970f1848808ee9da033232db1
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-09-12 09:14:36 +0530

    Time: 11 ms (52.87%), Space: 10.6 MB (66.86%) - LeetHub

---
+++ 948-bag-of-tokens/948-bag-of-tokens.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        sort(begin(tokens), end(tokens));
+        int start = 0, end = tokens.size() - 1;
+        
+        int maxScore = 0;
+        int currentScore = 0;
+        
+        while(start <= end)
+        {
+            if(power >= tokens[start])
+            {
+                power -= tokens[start++];
+                currentScore += 1;
+            }
+            else
+            {
+                if(currentScore > 0)
+                {
+                    currentScore -= 1;
+                    power += tokens[end--];    
+                }
+                else
+                {
+                    break;
+                }
+            }
+            
+            maxScore = max(maxScore, currentScore);
+        }
+        
+        return maxScore;
+    }
+};
