d944c1dd611f04480af129c465adfbcc948009ea
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-10-16 18:42:15 +0530

    Time: 0 ms, Memory: 6 MB - LeetHub

---
+++ convert-a-number-to-hexadecimal/convert-a-number-to-hexadecimal.cpp
@@ -0,0 +1,14 @@
+const string HEX = "0123456789abcdef";
+class Solution {
+public:
+    string toHex(int num) {
+        if (num == 0) return "0";
+        string result;
+        int count = 0;
+        while (num && count++ < 8) {
+            result = HEX[(num & 0xf)] + result;
+            num >>= 4;
+        }
+        return result;
+    }
+};
