0a7787e0cae39dfdee7f9040cf40c6ac22f130ce
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-10-18 09:41:11 +0530

    Time: 23 ms (31.33%), Space: 6.6 MB (56.81%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.cpp
+++ 0038-count-and-say/0038-count-and-say.cpp
@@ -1,28 +1,37 @@
-class Solution {
-public:
-    string countAndSay(int n) {
-        if(n==1)
-            return "1";
-        
-        string previous = countAndSay(n-1);
+class Solution
+{
+    string getCurrentSequence(string &previous)
+    {
         string current;
         int counter = 1;
-        
-        for(int idx = 1; idx < previous.length(); idx++)
+
+        for (int idx = 1; idx < previous.length(); idx++)
         {
-            if(previous[idx - 1] == previous[idx])
+            if (previous[idx - 1] == previous[idx])
             {
-                counter+=1;
+                counter += 1;
             }
             else
             {
                 current += to_string(counter);
-                current.push_back(previous[idx-1]);
+                current.push_back(previous[idx - 1]);
                 counter = 1;
             }
         }
+
         current += to_string(counter);
         current.push_back(previous.back());
+
         return current;
     }
+
+    public:
+        string countAndSay(int n)
+        {
+            if (n == 1)
+                return "1";
+
+            string previous = countAndSay(n - 1);
+            return getCurrentSequence(previous);
+        }
 };
