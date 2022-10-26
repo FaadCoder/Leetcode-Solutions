376b9d70d39637fe917cd39fca8aa2f16e53528c
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-10-26 09:30:56 +0530

    Time: 311 ms (77.73%), Space: 113.8 MB (46.74%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -24,16 +24,3 @@ public:
     }
     
 };
-    // Map<Integer, Integer> map = new HashMap<>(){{put(0,-1);}};;
-    // int runningSum = 0;
-    // for (int i=0;i<nums.length;i++) {
-    //     runningSum += nums[i];
-    //     if (k != 0) runningSum %= k; 
-    //     Integer prev = map.get(runningSum);
-    //     if (prev != null) {
-    //         if (i - prev > 1) return true;
-    //     }
-    //     else map.put(runningSum, i);
-    // }
-    // return false;
-//(x%k) = (k-y)%k
