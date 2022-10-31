8faa63bc191ae07f7bcdf17d59c748089463ef26
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-10-31 21:46:33 +0530

    Time: 8 ms (5.42%), Space: 6.2 MB (51.12%) - LeetHub

---
+++ 1041-robot-bounded-in-circle/1041-robot-bounded-in-circle.cpp
@@ -0,0 +1,66 @@
+class Solution
+{
+    enum Direction
+    {
+        North,
+        East,
+        South,
+        West
+    };
+
+    vector<Direction> directions = 
+    { 
+        North,
+        East,
+        South,
+        West
+    };
+
+    void turnLeft(Direction & currentDirection)
+    {
+        currentDirection = directions[(currentDirection + 3) % 4];
+    }
+
+    void turnRight(Direction & currentDirection)
+    {
+        currentDirection = directions[(currentDirection + 1) % 4];
+    }
+
+    void move(Direction direction, int &dirX, int &dirY)
+    {
+        switch (direction)
+        {
+            case North:
+                dirY -= 1;
+                break;
+            case South:
+                dirY += 1;
+                break;
+            case East:
+                dirX += 1;
+                break;
+            case West:
+                dirX -= 1;
+                break;
+        }
+    }
+
+    public:
+        bool isRobotBounded(string instructions)
+        {
+            int dirX = 0, dirY = 0;
+            Direction currentDirection = North;
+
+            for (char &ch: instructions)
+            {
+                if (ch == 'G')
+                    move(currentDirection, dirX, dirY);
+                else if (ch == 'L')
+                    turnLeft(currentDirection);
+                else
+                    turnRight(currentDirection);
+            }
+
+            return (dirX == 0 and dirY == 0) or(currentDirection != North);
+        }
+};
