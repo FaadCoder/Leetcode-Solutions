14f886cb84b87644ebd565609539a953174b047f
Paras saini <67194519+FaadCoder@users.noreply.github.com>
2022-11-01 09:38:53 +0530

    Time: 58 ms (43.94%), Space: 13.2 MB (52.18%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -1,32 +1,56 @@
-class Solution {
+class Solution
+{
     int rows, cols;
-    
-public:
-    vector<int> findBall(vector<vector<int>>& grid) {
-        rows = grid.size();
-        cols = grid[0].size();
-        
-        vector<int> result(cols);
-        
-        for(int col = 0; col<cols; col++)
-        {
-            int currentCol = col, currentRow = 0;
-            
-            while(currentRow < rows)
-            {
-                int currentValue = grid[currentRow][currentCol];
-                
-                if(currentValue == 1 and (currentCol + 1) < cols and currentValue == grid[currentRow][currentCol + 1] )
-                    currentRow++, currentCol++;
-                else if(currentValue == -1 and (currentCol - 1) >= 0 and currentValue == grid[currentRow][currentCol - 1] )
-                    currentRow++, currentCol--;
-                else
-                    break;
-            }
-            
-            result[col] = (currentRow >= rows) ? currentCol : -1;
-        }
-        
-        return result;
+
+    bool isValidRightMove(int currentRow, int currentCol, vector<vector < int>> &grid)
+    {
+        int currentValue = grid[currentRow][currentCol];
+        return (currentValue == 1 and(currentCol + 1) < cols and currentValue == grid[currentRow][currentCol + 1]);
     }
+
+    bool isValidLeftMove(int currentRow, int currentCol, vector<vector < int>> &grid)
+    {
+        int currentValue = grid[currentRow][currentCol];
+        return (currentValue == -1 and(currentCol - 1) >= 0 and currentValue == grid[currentRow][currentCol - 1]);
+    }
+
+    void moveRight(int &row, int &col)
+    {
+        row += 1;
+        col += 1;
+    }
+
+    void moveLeft(int &row, int &col)
+    {
+        row += 1;
+        col -= 1;
+    }
+
+    public:
+        vector<int> findBall(vector<vector < int>> &grid)
+        {
+            rows = grid.size();
+            cols = grid[0].size();
+
+            vector<int> result(cols);
+
+            for (int col = 0; col < cols; col++)
+            {
+                int currentCol = col, currentRow = 0;
+
+                while (currentRow < rows)
+                {
+                    if (isValidRightMove(currentRow, currentCol, grid))
+                        moveRight(currentRow, currentCol);
+                    else if (isValidLeftMove(currentRow, currentCol, grid))
+                        moveLeft(currentRow, currentCol);
+                    else
+                        break;
+                }
+
+                result[col] = (currentRow >= rows) ? currentCol : -1;
+            }
+
+            return result;
+        }
 };
